7bc8e809d5c90a67f11f7bb80c5de983172c7c34
aha <aha@scireum.de>
2018-06-28 17:37:59 +0200

    Adds a security log ("Audit Log") as new facility.

---
+++ src/main/java/sirius/biz/protocol/AuditLog.java
@@ -0,0 +1,149 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.protocol;
+
+import sirius.db.es.Elastic;
+import sirius.kernel.Sirius;
+import sirius.kernel.async.CallContext;
+import sirius.kernel.commons.Strings;
+import sirius.kernel.di.Initializable;
+import sirius.kernel.di.std.Part;
+import sirius.kernel.di.std.Register;
+import sirius.kernel.health.Exceptions;
+import sirius.web.http.WebContext;
+import sirius.web.security.UserContext;
+import sirius.web.security.UserInfo;
+
+import javax.annotation.CheckReturnValue;
+import javax.annotation.Nullable;
+import java.time.LocalDateTime;
+
+/**
+ * Inserts entries into the security log of the system.
+ */
+@Register(classes = {AuditLog.class, Initializable.class})
+public class AuditLog implements Initializable {
+
+    @Part
+    private Elastic elastic;
+
+    private boolean enabled;
+
+    @Override
+    public void initialize() throws Exception {
+        enabled = Sirius.isFrameworkEnabled(Protocols.FRAMEWORK_PROTOCOLS);
+    }
+
+    /**
+     * Fluent builder for audit logs.
+     */
+    public class AuditLogBuilder {
+
+        private AuditLogEntry entry = new AuditLogEntry();
+
+        protected AuditLogBuilder(String message, boolean negative) {
+            entry.setTimestamp(LocalDateTime.now());
+            entry.setNegative(negative);
+            entry.setMessage(message);
+            entry.setIp(getCurrentIP());
+        }
+
+        private String getCurrentIP() {
+            WebContext webContext = CallContext.getCurrent().get(WebContext.class);
+            if (webContext.isValid()) {
+                return webContext.getRemoteIP().toString();
+            }
+
+            return null;
+        }
+
+        /**
+         * Creates the entry for the current user in {@link UserContext#getCurrentUser()}.
+         *
+         * @return the builder for fluent method calls
+         */
+        @CheckReturnValue
+        public AuditLogBuilder forCurrentUser() {
+            UserInfo user = UserContext.getCurrentUser();
+            return forUser(user.getUserId(), user.getUserName()).forTenant(user.getTenantId(), user.getTenantName());
+        }
+
+        /**
+         * Creates the entry for the given user.
+         *
+         * @param id   the ID of the user
+         * @param name the name of the user
+         * @return the builder for fluent method calls
+         */
+        @CheckReturnValue
+        public AuditLogBuilder forUser(@Nullable String id, @Nullable String name) {
+            entry.setUser(id);
+            entry.setUserName(name);
+
+            return this;
+        }
+
+        /**
+         * Creates the entry for the given tenant.
+         *
+         * @param id   the ID of the tenant
+         * @param name the name of the tenant
+         * @return the builder for fluent method calls
+         */
+        @CheckReturnValue
+        public AuditLogBuilder forTenant(@Nullable String id, @Nullable String name) {
+            entry.setTenant(id);
+            entry.setTenantName(name);
+
+            return this;
+        }
+
+        /**
+         * Writes the entry into the log table.
+         */
+        public void log() {
+            try {
+                if (!enabled || elastic == null || !elastic.getReadyFuture().isCompleted() || Sirius.isStartedAsTest()) {
+                    return;
+                }
+                elastic.update(entry);
+            } catch (Exception e) {
+                Exceptions.ignore(e);
+            }
+        }
+    }
+
+    /**
+     * Prepares a "neutral" entry to be inserted into the log.
+     * <p>
+     * Note that a builder is returned and eventually {@link AuditLogBuilder#log()} has to be called.
+     *
+     * @param message the message to log
+     * @param params  the objects to fill into the placeholders in <tt>message</tt>
+     * @return the builder used to supply further info
+     */
+    @CheckReturnValue
+    public AuditLogBuilder neutral(String message, Object... params) {
+        return new AuditLogBuilder(params.length == 0 ? message : Strings.apply(message, params), false);
+    }
+
+    /**
+     * Prepares a "negative" entry to be inserted into the log.
+     * <p>
+     * Note that a builder is returned and eventually {@link AuditLogBuilder#log()} has to be called.
+     *
+     * @param message the message to log
+     * @param params  the objects to fill into the placeholders in <tt>message</tt>
+     * @return the builder used to supply further info
+     */
+    @CheckReturnValue
+    public AuditLogBuilder negative(String message, Object... params) {
+        return new AuditLogBuilder(params.length == 0 ? message : Strings.apply(message, params), true);
+    }
+}
