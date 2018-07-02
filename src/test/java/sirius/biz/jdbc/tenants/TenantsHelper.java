09d5843c9b3b27a9e06ca62f9f0c31936bb45c64
fak <fak@scireum.de>
2018-07-02 09:01:41 +0200

    Remove hacky CSRF tokens in tests
    A better solution is needed.

--- src/test/java/sirius/biz/jdbc/tenants/TenantsHelper.java
+++ src/test/java/sirius/biz/jdbc/tenants/TenantsHelper.java
@@ -12,7 +12,6 @@ import sirius.biz.jdbc.model.LoginData;
 import sirius.db.jdbc.OMA;
 import sirius.kernel.di.std.ConfigValue;
 import sirius.kernel.di.std.Part;
-import sirius.web.http.CSRFHelper;
 import sirius.web.http.TestRequest;
 import sirius.web.security.UserContext;
 
@@ -35,9 +34,6 @@ public class TenantsHelper {
 
     @Part
     private static Tenants tenants;
-
-    @Part
-    private static CSRFHelper csrfHelper;
 
     private static Tenant testTenant;
     private static UserAccount testUser;
@@ -94,7 +90,6 @@ public class TenantsHelper {
         request.setSessionValue("default-tenant-id", getTestTenant().getId());
         request.setSessionValue("default-tenant-name", getTestTenant().getName());
         request.setSessionValue("default-user-id", getTestUser().getUniqueName());
-        request.setAttribute("CSRFToken", csrfHelper.getCSRFToken(request));
     }
 
     public static void clearCurrentUser() {
