0e5e901a74bfad84b47532989a12c58e9258193a
aha <aha@scireum.de>
2019-02-25 16:59:19 +0100

    Provides an import handler used by the ImporterSpec.

---
+++ src/test/java/sirius/biz/importer/SQLTenantImportHandler.java
@@ -0,0 +1,59 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.importer;
+
+import sirius.biz.tenants.TenantData;
+import sirius.biz.tenants.jdbc.SQLTenant;
+import sirius.db.jdbc.batch.FindQuery;
+import sirius.kernel.commons.Strings;
+import sirius.kernel.di.std.Register;
+
+import java.util.function.BiConsumer;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+/**
+ * Provides a test importer used for SQL tenants and alsi in {@link ImporterSpec}.
+ */
+public class SQLTenantImportHandler extends SQLEntityImportHandler<SQLTenant> {
+
+    @Register
+    public static class SQLTenantImportHandlerFactory implements ImportHandlerFactory {
+
+        @Override
+        public boolean accepts(Class<?> type) {
+            return type == SQLTenant.class;
+        }
+
+        @Override
+        public ImportHandler<?> create(Class<?> type, ImporterContext context) {
+            return new SQLTenantImportHandler(type, context);
+        }
+    }
+
+    /**
+     * Creates a new instance for the given type of entities and import context.
+     *
+     * @param clazz   the type of entities being handled
+     * @param context the import context to use
+     */
+    protected SQLTenantImportHandler(Class<?> clazz, ImporterContext context) {
+        super(clazz, context);
+    }
+
+    @Override
+    protected void collectFindQueries(BiConsumer<Predicate<SQLTenant>, Supplier<FindQuery<SQLTenant>>> queryConsumer) {
+        queryConsumer.accept(tenant -> Strings.isFilled(tenant.getIdAsString()),
+                             () -> context.getBatchContext().findQuery(SQLTenant.class, SQLTenant.ID));
+        queryConsumer.accept(tenant -> Strings.isFilled(tenant.getTenantData().getAccountNumber()),
+                             () -> context.getBatchContext()
+                                          .findQuery(SQLTenant.class,
+                                                     SQLTenant.TENANT_DATA.inner(TenantData.ACCOUNT_NUMBER)));
+    }
+}
