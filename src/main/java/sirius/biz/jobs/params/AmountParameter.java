640094bc44134cd31d3e64426790c25d2d558355
fak <fak@scireum.de>
2019-04-08 15:56:39 +0200

    Parameter for accepting amounts
    Parameter for using amounts in jobs.
    
    Tags: job, parameter, amount

---
+++ src/main/java/sirius/biz/jobs/params/AmountParameter.java
@@ -0,0 +1,47 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.jobs.params;
+
+import sirius.kernel.commons.Amount;
+import sirius.kernel.commons.Value;
+import sirius.kernel.nls.NLS;
+
+import java.util.Optional;
+
+/**
+ * Provides a parameter which accepts {@link Amount amounts}.
+ */
+public class AmountParameter extends Parameter<Amount, AmountParameter> {
+
+    /**
+     * Creates a new parameter with the given name and label.
+     *
+     * @param name  the name of the parameter
+     * @param label the label of the parameter, which will be {@link NLS#smartGet(String) auto translated}
+     */
+    public AmountParameter(String name, String label) {
+        super(name, label);
+    }
+
+    @Override
+    public String getTemplateName() {
+        return "/templates/biz/jobs/params/textfield.html.pasta";
+    }
+
+    @Override
+    protected String checkAndTransformValue(Value input) {
+        Amount value = NLS.parseUserString(Amount.class, input.asString());
+        return NLS.toMachineString(value);
+    }
+
+    @Override
+    protected Optional<Amount> resolveFromString(Value input) {
+        return Optional.of(input.getAmount());
+    }
+}
