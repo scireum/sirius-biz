e53a1d684f02e290d6ac8d6a3f0368c14d94bfa6
aha <aha@scireum.de>
2019-04-11 13:34:32 +0200

    Provides the distributed Checks Framework.

---
+++ src/main/java/sirius/biz/analytics/checks/SQLChangeCheckScheduler.java
@@ -0,0 +1,72 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.analytics.checks;
+
+import sirius.biz.analytics.scheduler.AnalyticsBatchExecutor;
+import sirius.biz.analytics.scheduler.AnalyticsScheduler;
+import sirius.biz.analytics.scheduler.AnalyticsSchedulerExecutor;
+import sirius.biz.analytics.scheduler.SQLAnalyticalTaskScheduler;
+import sirius.biz.analytics.scheduler.ScheduleInterval;
+import sirius.biz.protocol.TraceData;
+import sirius.biz.protocol.Traced;
+import sirius.db.jdbc.OMA;
+import sirius.db.jdbc.SQLEntity;
+import sirius.db.jdbc.SmartQuery;
+import sirius.kernel.di.std.Register;
+
+import javax.annotation.Nonnull;
+import java.time.LocalDateTime;
+
+/**
+ * Provides the scheduler which is in charge of creating and executing the batches of JDBC entities for which
+ * {@link ChangeCheck change checks} are present.
+ */
+@Register(classes = AnalyticsScheduler.class)
+public class SQLChangeCheckScheduler extends SQLAnalyticalTaskScheduler {
+
+    @Override
+    protected Class<?> getAnalyticalTaskType() {
+        return ChangeCheck.class;
+    }
+
+    @Override
+    public Class<? extends AnalyticsSchedulerExecutor> getExecutorForScheduling() {
+        return CheckSchedulerExecutor.class;
+    }
+
+    @Override
+    public Class<? extends AnalyticsBatchExecutor> getExecutorForTasks() {
+        return CheckBatchExecutor.class;
+    }
+
+    @Override
+    protected <E extends SQLEntity> void extendBatchQuery(SmartQuery<E> query) {
+        if (!Traced.class.isAssignableFrom(query.getDescriptor().getType())) {
+            throw new IllegalArgumentException("Entitis for which 'ChangeChecks' are created must implement 'Traced'.");
+        }
+
+        query.where(OMA.FILTERS.gt(Traced.TRACE.inner(TraceData.CHANGED_AT), LocalDateTime.now().minusDays(1)));
+    }
+
+    @Override
+    public boolean useBestEffortScheduling() {
+        return true;
+    }
+
+    @Override
+    public ScheduleInterval getInterval() {
+        return ScheduleInterval.DAILY;
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "sql-change-checks";
+    }
+}
--- src/main/java/sirius/biz/protocol/Traced.java
+++ src/main/java/sirius/biz/protocol/Traced.java
@@ -8,12 +8,22 @@
 
 package sirius.biz.protocol;
 
+import sirius.db.mixing.Mapping;
+import sirius.kernel.commons.Explain;
+
 /**
  * Marks an entity as traced.
  * <p>
  * A traced entity provides a {@link TraceData}.
  */
+@SuppressWarnings("squid:S1214")
+@Explain("The constant is best kept here for consistency reasons.")
 public interface Traced {
+
+    /**
+     * Provides the default mapping for accessing the trace data.
+     */
+    Mapping TRACE = Mapping.named("trace");
 
     /**
      * Returns tracing data which records which user created and last edited the entity
