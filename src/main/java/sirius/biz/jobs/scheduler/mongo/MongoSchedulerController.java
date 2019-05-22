ca3f782a6cf3effb95ccb21e7f6c9b98b85cdb1b
aha <aha@scireum.de>
2019-05-22 17:11:19 +0200

    Provides a MongoDB based job scheduler.

---
+++ src/main/java/sirius/biz/jobs/scheduler/mongo/MongoSchedulerController.java
@@ -0,0 +1,40 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.jobs.scheduler.mongo;
+
+import sirius.biz.jobs.scheduler.JobConfigData;
+import sirius.biz.jobs.scheduler.SchedulerController;
+import sirius.biz.web.BasePageHelper;
+import sirius.biz.web.MongoPageHelper;
+import sirius.kernel.di.std.Register;
+import sirius.web.controller.Controller;
+
+/**
+ * Provides the controller for managing the MongoDB based scheduler.
+ */
+@Register(classes = Controller.class, framework = MongoSchedulerController.FRAMEWORK_SCHEDULER_MONGO)
+public class MongoSchedulerController extends SchedulerController<MongoSchedulerEntry> {
+
+    /**
+     * Names the framework which must be enabled to activate the scheduling for MongoDB based entries.
+     */
+    public static final String FRAMEWORK_SCHEDULER_MONGO = "biz.scheduler-mongo";
+
+    @Override
+    protected Class<MongoSchedulerEntry> getEntryType() {
+        return MongoSchedulerEntry.class;
+    }
+
+    @Override
+    protected BasePageHelper<MongoSchedulerEntry, ?, ?, ?> getEntriesAsPage() {
+        return MongoPageHelper.withQuery(tenants.forCurrentTenant(mango.select(MongoSchedulerEntry.class)
+                                                                       .orderAsc(MongoSchedulerEntry.JOB_CONFIG_DATA.inner(
+                                                                               JobConfigData.JOB))));
+    }
+}
---
+++ src/main/java/sirius/biz/jobs/scheduler/mongo/MongoSchedulerEntryProvider.java
@@ -0,0 +1,63 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.jobs.scheduler.mongo;
+
+import sirius.biz.jobs.scheduler.SchedulerData;
+import sirius.biz.jobs.scheduler.SchedulerEntryProvider;
+import sirius.db.mongo.Finder;
+import sirius.db.mongo.Mango;
+import sirius.kernel.di.std.Part;
+import sirius.kernel.di.std.Register;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Supplies the {@link MongoSchedulerEntry SQL scheduler entries} to the {@link sirius.biz.jobs.scheduler.JobSchedulerLoop}.
+ */
+@Register(framework = MongoSchedulerController.FRAMEWORK_SCHEDULER_MONGO)
+public class MongoSchedulerEntryProvider implements SchedulerEntryProvider<MongoSchedulerEntry> {
+
+    @Part
+    private Mango mango;
+
+    @Override
+    public List<MongoSchedulerEntry> getActiveScheduledJobs() {
+        return mango.select(MongoSchedulerEntry.class)
+                    .fields(MongoSchedulerEntry.ID,
+                            MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.ENABLED),
+                            MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.YEAR),
+                            MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.MONTH),
+                            MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.DAY_OF_MONTH),
+                            MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.DAY_OF_WEEK),
+                            MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.HOUR_OF_DAY),
+                            MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.MINUTE),
+                            MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.LAST_EXECUTION),
+                            MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.NUMBER_OF_EXECUTIONS),
+                            MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.RUNS),
+                            MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.LAST_EXECUTION))
+                    .eq(MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.ENABLED), true)
+                    .where(Finder.FILTERS.or(Finder.FILTERS.ne(MongoSchedulerEntry.SCHEDULER_DATA.inner(SchedulerData.RUNS),
+                                                               0),
+                                             Finder.FILTERS.notFilled(MongoSchedulerEntry.SCHEDULER_DATA.inner(
+                                                     SchedulerData.RUNS))))
+                    .queryList();
+    }
+
+    @Override
+    public MongoSchedulerEntry fetchFullInformation(MongoSchedulerEntry job) {
+        return mango.tryRefresh(job);
+    }
+
+    @Override
+    public void markExecuted(MongoSchedulerEntry job, LocalDateTime timestamp) {
+        job.getSchedulerData().rememberExecution(timestamp);
+        mango.update(job);
+    }
+}
