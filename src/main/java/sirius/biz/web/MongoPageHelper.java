7ab7c21c7d801acd43a9cae2615b6411e8a8a13e
aha <aha@scireum.de>
2019-06-04 18:56:33 +0200

    Adds properly computed aggregations to the MongoPageHelper.

--- src/main/java/sirius/biz/web/MongoPageHelper.java
+++ src/main/java/sirius/biz/web/MongoPageHelper.java
@@ -8,11 +8,22 @@
 
 package sirius.biz.web;
 
+import sirius.db.mixing.Mapping;
 import sirius.db.mongo.Mongo;
 import sirius.db.mongo.MongoEntity;
 import sirius.db.mongo.MongoQuery;
 import sirius.db.mongo.constraints.MongoConstraint;
+import sirius.db.mongo.facets.MongoTermFacet;
+import sirius.kernel.cache.ValueComputer;
+import sirius.kernel.commons.Strings;
+import sirius.kernel.commons.Tuple;
+import sirius.kernel.commons.Watch;
 import sirius.kernel.di.std.Part;
+import sirius.web.controller.Facet;
+import sirius.web.controller.Page;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Implements a page helper for {@link MongoQuery MongoDB queries}.
@@ -30,6 +41,81 @@ public class MongoPageHelper<E extends MongoEntity>
     }
 
     /**
+     * Adds a automatic facet for values in the given field.
+     *
+     * @param field the field to aggregate on
+     * @return the helper itself for fluent method calls
+     */
+    public MongoPageHelper<E> addTermAggregation(Mapping field) {
+        return addTermAggregation(baseQuery.getDescriptor().findProperty(field.toString()).getLabel(), field, null);
+    }
+
+    /**
+     * Adds a automatic facet for values in the given field.
+     *
+     * @param field      the field to aggregate on
+     * @param translator the translator used to convert field values into filter labels
+     * @return the helper itself for fluent method calls
+     */
+    public MongoPageHelper<E> addTermAggregation(Mapping field, ValueComputer<String, String> translator) {
+        return addTermAggregation(baseQuery.getDescriptor().findProperty(field.toString()).getLabel(),
+                                  field,
+                                  translator);
+    }
+
+    /**
+     * Adds a automatic facet for values in the given field.
+     *
+     * @param field    the field to aggregate on
+     * @param enumType the type of enums in this field used for proper translation
+     * @return the helper itself for fluent method calls
+     */
+    public MongoPageHelper<E> addTermAggregation(Mapping field, Class<? extends Enum<?>> enumType) {
+        return addTermAggregation(baseQuery.getDescriptor().findProperty(field.toString()).getLabel(),
+                                  field,
+                                  value -> Arrays.stream(enumType.getEnumConstants())
+                                                 .filter(enumConst -> Strings.areEqual(enumConst.name(), value))
+                                                 .findFirst()
+                                                 .map(Object::toString)
+                                                 .orElse(value));
+    }
+
+    /**
+     * Adds a automatic facet for values in the given field.
+     *
+     * @param title      the title to use for the facet
+     * @param field      the field to aggregate on
+     * @param translator the translator used to convert field values into filter labels
+     * @return the helper itself for fluent method calls
+     */
+    public MongoPageHelper<E> addTermAggregation(String title,
+                                                 Mapping field,
+                                                 ValueComputer<String, String> translator) {
+        ValueComputer<String, String> nonNullTranslator = translator == null ? key -> key : translator;
+
+        Facet facet = new Facet(title, field.toString(), null, translator);
+        addFilterFacet(facet);
+
+        baseQuery.addFacet(new MongoTermFacet(field).onComplete(mongoFacet -> {
+            if (mongoFacet.getValues().isEmpty()) {
+                // If we didn't find any aggregation value we have to check if a filter for this
+                // facet is active and artificially create an "0" item for this so that it can be
+                // disabled...
+                String filterValue = ctx.get(facet.getName()).asString();
+                if (Strings.isFilled(filterValue)) {
+                    facet.addItem(filterValue, nonNullTranslator.compute(filterValue), 0);
+                }
+            } else {
+                for (Tuple<String, Integer> value : mongoFacet.getValues()) {
+                    facet.addItem(value.getFirst(), nonNullTranslator.compute(value.getFirst()), value.getSecond());
+                }
+            }
+        }));
+
+        return this;
+    }
+
+    /**
      * Creates a new instance with the given base query.
      *
      * @param baseQuery the initial query to execute
@@ -38,5 +124,11 @@ public class MongoPageHelper<E extends MongoEntity>
      */
     public static <E extends MongoEntity> MongoPageHelper<E> withQuery(MongoQuery<E> baseQuery) {
         return new MongoPageHelper<>(baseQuery);
+    }
+
+    @Override
+    protected void fillPage(Watch w, Page<E> result, List<E> items) {
+        baseQuery.executeFacets();
+        super.fillPage(w, result, items);
     }
 }
