0e577d3ea793360577a21f2d2f7588d7e6ac29f9
aha <aha@scireum.de>
2019-08-01 17:44:31 +0200

    Adds some javadocs.

--- src/main/java/sirius/biz/jobs/infos/JobInfo.java
+++ src/main/java/sirius/biz/jobs/infos/JobInfo.java
@@ -8,8 +8,15 @@
 
 package sirius.biz.jobs.infos;
 
+/**
+ * Represents a block or piece of documentation for a {@link sirius.biz.jobs.JobFactory job}.
+ */
 public interface JobInfo {
 
+    /**
+     * Determines which template to use to render this block.
+     *
+     * @return the name / path of the template to use
+     */
     String getTemplateName();
-
 }
