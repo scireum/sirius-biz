e36e5ae8bc4194553fca373f965b3f73ce5ac4cb
Fabian Welz <fwe@scireum.de>
2019-08-02 17:05:03 +0200

    Translate permission when reporting a missing permission

--- src/main/java/sirius/biz/tenants/BizInterceptor.java
+++ src/main/java/sirius/biz/tenants/BizInterceptor.java
@@ -8,6 +8,8 @@
 
 package sirius.biz.tenants;
 
+import sirius.kernel.async.CallContext;
+import sirius.kernel.di.std.ConfigValue;
 import sirius.kernel.di.std.Register;
 import sirius.kernel.nls.NLS;
 import sirius.web.controller.Controller;
@@ -17,12 +19,19 @@ import sirius.web.security.ScopeInfo;
 import sirius.web.security.UserContext;
 
 import java.lang.reflect.Method;
+import java.util.List;
 
 /**
  * Redirects unauthorized calls to the wondergem login page (for the default scope).
  */
 @Register(framework = Tenants.FRAMEWORK_TENANTS)
 public class BizInterceptor implements Interceptor {
+
+    @ConfigValue("security.roles")
+    protected List<String> roles;
+
+    @ConfigValue("security.tenantPermissions")
+    protected List<String> tenantPermissions;
 
     @Override
     public boolean before(WebContext ctx, boolean jsonCall, Controller controller, Method method) throws Exception {
@@ -46,9 +55,21 @@ public class BizInterceptor implements Interceptor {
         } else {
             ctx.respondWith()
                .template("/templates/wondergem/error.html.pasta",
-                         NLS.fmtr("BizInterceptor.missingPermission").set("permission", permission).format());
+                         NLS.fmtr("BizInterceptor.missingPermission")
+                            .set("permission", getTranslatedPermission(permission))
+                            .format());
         }
         return true;
+    }
+
+    private String getTranslatedPermission(String permission) {
+        if (roles.contains(permission)) {
+            return NLS.get("Role." + permission);
+        }
+        if (tenantPermissions.contains(permission)) {
+            return NLS.get("TenantPermission." + permission);
+        }
+        return NLS.getIfExists("Permission." + permission, CallContext.getCurrent().getLang()).orElse(permission);
     }
 
     @Override
