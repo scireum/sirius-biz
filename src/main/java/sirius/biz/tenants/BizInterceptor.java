14b5664e9e85aac652832c11d2479d61681da1fc
Fabian Welz <fwe@scireum.de>
2019-08-05 15:10:35 +0200

    Use Permissions#getTranslatedPermission to get translated permissions

--- src/main/java/sirius/biz/tenants/BizInterceptor.java
+++ src/main/java/sirius/biz/tenants/BizInterceptor.java
@@ -8,30 +8,22 @@
 
 package sirius.biz.tenants;
 
-import sirius.kernel.async.CallContext;
-import sirius.kernel.di.std.ConfigValue;
 import sirius.kernel.di.std.Register;
 import sirius.kernel.nls.NLS;
 import sirius.web.controller.Controller;
 import sirius.web.controller.Interceptor;
 import sirius.web.http.WebContext;
+import sirius.web.security.Permissions;
 import sirius.web.security.ScopeInfo;
 import sirius.web.security.UserContext;
 
 import java.lang.reflect.Method;
-import java.util.List;
 
 /**
  * Redirects unauthorized calls to the wondergem login page (for the default scope).
  */
 @Register(framework = Tenants.FRAMEWORK_TENANTS)
 public class BizInterceptor implements Interceptor {
-
-    @ConfigValue("security.roles")
-    protected List<String> roles;
-
-    @ConfigValue("security.tenantPermissions")
-    protected List<String> tenantPermissions;
 
     @Override
     public boolean before(WebContext ctx, boolean jsonCall, Controller controller, Method method) throws Exception {
@@ -56,20 +48,10 @@ public class BizInterceptor implements Interceptor {
             ctx.respondWith()
                .template("/templates/wondergem/error.html.pasta",
                          NLS.fmtr("BizInterceptor.missingPermission")
-                            .set("permission", getTranslatedPermission(permission))
+                            .set("permission", Permissions.getTranslatedPermission(permission))
                             .format());
         }
         return true;
-    }
-
-    private String getTranslatedPermission(String permission) {
-        if (roles.contains(permission)) {
-            return NLS.get("Role." + permission);
-        }
-        if (tenantPermissions.contains(permission)) {
-            return NLS.get("TenantPermission." + permission);
-        }
-        return NLS.getIfExists("Permission." + permission, CallContext.getCurrent().getLang()).orElse(permission);
     }
 
     @Override
