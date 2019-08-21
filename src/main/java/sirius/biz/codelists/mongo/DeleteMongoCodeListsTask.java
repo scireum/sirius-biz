2c7e43f1492159c95dc807b1a2eaabbab4afa78d
Fabian Welz <fwe@scireum.de>
2019-08-21 16:44:07 +0200

    Introduce an abstract class to simplify the deletion of MongoEntities

--- src/main/java/sirius/biz/codelists/mongo/DeleteMongoCodeListsTask.java
+++ src/main/java/sirius/biz/codelists/mongo/DeleteMongoCodeListsTask.java
@@ -9,46 +9,20 @@
 package sirius.biz.codelists.mongo;
 
 import sirius.biz.codelists.CodeList;
-import sirius.biz.process.ProcessContext;
-import sirius.biz.process.logs.ProcessLog;
-import sirius.biz.tenants.Tenant;
-import sirius.biz.tenants.deletion.DeleteTenantJobFactory;
+import sirius.biz.mongo.MongoBizEntity;
+import sirius.biz.tenants.deletion.DeleteMongoEntitiesTask;
 import sirius.biz.tenants.deletion.DeleteTenantTask;
-import sirius.biz.web.TenantAware;
-import sirius.db.mongo.Mango;
-import sirius.db.mongo.MongoQuery;
-import sirius.kernel.commons.Watch;
-import sirius.kernel.di.std.Part;
 import sirius.kernel.di.std.Register;
 
 /**
  * Deletes all {@link CodeList code lists} of the given tenant.
  */
-@Register(framework = MongoCodeLists.FRAMEWORK_CODE_LISTS_MONGO)
-public class DeleteMongoCodeListsTask implements DeleteTenantTask {
-
-    @Part
-    private Mango mango;
-
-    private MongoQuery<MongoCodeList> getQuery(Tenant<?> tenant) {
-        return mango.select(MongoCodeList.class).eq(TenantAware.TENANT, tenant);
-    }
+@Register(classes = DeleteTenantTask.class, framework = MongoCodeLists.FRAMEWORK_CODE_LISTS_MONGO)
+public class DeleteMongoCodeListsTask extends DeleteMongoEntitiesTask {
 
     @Override
-    public void beforeExecution(ProcessContext process, Tenant<?> tenant, boolean simulate) {
-        long codeListCount = getQuery(tenant).count();
-        process.log(ProcessLog.info()
-                              .withNLSKey("DeleteCodeListsTask.beforeExecution")
-                              .withContext("count", codeListCount));
-    }
-
-    @Override
-    public void execute(ProcessContext process, Tenant<?> tenant) throws Exception {
-        getQuery(tenant).iterateAll(entry -> {
-            Watch watch = Watch.start();
-            mango.delete(entry);
-            process.addTiming(DeleteTenantJobFactory.TIMING_DELETED_ITEMS, watch.elapsedMillis());
-        });
+    protected Class<? extends MongoBizEntity> getEntityClass() {
+        return MongoCodeList.class;
     }
 
     @Override
---
+++ src/main/java/sirius/biz/tenants/deletion/DeleteMongoEntitiesTask.java
@@ -0,0 +1,64 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.tenants.deletion;
+
+import sirius.biz.mongo.MongoBizEntity;
+import sirius.biz.process.ProcessContext;
+import sirius.biz.process.logs.ProcessLog;
+import sirius.biz.tenants.Tenant;
+import sirius.biz.web.TenantAware;
+import sirius.db.mixing.Mixing;
+import sirius.db.mongo.Mango;
+import sirius.db.mongo.MongoQuery;
+import sirius.kernel.commons.Watch;
+import sirius.kernel.di.std.Part;
+
+/**
+ * Provides basic functionality to delete a list of MongoEntities of a tenant.
+ */
+public abstract class DeleteMongoEntitiesTask implements DeleteTenantTask {
+
+    @Part
+    protected Mango mango;
+
+    @Part
+    protected Mixing mixing;
+
+    @Override
+    public void beforeExecution(ProcessContext processContext, Tenant<?> tenant) {
+        processContext.log(ProcessLog.info()
+                                     .withNLSKey("DeleteTenantTask.beforeExecution")
+                                     .withContext("count", getQuery(tenant).count())
+                                     .withContext("name", getEntityName()));
+    }
+
+    @Override
+    public void execute(ProcessContext processContext, Tenant<?> tenant) {
+        getQuery(tenant).iterateAll(entity -> {
+            Watch watch = Watch.start();
+            mango.forceDelete(entity);
+            processContext.addTiming(DeleteTenantJobFactory.TIMING_DELETED_ITEMS, watch.elapsedMillis());
+        });
+    }
+
+    protected MongoQuery<? extends MongoBizEntity> getQuery(Tenant<?> tenant) {
+        return mango.select(getEntityClass()).eq(TenantAware.TENANT, tenant);
+    }
+
+    protected abstract Class<? extends MongoBizEntity> getEntityClass();
+
+    protected String getEntityName() {
+        return mixing.getDescriptor(getEntityClass()).getPluralLabel();
+    }
+
+    @Override
+    public int getPriority() {
+        return 100;
+    }
+}
