86b03f4e54a220debdeec2bb538e9be4f50d5bc7
aha <aha@scireum.de>
2019-10-07 17:59:20 +0200

    Adds some documentation.

--- src/main/java/sirius/biz/jobs/BasicJobFactory.java
+++ src/main/java/sirius/biz/jobs/BasicJobFactory.java
@@ -50,6 +50,16 @@ public abstract class BasicJobFactory implements JobFactory {
      */
     public static final String SYSTEM_JOBS = "JOBS";
 
+    /**
+     * Represents a special paramete which can be set when submitting a set of job parameters to
+     * {@link #startInteractively(WebContext)}. This will only digest the given parameters but not
+     * actually try to start the job.
+     * <p>
+     * This is used to pass in the parameters stored in a job preset - this way all values are
+     * rendered nicely.
+     */
+    private static final String PARAM_UPDATE_ONLY = "updateOnly";
+
     @Part
     private JobPresets presets;
 
@@ -173,7 +183,7 @@ public abstract class BasicJobFactory implements JobFactory {
         checkPermissions();
         setupTaskContext();
 
-        AtomicBoolean submit = new AtomicBoolean(request.isSafePOST() && !request.get("updateOnly").asBoolean());
+        AtomicBoolean submit = new AtomicBoolean(request.isSafePOST() && !request.get(PARAM_UPDATE_ONLY).asBoolean());
         Map<String, String> context = buildAndVerifyContext(request::get, submit.get(), error -> {
             UserContext.message(Message.error(error));
             submit.set(false);
