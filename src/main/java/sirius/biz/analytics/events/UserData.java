5da392cfe058d5a75db746882c5bbda532a2a5dc
aha <aha@scireum.de>
2019-10-16 23:08:56 +0200

    Improves the way custom field values are handled.
    If a custom value is present, it will not be overwritten by the BeforeSave handlers.
    Also improved the method names to ensure that the methods aren't invoked
    accidentally.

--- src/main/java/sirius/biz/analytics/events/UserData.java
+++ src/main/java/sirius/biz/analytics/events/UserData.java
@@ -13,6 +13,7 @@ import sirius.db.mixing.annotations.BeforeSave;
 import sirius.db.mixing.annotations.Length;
 import sirius.db.mixing.annotations.NullAllowed;
 import sirius.kernel.async.CallContext;
+import sirius.kernel.commons.Strings;
 import sirius.web.security.ScopeInfo;
 import sirius.web.security.UserContext;
 
@@ -44,13 +45,49 @@ public class UserData extends Composite {
     @BeforeSave
     protected void fill() {
         UserContext ctx = CallContext.getCurrent().get(UserContext.class);
-        if (ctx.getScope() != ScopeInfo.DEFAULT_SCOPE) {
+        if (ctx.getScope() != ScopeInfo.DEFAULT_SCOPE && Strings.isEmpty(scopeId)) {
             scopeId = ctx.getScope().getScopeId();
         }
-        if (ctx.getUser().isLoggedIn()) {
+        if (ctx.getUser().isLoggedIn() && Strings.isEmpty(userId)) {
             userId = ctx.getUser().getUserId();
             tenantId = ctx.getUser().getTenantId();
         }
+    }
+
+    /**
+     * Specifies the user id to record.
+     * <p>
+     * In most cases this method shouldn't be called manually as the event will initialize this field with
+     * the current user (as indicated by the {@link UserContext}).
+     *
+     * @param userId the user id to store
+     */
+    public void setCustomUserId(String userId) {
+        this.userId = userId;
+    }
+
+    /**
+     * Specifies the tenant id to record.
+     * <p>
+     * In most cases this method shouldn't be called manually as the event will initialize this field with
+     * the current tenant (as indicated by the {@link UserContext}).
+     *
+     * @param tenantId the tenant id to store
+     */
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    /**
+     * Specifies the scope id to record.
+     * <p>
+     * In most cases this method shouldn't be called manually as the event will initialize this field with
+     * the current scope (as indicated by the {@link UserContext}).
+     *
+     * @param scopeId the scope id to store
+     */
+    public void setScopeId(String scopeId) {
+        this.scopeId = scopeId;
     }
 
     public String getUserId() {
@@ -63,17 +100,5 @@ public class UserData extends Composite {
 
     public String getScopeId() {
         return scopeId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public void setTenantId(String tenantId) {
-        this.tenantId = tenantId;
-    }
-
-    public void setScopeId(String scopeId) {
-        this.scopeId = scopeId;
     }
 }
