f94aed985505911b7f918d1c98643bd2e6ccb6ff
aha <aha@scireum.de>
2019-11-13 23:47:45 +0100

    Provides some helper classes to simplify satisfying the VFS API.

---
+++ src/main/java/sirius/biz/storage/layer3/FindOnlyProvider.java
@@ -0,0 +1,41 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.storage.layer3;
+
+import java.util.Optional;
+import java.util.function.BiFunction;
+
+/**
+ * Represents a {@link ChildProvider} which can only resolve files / direcoties but never enumerate them.
+ * <p>
+ * This is useful for virtual folders which accept (non-exisiting) child files, which are then processed elsewhere.
+ */
+public class FindOnlyProvider implements ChildProvider {
+
+    private BiFunction<VirtualFile, String, Optional<VirtualFile>> resolver;
+
+    /**
+     * Creates a new provider which delegates the find call to the given resolver.
+     *
+     * @param resolver the actual resolver which tries to find the requested file
+     */
+    public FindOnlyProvider(BiFunction<VirtualFile, String, Optional<VirtualFile>> resolver) {
+        this.resolver = resolver;
+    }
+
+    @Override
+    public Optional<VirtualFile> findChild(VirtualFile parent, String name) {
+        return resolver.apply(parent, name);
+    }
+
+    @Override
+    public void enumerate(VirtualFile parent, FileSearch search) {
+        // Intentionally left empty...
+    }
+}
