4e938ab3ca223ecf4aa33da182d3eaa95cff64f1
aha <aha@scireum.de>
2019-11-28 14:54:58 +0100

    Provides export jobs for code lists.
    Fixes: SIRI-117

---
+++ src/main/java/sirius/biz/codelists/jdbc/SQLCodeListExportJobFactory.java
@@ -0,0 +1,68 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.codelists.jdbc;
+
+import sirius.biz.codelists.CodeListController;
+import sirius.biz.codelists.CodeListEntry;
+import sirius.biz.codelists.mongo.MongoCodeList;
+import sirius.biz.jobs.JobFactory;
+import sirius.biz.jobs.batch.file.EntityExportJobFactory;
+import sirius.biz.jobs.params.CodeListParameter;
+import sirius.biz.jobs.params.Parameter;
+import sirius.biz.process.ProcessContext;
+import sirius.db.jdbc.SmartQuery;
+import sirius.kernel.di.std.Register;
+import sirius.web.security.Permission;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.function.Consumer;
+
+/**
+ * Provides an export for entries of a {@link sirius.biz.codelists.jdbc.SQLCodeList}.
+ */
+@Register(classes = JobFactory.class, framework = SQLCodeLists.FRAMEWORK_CODE_LISTS_JDBC)
+@Permission(CodeListController.PERMISSION_MANAGE_CODELISTS)
+public class SQLCodeListExportJobFactory
+        extends EntityExportJobFactory<SQLCodeListEntry, SmartQuery<SQLCodeListEntry>> {
+
+    private CodeListParameter codeListParameter = new CodeListParameter("codeList", "$CodeList").markRequired();
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "export-sql-code-list-entries";
+    }
+
+    @Override
+    protected Class<SQLCodeListEntry> getExportType() {
+        return SQLCodeListEntry.class;
+    }
+
+    @Override
+    protected void collectParameters(Consumer<Parameter<?, ?>> parameterCollector) {
+        parameterCollector.accept(codeListParameter);
+        super.collectParameters(parameterCollector);
+    }
+
+    @Override
+    protected void extendSelectQuery(SmartQuery<SQLCodeListEntry> query, ProcessContext processContext) {
+        query.eq(CodeListEntry.CODE_LIST, processContext.require(codeListParameter));
+    }
+
+    @Override
+    protected boolean hasPresetFor(Object targetObject) {
+        return targetObject instanceof SQLCodeList;
+    }
+
+    @Override
+    protected void computePresetFor(Object targetObject, Map<String, Object> preset) {
+        preset.put(codeListParameter.getName(), ((SQLCodeList) targetObject).getId());
+    }
+}
---
+++ src/main/java/sirius/biz/codelists/mongo/MongoCodeListExportJobFactory.java
@@ -0,0 +1,68 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.codelists.mongo;
+
+import sirius.biz.codelists.CodeListController;
+import sirius.biz.codelists.CodeListEntry;
+import sirius.biz.codelists.jdbc.SQLCodeListEntry;
+import sirius.biz.jobs.JobFactory;
+import sirius.biz.jobs.batch.file.EntityExportJobFactory;
+import sirius.biz.jobs.params.CodeListParameter;
+import sirius.biz.jobs.params.Parameter;
+import sirius.biz.process.ProcessContext;
+import sirius.db.jdbc.SmartQuery;
+import sirius.db.mongo.MongoQuery;
+import sirius.kernel.di.std.Register;
+import sirius.web.security.Permission;
+
+import javax.annotation.Nonnull;
+import java.util.Map;
+import java.util.function.Consumer;
+
+/**
+ * Provides an export for entries of a {@link sirius.biz.codelists.mongo.MongoCodeList}.
+ */
+@Register(classes = JobFactory.class, framework = MongoCodeLists.FRAMEWORK_CODE_LISTS_MONGO)
+@Permission(CodeListController.PERMISSION_MANAGE_CODELISTS)
+public class MongoCodeListExportJobFactory extends EntityExportJobFactory<MongoCodeListEntry, MongoQuery<MongoCodeListEntry>> {
+
+    private CodeListParameter codeListParameter = new CodeListParameter("codeList", "$CodeList").markRequired();
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "export-mongo-code-list-entries";
+    }
+
+    @Override
+    protected Class<MongoCodeListEntry> getExportType() {
+        return MongoCodeListEntry.class;
+    }
+
+    @Override
+    protected void collectParameters(Consumer<Parameter<?, ?>> parameterCollector) {
+        parameterCollector.accept(codeListParameter);
+        super.collectParameters(parameterCollector);
+    }
+
+    @Override
+    protected void extendSelectQuery(MongoQuery<MongoCodeListEntry> query, ProcessContext processContext) {
+        query.eq(CodeListEntry.CODE_LIST, processContext.require(codeListParameter));
+    }
+
+    @Override
+    protected boolean hasPresetFor(Object targetObject) {
+        return targetObject instanceof MongoCodeList;
+    }
+
+    @Override
+    protected void computePresetFor(Object targetObject, Map<String, Object> preset) {
+        preset.put(codeListParameter.getName(), ((MongoCodeList) targetObject).getId());
+    }
+}
