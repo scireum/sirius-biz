c32b893927789f6c4e7edc4c1643a098a67eb77e
Tobias Bischoff <tbi@scireum.de>
2019-12-08 10:03:39 +0100

    Sets the user to run the rehashing logic to ensure that a tenant is set
    the personData.beforeSave logic requires a codelist check. This requires a tenant
    During the rehashing process we don't yet have access to getCurrentUser which is required.

--- src/main/java/sirius/biz/tenants/TenantUserManager.java
+++ src/main/java/sirius/biz/tenants/TenantUserManager.java
@@ -23,6 +23,7 @@ import sirius.kernel.cache.CacheManager;
 import sirius.kernel.commons.Explain;
 import sirius.kernel.commons.Strings;
 import sirius.kernel.commons.Tuple;
+import sirius.kernel.commons.ValueHolder;
 import sirius.kernel.di.PartCollection;
 import sirius.kernel.di.std.Part;
 import sirius.kernel.di.std.Parts;
@@ -591,7 +592,12 @@ public abstract class TenantUserManager<I, T extends BaseEntity<I> & Tenant<I>, 
             completeAuditLogForUser(auditLog.neutral("AuditLog.passwordLogin"), account);
 
             if (pwResult == LoginData.PasswordVerificationResult.VALID_NEEDS_RE_HASH) {
-                return updatePasswordHashing(result, password);
+                ValueHolder<UserInfo> rehashingResult = new ValueHolder<>(null);
+                UserContext.get().runAs(result, () -> {
+                    rehashingResult.set(updatePasswordHashing(result, password));
+                });
+
+                return rehashingResult.get();
             }
 
             return result;
