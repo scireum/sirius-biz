3fc1c4bfa50935b141bab0429dccf4b16fb54262
aha <aha@scireum.de>
2020-01-27 13:16:04 +0100

    BREAKING CHANGE: Makes the required check aware of the automatic trim performed by most of the frameworks around it.
    This is a breaking change as we kind of flipped the default. We now trim by
    default, which can be disabled - previously the check was conducted without
    trimming.

--- src/main/java/sirius/biz/importer/format/RequiredCheck.java
+++ src/main/java/sirius/biz/importer/format/RequiredCheck.java
@@ -8,19 +8,43 @@
 
 package sirius.biz.importer.format;
 
+import sirius.kernel.commons.Strings;
 import sirius.kernel.commons.Value;
 import sirius.kernel.nls.NLS;
 
 /**
  * Enforces the presence of a value for the associated field.
+ * <p>
+ * Note, as most of the {@link Value} methods will perform an automatic <tt>trim</tt>, we also trim the contents before
+ * checking for a non-empty value. (Thus " " would be considered empty and yield an error).
+ * <p>
+ * Use {@link #checkUntrimmed()} in order to suppress this behaviour.
  */
 public class RequiredCheck implements ValueCheck {
 
+    private boolean trim = true;
+
     @Override
     public void perform(Value value) {
-        if (value.isEmptyString()) {
+        String effectiveValue = value.asString();
+
+        if (trim) {
+            effectiveValue = effectiveValue.trim();
+        }
+
+        if (Strings.isEmpty(effectiveValue)) {
             throw new IllegalArgumentException(NLS.get("RequiredCheck.errorMsg"));
         }
+    }
+
+    /**
+     * Suppresses the automatic trim before checking if the given value is present.
+     *
+     * @return the check itself for fluent method calls
+     */
+    public RequiredCheck checkUntrimmed() {
+        this.trim = false;
+        return this;
     }
 
     @Override
