dd21d363ac2503de8ec0d624e4683f38d6cf965b
Idevaldo De Lira <ili@scireum.de>
2020-03-07 17:00:07 +0100

    Adds a test scenario for non-batch indicators

---
+++ src/test/java/sirius/biz/analytics/indicators/IndicatorTestEntity.java
@@ -0,0 +1,30 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.analytics.indicators;
+
+import sirius.db.mongo.MongoEntity;
+
+public class IndicatorTestEntity extends MongoEntity implements IndicatedEntity {
+
+    private String value;
+    private final IndicatorData indicatorData = new IndicatorData(this);
+
+    @Override
+    public IndicatorData getIndicators() {
+        return this.indicatorData;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
