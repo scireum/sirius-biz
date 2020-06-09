949b80dc122ed7b836b0e7ef91eeb008001d0041
aha <aha@scireum.de>
2020-06-09 11:06:30 +0200

    Removes prematurely committed code.

--- src/main/java/sirius/biz/storage/layer1/FSObjectStorageSpace.java
+++ src/main/java/sirius/biz/storage/layer1/FSObjectStorageSpace.java
@@ -27,17 +27,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.function.IntConsumer;
-import java.util.function.Predicate;
 
 /**
  * Provides a {@link ObjectStorageSpace} which operates on the local file system.
  */
-
 public class FSObjectStorageSpace extends ObjectStorageSpace {
 
     /**
@@ -257,22 +251,5 @@ public class FSObjectStorageSpace extends ObjectStorageSpace {
         }
 
         return null;
-    }
-
-    @Override
-    public void iterateObjects(Predicate<String> physicalKeyHandler) throws IOException {
-        java.nio.file.Files.walkFileTree(baseDir.toPath(), new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                if (file.toFile().isFile()) {
-                    if (physicalKeyHandler.test(file.toFile().getName())) {
-                        return FileVisitResult.CONTINUE;
-                    } else {
-                        return FileVisitResult.TERMINATE;
-                    }
-                }
-                return super.visitFile(file, attrs);
-            }
-        });
     }
 }
