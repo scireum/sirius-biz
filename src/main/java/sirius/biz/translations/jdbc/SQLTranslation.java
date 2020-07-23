ee5b15659b5975dd9aaf9a5176b86ff308634de4
Christian Schierle <csc@scireum.de>
2020-07-23 10:57:27 +0200

    Adds index
    Fixes: SIRI-228

--- src/main/java/sirius/biz/translations/jdbc/SQLTranslation.java
+++ src/main/java/sirius/biz/translations/jdbc/SQLTranslation.java
@@ -11,12 +11,16 @@ package sirius.biz.translations.jdbc;
 import sirius.biz.translations.Translation;
 import sirius.biz.translations.TranslationData;
 import sirius.db.jdbc.SQLEntity;
+import sirius.db.mixing.annotations.Index;
 
 /**
  * Stores translations as a table.
  * <p>
  * Note that translations should only be accessed via siblings of {@link sirius.biz.translations.BasicTranslations}.
  */
+@Index(name = "lookup",
+        columns = {"translationData_owner", "translationData_field", "translationData_lang"},
+        unique = true)
 public class SQLTranslation extends SQLEntity implements Translation {
     private final TranslationData translationData = new TranslationData();
 
--- src/main/java/sirius/biz/translations/mongo/MongoTranslation.java
+++ src/main/java/sirius/biz/translations/mongo/MongoTranslation.java
@@ -10,6 +10,8 @@ package sirius.biz.translations.mongo;
 
 import sirius.biz.translations.Translation;
 import sirius.biz.translations.TranslationData;
+import sirius.db.mixing.annotations.Index;
+import sirius.db.mongo.Mango;
 import sirius.db.mongo.MongoEntity;
 
 /**
@@ -17,6 +19,10 @@ import sirius.db.mongo.MongoEntity;
  * <p>
  * Note that translations should only be accessed via siblings of {@link sirius.biz.translations.BasicTranslations}.
  */
+@Index(name = "lookup",
+        columns = {"translationData_owner", "translationData_field", "translationData_lang"},
+        columnSettings = {Mango.INDEX_ASCENDING, Mango.INDEX_ASCENDING, Mango.INDEX_ASCENDING},
+        unique = true)
 public class MongoTranslation extends MongoEntity implements Translation {
     private final TranslationData translationData = new TranslationData();
 
