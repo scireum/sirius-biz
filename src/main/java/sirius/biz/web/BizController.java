6856c1830f9da880cb5428eb1e4982de944bccc0
Marc Egenrieder <meg@scireum.de>
2020-08-11 14:02:16 +0200

    Fixes errornous if condition

--- src/main/java/sirius/biz/web/BizController.java
+++ src/main/java/sirius/biz/web/BizController.java
@@ -267,7 +267,7 @@ public class BizController extends BasicController {
     private boolean tryLoadProperty(WebContext webContext, BaseEntity<?> entity, Property property) {
         String propertyName = property.getName();
 
-        if (webContext.hasParameter(propertyName)) {
+        if (!webContext.hasParameter(propertyName)) {
             // If the parameter is not present in the request we just skip it to prevent resetting the field to null
             return true;
         }
