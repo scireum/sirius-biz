bf5d3f6676aeac48181fd0eab1589fc0fed8fcd3
Christian Schierle <csc@scireum.de>
2020-08-21 16:26:27 +0200

    Creates translation entities when a language is selected during codelist import
    Fixes: SIRI-243

--- src/main/java/sirius/biz/codelists/mongo/MongoCodeListImportJobFactory.java
+++ src/main/java/sirius/biz/codelists/mongo/MongoCodeListImportJobFactory.java
@@ -8,20 +8,29 @@
 
 package sirius.biz.codelists.mongo;
 
+import sirius.biz.codelists.CodeList;
 import sirius.biz.codelists.CodeListController;
 import sirius.biz.codelists.CodeListEntry;
-import sirius.biz.importer.ImportContext;
+import sirius.biz.codelists.CodeListEntryData;
+import sirius.biz.codelists.CodeLists;
+import sirius.biz.importer.format.ImportDictionary;
+import sirius.biz.jobs.batch.file.EntityImportJob;
 import sirius.biz.jobs.batch.file.EntityImportJobFactory;
 import sirius.biz.jobs.params.CodeListParameter;
+import sirius.biz.jobs.params.LanguageParameter;
 import sirius.biz.jobs.params.Parameter;
 import sirius.biz.process.ProcessContext;
+import sirius.biz.storage.layer3.FileParameter;
 import sirius.db.mixing.BaseEntity;
+import sirius.kernel.commons.Context;
+import sirius.kernel.di.std.Part;
 import sirius.kernel.di.std.Register;
 import sirius.web.http.QueryString;
 import sirius.web.security.Permission;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
+import java.util.Optional;
 import java.util.function.Consumer;
 
 /**
@@ -30,6 +39,8 @@ import java.util.function.Consumer;
 @Register(framework = MongoCodeLists.FRAMEWORK_CODE_LISTS_MONGO)
 @Permission(CodeListController.PERMISSION_MANAGE_CODELISTS)
 public class MongoCodeListImportJobFactory extends EntityImportJobFactory {
+    @Part
+    private static CodeLists<?, ?, ?, ?> codeLists;
 
     /**
      * Contains the mongo code list to import the code list entries into.
@@ -69,11 +80,6 @@ public class MongoCodeListImportJobFactory extends EntityImportJobFactory {
     }
 
     @Override
-    protected void transferParameters(ImportContext context, ProcessContext processContext) {
-        context.set(CodeListEntry.CODE_LIST, processContext.require(codeListParameter));
-    }
-
-    @Override
     protected boolean hasPresetFor(QueryString queryString, Object targetObject) {
         return targetObject instanceof MongoCodeList;
     }
@@ -83,5 +89,61 @@ public class MongoCodeListImportJobFactory extends EntityImportJobFactory {
         preset.put(CODE_LIST_PARAMETER.getName(), ((MongoCodeList) targetObject).getCodeListData().getCode());
         queryString.get(LANGUAGE_PARAMETER.getName())
                    .ifFilled(value -> preset.put(LANGUAGE_PARAMETER.getName(), value));
+    }
+
+    private class CodeListEntryTranslationImportJob<E extends BaseEntity<?> & CodeListEntry<?, ?, ?>>
+            extends EntityImportJob<E> {
+
+        private CodeList codeList;
+        private Optional<String> language;
+
+        private CodeListEntryTranslationImportJob(FileParameter fileParameter,
+                                                  ImportDictionary dictionary,
+                                                  Class<E> type,
+                                                  ProcessContext process,
+                                                  CodeListParameter codeListParameter,
+                                                  LanguageParameter languageParameter,
+                                                  String factoryName) {
+            super(fileParameter, ignoreEmptyParameter, importModeParameter, type, dictionary, process, factoryName);
+            this.codeList = process.require(codeListParameter);
+            this.language = process.getParameter(LANGUAGE_PARAMETER);
+        }
+
+        @Override
+        protected E findAndLoad(Context data) {
+            data.set(CodeListEntry.CODE_LIST.getName(), process.require(CODE_LIST_PARAMETER));
+            return super.findAndLoad(data);
+        }
+
+        /**
+         * Checks, if the code of the given CodeListEntry is already in its parent CodeList.
+         * If yes, adds a new translation for the description of the existing CodeListEntry instead of creating a new entry.
+         * Continues with normal import execution otherwise.
+         *
+         * @param entity
+         * @return
+         */
+        @Override
+        protected boolean shouldSkip(E entity) {
+            boolean shouldSkip = super.shouldSkip(entity);
+
+            if (language.isPresent()) {
+                if (codeLists.getEntry(codeList.getCodeListData().getCode(), entity.getCodeListEntryData().getCode())
+                             .isPresent()) {
+                    // strip language code from text provided in LanguageParameter (e.g. "deu" from "Deutsch (deu)")
+                    String lang = language.get().split("\\(|\\)")[1];
+                    codeLists.getEntry(codeList.getCodeListData().getCode(), entity.getCodeListEntryData().getCode())
+                             .ifPresent(cle -> {
+                                 cle.getTranslations()
+                                    .updateText(CodeListEntry.CODE_LIST_ENTRY_DATA.inner(CodeListEntryData.DESCRIPTION),
+                                                lang,
+                                                entity.getCodeListEntryData().getDescription());
+                             });
+                    shouldSkip = true;
+                }
+            }
+
+            return shouldSkip;
+        }
     }
 }
