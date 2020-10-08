44ef4c525b3b9040ed9c87fdbc9ae608c7a1663d
Christian Schierle <csc@scireum.de>
2020-10-08 11:43:43 +0200

    Extracts common functionality to its own class
    Fixes: SIRI-243

--- src/main/java/sirius/biz/codelists/jdbc/SQLCodeListImportJobFactory.java
+++ src/main/java/sirius/biz/codelists/jdbc/SQLCodeListImportJobFactory.java
@@ -9,8 +9,8 @@
 package sirius.biz.codelists.jdbc;
 
 import sirius.biz.codelists.CodeListController;
-import sirius.biz.codelists.CodeListEntry;
-import sirius.biz.importer.ImportContext;
+import sirius.biz.codelists.TranslatableCodeListImportJob;
+import sirius.biz.jobs.batch.file.EntityImportJob;
 import sirius.biz.jobs.batch.file.EntityImportJobFactory;
 import sirius.biz.jobs.params.CodeListParameter;
 import sirius.biz.jobs.params.LanguageParameter;
@@ -31,11 +31,26 @@ import java.util.function.Consumer;
 @Register(framework = SQLCodeLists.FRAMEWORK_CODE_LISTS_JDBC)
 @Permission(CodeListController.PERMISSION_MANAGE_CODELISTS)
 public class SQLCodeListImportJobFactory extends EntityImportJobFactory {
-
+    /**
+     * Contains the SQL code list to import the code list entries into.
+     */
     private static final CodeListParameter CODE_LIST_PARAMETER =
             new CodeListParameter("codeList", "$CodeList").markRequired();
     private static final LanguageParameter LANGUAGE_PARAMETER =
             new LanguageParameter(LanguageParameter.PARAMETER_NAME, "$LocaleData.lang");
+
+    @Override
+    protected EntityImportJob<SQLCodeListEntry> createJob(ProcessContext process) {
+        return new TranslatableCodeListImportJob<>(fileParameter,
+                                                   ignoreEmptyParameter,
+                                                   importModeParameter,
+                                                   CODE_LIST_PARAMETER,
+                                                   LANGUAGE_PARAMETER,
+                                                   SQLCodeListEntry.class,
+                                                   getDictionary(),
+                                                   process,
+                                                   this.getClass().getName());
+    }
 
     @Nonnull
     @Override
@@ -53,12 +68,6 @@ public class SQLCodeListImportJobFactory extends EntityImportJobFactory {
         parameterCollector.accept(CODE_LIST_PARAMETER);
         parameterCollector.accept(LANGUAGE_PARAMETER);
         super.collectParameters(parameterCollector);
-    }
-
-    @Override
-    protected void transferParameters(ImportContext context, ProcessContext processContext) {
-        context.set(CodeListEntry.CODE_LIST, processContext.require(CODE_LIST_PARAMETER));
-        context.set(LanguageParameter.PARAMETER_NAME, LANGUAGE_PARAMETER);
     }
 
     @Override
--- src/main/java/sirius/biz/codelists/mongo/MongoCodeListImportJobFactory.java
+++ src/main/java/sirius/biz/codelists/mongo/MongoCodeListImportJobFactory.java
@@ -8,30 +8,21 @@
 
 package sirius.biz.codelists.mongo;
 
-import sirius.biz.codelists.CodeList;
 import sirius.biz.codelists.CodeListController;
-import sirius.biz.codelists.CodeListEntry;
-import sirius.biz.codelists.CodeListEntryData;
-import sirius.biz.codelists.CodeLists;
-import sirius.biz.importer.format.ImportDictionary;
+import sirius.biz.codelists.TranslatableCodeListImportJob;
 import sirius.biz.jobs.batch.file.EntityImportJob;
 import sirius.biz.jobs.batch.file.EntityImportJobFactory;
 import sirius.biz.jobs.params.CodeListParameter;
 import sirius.biz.jobs.params.LanguageParameter;
 import sirius.biz.jobs.params.Parameter;
 import sirius.biz.process.ProcessContext;
-import sirius.biz.storage.layer3.FileParameter;
 import sirius.db.mixing.BaseEntity;
-import sirius.kernel.commons.Context;
-import sirius.kernel.commons.Tuple;
-import sirius.kernel.di.std.Part;
 import sirius.kernel.di.std.Register;
 import sirius.web.http.QueryString;
 import sirius.web.security.Permission;
 
 import javax.annotation.Nonnull;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Consumer;
 
 /**
@@ -40,9 +31,6 @@ import java.util.function.Consumer;
 @Register(framework = MongoCodeLists.FRAMEWORK_CODE_LISTS_MONGO)
 @Permission(CodeListController.PERMISSION_MANAGE_CODELISTS)
 public class MongoCodeListImportJobFactory extends EntityImportJobFactory {
-    @Part
-    private static CodeLists<?, ?, ?> codeLists;
-
     /**
      * Contains the mongo code list to import the code list entries into.
      */
@@ -53,13 +41,15 @@ public class MongoCodeListImportJobFactory extends EntityImportJobFactory {
 
     @Override
     protected EntityImportJob<MongoCodeListEntry> createJob(ProcessContext process) {
-        return new CodeListEntryTranslationImportJob<>(fileParameter,
-                                                       getDictionary(),
-                                                       MongoCodeListEntry.class,
-                                                       process,
-                                                       CODE_LIST_PARAMETER,
-                                                       LANGUAGE_PARAMETER,
-                                                       this.getClass().getName());
+        return new TranslatableCodeListImportJob<>(fileParameter,
+                                                   ignoreEmptyParameter,
+                                                   importModeParameter,
+                                                   CODE_LIST_PARAMETER,
+                                                   LANGUAGE_PARAMETER,
+                                                   MongoCodeListEntry.class,
+                                                   getDictionary(),
+                                                   process,
+                                                   this.getClass().getName());
     }
 
     @Nonnull
@@ -90,68 +80,5 @@ public class MongoCodeListImportJobFactory extends EntityImportJobFactory {
         preset.put(CODE_LIST_PARAMETER.getName(), ((MongoCodeList) targetObject).getCodeListData().getCode());
         queryString.get(LANGUAGE_PARAMETER.getName())
                    .ifFilled(value -> preset.put(LANGUAGE_PARAMETER.getName(), value));
-    }
-
-    private class CodeListEntryTranslationImportJob<E extends BaseEntity<?> & CodeListEntry<?, ?, ?>>
-            extends EntityImportJob<E> {
-
-        private CodeList codeList;
-        private LanguageParameter languageParameter;
-        private Optional<String> language;
-
-        private CodeListEntryTranslationImportJob(FileParameter fileParameter,
-                                                  ImportDictionary dictionary,
-                                                  Class<E> type,
-                                                  ProcessContext process,
-                                                  CodeListParameter codeListParameter,
-                                                  LanguageParameter languageParameter,
-                                                  String factoryName) {
-            super(fileParameter, ignoreEmptyParameter, importModeParameter, type, dictionary, process, factoryName);
-            this.codeList = process.require(codeListParameter);
-            this.languageParameter = languageParameter;
-            this.language = process.getParameter(languageParameter);
-        }
-
-        @Override
-        protected E findAndLoad(Context data) {
-            data.set(CodeListEntry.CODE_LIST.getName(), process.require(CODE_LIST_PARAMETER));
-            return super.findAndLoad(data);
-        }
-
-        /**
-         * A new {@link CodeListEntry} only has to be created, if it is not yet present in the selected {@link CodeList}.
-         * <p>
-         * If a language is selected for the import, updates {@link sirius.biz.translations.Translation Translations}
-         * for the {@link CodeListEntry}'s description.
-         *
-         * @param entity  the entity to persist
-         * @param context the row represented as context
-         */
-        @Override
-        protected void createOrUpdate(E entity, Context context) {
-            if (!codeLists.getEntry(codeList.getCodeListData().getCode(), entity.getCodeListEntryData().getCode())
-                          .isPresent()) {
-                super.createOrUpdate(entity, context);
-            }
-            if (language.isPresent()) {
-                // get language code for selected language (e.g. "de" for "Deutsch (de)")
-                Optional<Tuple<String, String>> langCode = languageParameter.getValues()
-                                                                            .stream()
-                                                                            .filter(tuple -> tuple.getSecond()
-                                                                                                  .equals(language.get()))
-                                                                            .findFirst();
-
-                // update the translation text for the selected language
-                langCode.ifPresent(stringStringTuple -> {
-                    codeLists.getEntry(codeList.getCodeListData().getCode(), entity.getCodeListEntryData().getCode())
-                             .ifPresent(cle -> {
-                                 cle.getTranslations()
-                                    .updateText(CodeListEntry.CODE_LIST_ENTRY_DATA.inner(CodeListEntryData.DESCRIPTION),
-                                                stringStringTuple.getFirst(),
-                                                entity.getCodeListEntryData().getDescription());
-                             });
-                });
-            }
-        }
     }
 }
