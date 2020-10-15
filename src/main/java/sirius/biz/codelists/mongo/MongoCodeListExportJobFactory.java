0b4831c39ec6c9bebdab2808fb437a199dfb14de
Christian Schierle <csc@scireum.de>
2020-10-15 15:38:07 +0200

    Adds LanguageParameter to codelist export so it can handle different languages properly
    Fixes: SIRI-243

--- src/main/java/sirius/biz/codelists/mongo/MongoCodeListExportJobFactory.java
+++ src/main/java/sirius/biz/codelists/mongo/MongoCodeListExportJobFactory.java
@@ -10,12 +10,16 @@ package sirius.biz.codelists.mongo;
 
 import sirius.biz.codelists.CodeListController;
 import sirius.biz.codelists.CodeListEntry;
+import sirius.biz.codelists.CodeListExportJob;
 import sirius.biz.importer.ImportContext;
+import sirius.biz.jobs.batch.file.EntityExportJob;
 import sirius.biz.jobs.batch.file.EntityExportJobFactory;
 import sirius.biz.jobs.params.CodeListParameter;
+import sirius.biz.jobs.params.LanguageParameter;
 import sirius.biz.jobs.params.Parameter;
 import sirius.biz.process.ProcessContext;
 import sirius.db.mongo.MongoQuery;
+import sirius.kernel.commons.Explain;
 import sirius.kernel.di.std.Register;
 import sirius.web.http.QueryString;
 import sirius.web.security.Permission;
@@ -32,7 +36,10 @@ import java.util.function.Consumer;
 public class MongoCodeListExportJobFactory
         extends EntityExportJobFactory<MongoCodeListEntry, MongoQuery<MongoCodeListEntry>> {
 
-    private CodeListParameter codeListParameter = new CodeListParameter("codeList", "$CodeList").markRequired();
+    private static final CodeListParameter CODE_LIST_PARAMETER =
+            new CodeListParameter("codeList", "$CodeList").markRequired();
+    private static final LanguageParameter LANGUAGE_PARAMETER =
+            new LanguageParameter(LanguageParameter.PARAMETER_NAME, "$LocaleData.lang");
 
     @Nonnull
     @Override
@@ -47,18 +54,20 @@ public class MongoCodeListExportJobFactory
 
     @Override
     protected void collectParameters(Consumer<Parameter<?, ?>> parameterCollector) {
-        parameterCollector.accept(codeListParameter);
+        parameterCollector.accept(CODE_LIST_PARAMETER);
+        parameterCollector.accept(LANGUAGE_PARAMETER);
         super.collectParameters(parameterCollector);
     }
 
     @Override
     protected void extendSelectQuery(MongoQuery<MongoCodeListEntry> query, ProcessContext processContext) {
-        query.eq(CodeListEntry.CODE_LIST, processContext.require(codeListParameter));
+        query.eq(CodeListEntry.CODE_LIST, processContext.require(CODE_LIST_PARAMETER));
     }
 
     @Override
     protected void transferParameters(ImportContext context, ProcessContext processContext) {
-        context.set(CodeListEntry.CODE_LIST, processContext.require(codeListParameter));
+        context.set(CodeListEntry.CODE_LIST, processContext.require(CODE_LIST_PARAMETER));
+        context.set(LANGUAGE_PARAMETER.getName(), processContext.getParameter(LANGUAGE_PARAMETER));
     }
 
     @Override
@@ -68,6 +77,35 @@ public class MongoCodeListExportJobFactory
 
     @Override
     protected void computePresetFor(QueryString queryString, Object targetObject, Map<String, Object> preset) {
-        preset.put(codeListParameter.getName(), ((MongoCodeList) targetObject).getCodeListData().getCode());
+        preset.put(CODE_LIST_PARAMETER.getName(), ((MongoCodeList) targetObject).getCodeListData().getCode());
+        queryString.get(LANGUAGE_PARAMETER.getName())
+                   .ifFilled(value -> preset.put(LANGUAGE_PARAMETER.getName(), value));
+    }
+
+    @SuppressWarnings("squid:S2095")
+    @Explain("The job must not be closed here as it is returned and managed by the caller.")
+    @Override
+    protected EntityExportJob<MongoCodeListEntry, MongoQuery<MongoCodeListEntry>> createJob(ProcessContext process) {
+        ImportContext parameterContext = new ImportContext();
+        transferParameters(parameterContext, process);
+
+        return new CodeListExportJob<MongoCodeListEntry, MongoQuery<MongoCodeListEntry>>(templateFileParameter,
+                                                                                         destinationParameter,
+                                                                                         fileTypeParameter,
+                                                                                         getExportType(),
+                                                                                         getDictionary(),
+                                                                                         getDefaultMapping(),
+                                                                                         process,
+                                                                                         getName(),
+                                                                                         process.getParameter(
+                                                                                                 LANGUAGE_PARAMETER),
+                                                                                         process.require(
+                                                                                                 CODE_LIST_PARAMETER)).withQueryExtender(
+                query -> extendSelectQuery(query, process))
+                                                                                                                      .withContextExtender(
+                                                                                                                              context -> context
+                                                                                                                                      .putAll(parameterContext))
+                                                                                                                      .withFileName(
+                                                                                                                              getCustomFileName());
     }
 }
