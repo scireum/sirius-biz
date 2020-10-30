4f7b59c294ea16101a9011216c4705debee50e73
Marc Egenrieder <meg@scireum.de>
2020-10-30 13:37:39 +0100

    codestyle

--- src/main/java/sirius/biz/model/QueryController.java
+++ src/main/java/sirius/biz/model/QueryController.java
@@ -30,7 +30,11 @@ import sirius.web.security.Permission;
 import sirius.web.security.UserContext;
 
 import javax.annotation.Nonnull;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
