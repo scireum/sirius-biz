4c8dc770eccbaac5dfbda5c60708d39daed813e2
aha <aha@scireum.de>
2020-11-10 15:26:15 +0100

    Permits to handle "auxiliary" files in a ZIP archive.
    These files which reside in a ZIP archive being imported next to the files which are actually being processed.
    by default these files are ignored, but they can be very easily be copied to a VFS destination by overwriting
    a single method.

--- src/main/java/sirius/biz/jobs/batch/file/FileImportJob.java
+++ src/main/java/sirius/biz/jobs/batch/file/FileImportJob.java
@@ -15,13 +15,20 @@ import sirius.biz.process.logs.ProcessLog;
 import sirius.biz.storage.layer1.FileHandle;
 import sirius.biz.storage.layer3.FileParameter;
 import sirius.biz.storage.layer3.VirtualFile;
+import sirius.biz.storage.layer3.VirtualFileSystem;
 import sirius.kernel.commons.Files;
 import sirius.kernel.commons.Strings;
 import sirius.kernel.commons.Value;
+import sirius.kernel.commons.ValueHolder;
+import sirius.kernel.commons.Watch;
+import sirius.kernel.di.std.Part;
 import sirius.kernel.health.Exceptions;
+import sirius.kernel.health.Log;
+import sirius.kernel.nls.NLS;
 
 import javax.annotation.Nullable;
 import java.io.InputStream;
+import java.time.Instant;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -33,6 +40,11 @@ public abstract class FileImportJob extends ImportJob {
     protected static final String FILE_EXTENSION_ZIP = "zip";
 
     protected FileParameter fileParameter;
+
+    @Part
+    private static VirtualFileSystem vfs;
+
+    protected ValueHolder<VirtualFile> unusedFilesDestination;
 
     /**
      * Creates a new job for the given factory and process context.
@@ -90,12 +102,7 @@ public abstract class FileImportJob extends ImportJob {
 
             int filesImported = 0;
             while (entry != null) {
-                if (!isHiddenFile(entry.getName()) && canHandleFileExtension(Files.getFileExtension(entry.getName()))) {
-                    process.log(ProcessLog.info()
-                                          .withNLSKey("FileImportJob.importingZippedFile")
-                                          .withContext("filename", entry.getName()));
-
-                    executeForStream(entry.getName(), new CloseShieldInputStream(zipInputStream));
+                if (executeForEntry(zipInputStream, entry)) {
                     filesImported++;
                 }
 
@@ -106,6 +113,93 @@ public abstract class FileImportJob extends ImportJob {
                 throw Exceptions.createHandled().withNLSKey("FileImportJob.noZippedFileFound").handle();
             }
         }
+    }
+
+    private boolean executeForEntry(ZipInputStream zipInputStream, ZipEntry entry) throws Exception {
+        if (isHiddenFile(entry.getName())) {
+            return false;
+        }
+
+        if (canHandleFileExtension(Files.getFileExtension(entry.getName()))) {
+            process.log(ProcessLog.info()
+                                  .withNLSKey("FileImportJob.importingZippedFile")
+                                  .withContext("filename", entry.getName()));
+
+            executeForStream(entry.getName(), new CloseShieldInputStream(zipInputStream));
+            return true;
+        } else {
+            return handleUnsupportedFile(entry.getName(),
+                                         entry.getSize(),
+                                         entry.getLastModifiedTime().toInstant(),
+                                         new CloseShieldInputStream(zipInputStream));
+        }
+    }
+
+    /**
+     * Gets invoked for every entry in a given ZIP archive which cannot be processed by this job itself.
+     * <p>
+     * This might be used e.g. if an XML file is being processed which is accompanied with some media files to
+     * move them into the proper direcotry in the {@link sirius.biz.storage.layer3.VirtualFileSystem}.
+     * <p>
+     * By default this is attempted if the {@link #determineAuxiliaryFilesDirectory()} returns a non-null result.
+     * Otherwise these files are simply ignored.
+     *
+     * @param name    the name of the file
+     * @param size    the (uncompressed) size in bytes
+     * @param instant the last modificaion timestamp of the file
+     * @param data    the contents of the file
+     */
+    protected boolean handleUnsupportedFile(String name, long size, Instant instant, InputStream data) {
+        try {
+            Watch watch = Watch.start();
+            VirtualFile basePath = determineAuxiliaryFilesBasePath();
+            if (basePath == null) {
+                return false;
+            }
+
+            basePath.resolve(name).consumeStream(data, size);
+            process.addTiming(NLS.get("FileImportJob.auxiliaryFilesCopied"), watch.elapsedMillis());
+            return true;
+        } catch (Exception e) {
+            process.handle(Exceptions.handle()
+                                     .error(e)
+                                     .to(Log.BACKGROUND)
+                                     .withNLSKey("FileImportJob.copyAuxiliaryFileFailed")
+                                     .set("file", name)
+                                     .handle());
+            return false;
+        }
+    }
+
+    @Nullable
+    private VirtualFile determineAuxiliaryFilesBasePath() {
+        if (unusedFilesDestination == null) {
+            String unusedFilesPath = determineAuxiliaryFilesDirectory();
+            if (Strings.isEmpty(unusedFilesPath)) {
+                unusedFilesDestination = ValueHolder.of(null);
+            } else {
+                VirtualFile destination = vfs.resolve(unusedFilesPath);
+                if (destination.exists() && destination.isDirectory()) {
+                    unusedFilesDestination = ValueHolder.of(destination);
+                } else {
+                    unusedFilesDestination = ValueHolder.of(null);
+                }
+            }
+        }
+
+        return unusedFilesDestination.get();
+    }
+
+    /**
+     * Determines where (in the {@link VirtualFileSystem}) to store auxiliary files.
+     * <p>
+     * These are files which reside in a ZIP archive next to the files which are actually being processed by this
+     * job.
+     *
+     * @return the path where to store auxillary files or <tt>null</tt> to ignore them
+     */
+    protected String determineAuxiliaryFilesDirectory() {
+        return null;
     }
 
     protected boolean isHiddenFile(String name) {
