9672bde2ba36d6ce0645de18ffc79d4efb4b1215
Idevaldo De Lira <ili@scireum.de>
2020-11-27 12:18:37 +0100

    Unifies the delete and changed loops into one
    Also moves common code into an abstract class

---
+++ src/main/java/sirius/biz/storage/layer2/ProcessBlobsLoop.java
@@ -0,0 +1,65 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.storage.layer2;
+
+import sirius.kernel.async.BackgroundLoop;
+import sirius.kernel.commons.Strings;
+import sirius.kernel.di.std.PriorityParts;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Defines a loop to process created or renamed {@link Blob blobs}.
+ */
+public abstract class ProcessBlobsLoop extends BackgroundLoop {
+
+    private static final double FREQUENCY_EVERY_FIFTEEN_SECONDS = 1 / 15d;
+
+    @PriorityParts(BlobCreatedRenamedHandler.class)
+    protected List<BlobCreatedRenamedHandler> createdOrRenamedHandlers;
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "storage-layer2-process";
+    }
+
+    @Override
+    public double maxCallFrequency() {
+        return FREQUENCY_EVERY_FIFTEEN_SECONDS;
+    }
+
+    @Nullable
+    @Override
+    protected String doWork() throws Exception {
+        AtomicInteger deletedDirectories = deleteDirectories();
+        AtomicInteger deletedBlobs = deleteBlobs();
+        AtomicInteger createdRenamedBlobs = processCreatedOrRenamedBlobs();
+
+        if (deletedDirectories.get() == 0 && deletedBlobs.get() == 0 && createdRenamedBlobs.get() == 0) {
+            return null;
+        }
+
+        return Strings.apply("Deleted %s directories and %s blobs. Processed %s new or renamed blobs.",
+                             deletedDirectories.get(),
+                             deletedBlobs.get(),
+                             createdRenamedBlobs.get());
+    }
+
+    protected abstract AtomicInteger deleteBlobs();
+
+    protected abstract AtomicInteger deleteDirectories();
+
+    protected abstract AtomicInteger processCreatedOrRenamedBlobs();
+
+    protected abstract void propagateDelete(Directory dir);
+}
---
+++ src/main/java/sirius/biz/storage/layer2/jdbc/SQLDeletedBlobsLoop.java
@@ -0,0 +1,145 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.storage.layer2.jdbc;
+
+import sirius.biz.storage.layer2.Directory;
+import sirius.biz.storage.layer2.ProcessBlobsLoop;
+import sirius.biz.storage.util.StorageUtils;
+import sirius.db.jdbc.OMA;
+import sirius.kernel.commons.Strings;
+import sirius.kernel.di.std.Part;
+import sirius.kernel.di.std.Register;
+import sirius.kernel.health.Exceptions;
+
+import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Finally deletes {@link SQLDirectory directories} and {@link SQLBlob blobs} which have been marked as deleted.
+ */
+@Register(framework = SQLBlobStorage.FRAMEWORK_JDBC_BLOB_STORAGE)
+public class SQLDeletedBlobsLoop extends ProcessBlobsLoop {
+
+    @Part
+    private OMA oma;
+
+    @Override
+    protected AtomicInteger deleteBlobs() {
+        AtomicInteger numBlobs = new AtomicInteger();
+        oma.select(SQLBlob.class).eq(SQLBlob.DELETED, true).limit(256).iterateAll(blob -> {
+            try {
+                if (Strings.isFilled(blob.getPhysicalObjectKey())) {
+                    blob.getStorageSpace().getPhysicalSpace().delete(blob.getPhysicalObjectKey());
+                }
+
+                oma.delete(blob);
+                numBlobs.incrementAndGet();
+            } catch (Exception e) {
+                Exceptions.handle()
+                          .to(StorageUtils.LOG)
+                          .error(e)
+                          .withSystemErrorMessage("Layer 2/SQL: Failed to finally delete the blob %s (%s) in %s: (%s)",
+                                                  blob.getBlobKey(),
+                                                  blob.getFilename(),
+                                                  blob.getSpaceName())
+                          .handle();
+            }
+        });
+
+        return numBlobs;
+    }
+
+    @Override
+    protected AtomicInteger deleteDirectories() {
+        AtomicInteger numDirectories = new AtomicInteger();
+        oma.select(SQLDirectory.class).eq(SQLDirectory.DELETED, true).limit(256).iterateAll(dir -> {
+            try {
+                propagateDelete(dir);
+                oma.delete(dir);
+            } catch (Exception e) {
+                Exceptions.handle()
+                          .to(StorageUtils.LOG)
+                          .error(e)
+                          .withSystemErrorMessage(
+                                  "Layer 2/SQL: Failed to finally delete the directory %s (%s) in %s: (%s)",
+                                  dir.getId(),
+                                  dir.getName(),
+                                  dir.getSpaceName())
+                          .handle();
+            }
+            numDirectories.incrementAndGet();
+        });
+
+        return numDirectories;
+    }
+
+    @Override
+    protected AtomicInteger processCreatedOrRenamedBlobs() {
+        AtomicInteger numBlobs = new AtomicInteger();
+        oma.select(SQLBlob.class).eq(SQLBlob.CREATED_OR_RENAMED, true).limit(256).iterateAll(blob -> {
+            try {
+                createdOrRenamedHandlers.forEach(handler -> handler.execute(blob));
+                numBlobs.incrementAndGet();
+            } catch (Exception e) {
+                Exceptions.handle()
+                          .to(StorageUtils.LOG)
+                          .error(e)
+                          .withSystemErrorMessage("Layer 2/SQL: Failed to process the blob %s (%s) in %s: (%s)",
+                                                  blob.getBlobKey(),
+                                                  blob.getFilename(),
+                                                  blob.getSpaceName())
+                          .handle();
+            } finally {
+                try {
+                    oma.updateStatement(SQLBlob.class)
+                       .set(SQLBlob.CREATED_OR_RENAMED, false)
+                       .where(SQLBlob.ID, blob.getId())
+                       .executeUpdate();
+                } catch (SQLException e) {
+                    Exceptions.handle()
+                              .to(StorageUtils.LOG)
+                              .error(e)
+                              .withSystemErrorMessage(
+                                      "Layer 2/SQL: Failed to reset blob %s (%s) in %s as not changed: (%s)",
+                                      blob.getBlobKey(),
+                                      blob.getFilename(),
+                                      blob.getSpaceName())
+                              .handle();
+                }
+            }
+        });
+
+        return numBlobs;
+    }
+
+    @Override
+    protected void propagateDelete(Directory dir) {
+        Long directoryId = ((SQLDirectory) dir).getId();
+        try {
+            oma.updateStatement(SQLDirectory.class)
+               .set(SQLDirectory.DELETED, true)
+               .where(SQLDirectory.PARENT, directoryId)
+               .executeUpdate();
+            oma.updateStatement(SQLBlob.class)
+               .set(SQLBlob.DELETED, true)
+               .where(SQLBlob.PARENT, directoryId)
+               .executeUpdate();
+        } catch (SQLException e) {
+            Exceptions.handle()
+                      .to(StorageUtils.LOG)
+                      .error(e)
+                      .withSystemErrorMessage(
+                              "Layer 2/SQL: Failed to propagate deletion for directory %s (%s) in %s: (%s)",
+                              directoryId,
+                              dir.getName(),
+                              dir.getSpaceName())
+                      .handle();
+        }
+    }
+}
