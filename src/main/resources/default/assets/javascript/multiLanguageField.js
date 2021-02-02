a5909f2e51fb4d86278854b211787eb97eb3e45f
Sascha Bieberstein <sbi@scireum.de>
2021-02-02 19:40:47 +0100

    Extracts multi lang field js code into encapsulating class
    This allows to use multiple multi lang fields in the same page.
    It also replaces jQuery code with vanilla JS code.
    
    Fixes: SIRI-266

---
+++ src/main/resources/default/assets/javascript/multiLanguageField.js
@@ -0,0 +1,163 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+function MultiLanguageField(options) {
+    this.FALLBACK_CODE = 'fallback';
+
+    this.fieldName = options.fieldName;
+    this.hasFallback = options.hasFallback || false;
+    this.fallbackLabel = options.fallbackLabel;
+    this.wrapperId = options.wrapperId;
+    this.modalId = options.modalId;
+    this.values = options.values;
+    this.languageManagementEnabled = options.languageManagementEnabled;
+    this.addLanguageLabel = options.addLanguageLabel;
+
+    this.validLanguages = {};
+    if (this.hasFallback) {
+        this.validLanguages.fallback = fallbackLabel;
+    }
+    for (let langCode in options.validLanguages) {
+        this.validLanguages[langCode] = options.validLanguages[langCode];
+    }
+
+    this._wrapper = document.getElementById(this.wrapperId);
+    this._input = this._wrapper.querySelector('.mls-input');
+    this._hiddenInputs = this._wrapper.querySelector('.mls-hidden-inputs');
+    this._modal = document.getElementById(this.modalId);
+
+    let me = this;
+    this._modal.querySelector('.ok-btn').addEventListener('click', function () {
+        $(me._modal).modal('hide');
+        me.updateHiddenFields();
+    });
+
+    this.renderModalBody();
+    this.updateHiddenFields();
+}
+
+MultiLanguageField.prototype.renderModalBody = function () {
+    let _inputs = this._modal.querySelector('.mls-modal-inputs');
+
+    for (let langCode in this.validLanguages) {
+        let langName = this.validLanguages[langCode];
+
+        let _row = document.createElement('div');
+        _row.classList.add('row');
+        _row.classList.add('form-group');
+        _inputs.appendChild(_row);
+
+        let _labelColumn = document.createElement('div');
+        _labelColumn.classList.add('col-md-3');
+        _labelColumn.classList.add('language-flag');
+        // TODO hacky
+        _labelColumn.innerHTML = this.getFlagImage(langCode) + ' ' + langName;
+        _row.appendChild(_labelColumn);
+
+        let _inputColumn = document.createElement('div');
+        _inputColumn.classList.add('col-md-9');
+        _row.appendChild(_inputColumn);
+
+        let _textInput = document.createElement('input');
+        _textInput.classList.add('form-control');
+        _textInput.type = 'text';
+        _textInput.dataset.lang = langCode;
+        _textInput.value = this.values[langCode] || '';
+        _inputColumn.appendChild(_textInput);
+    }
+
+    if (this.languageManagementEnabled) {
+        let _addLanguageContainer = document.createElement('div');
+        _addLanguageContainer.classList.add('row');
+        _inputs.appendChild(_addLanguageContainer);
+        
+        let _addLanguageLink = document.createElement('a');
+        _addLanguageLink.classList.add('add-language');
+        // TODO hacky
+        _addLanguageLink.innerHTML = '<i class="fa fa-plus"></i> ' + this.addLanguageLabel;
+        
+        let me = this;
+        _addLanguageLink.addEventListener('click', function () {
+            let _row = document.createElement('div');
+            _row.classList.add('row');
+            _row.classList.add('form-group');
+            _inputs.appendChild(_row);
+            
+            let _languageColumn = document.createElement('div');
+            _languageColumn.classList.add('col-md-3');
+            _languageColumn.classList.add('language-flag');
+            _row.appendChild(_languageColumn);
+            
+            let _languageSelector = document.createElement('select');
+            _languageSelector.classList.add('form-control');
+            _languageSelector.addEventListener('change', function () {
+                let selectedLanguage = _languageSelector.value;
+                if (selectedLanguage !== '') {
+                    let _textInput = document.createElement('input');
+                    _textInput.classList.add('form-control');
+                    _textInput.type = 'text';
+                    _textInput.dataset.lang = selectedLanguage;
+                    _inputColumn.appendChild(_textInput);
+                }
+            });
+            _languageColumn.appendChild(_languageSelector);
+
+            for (let langCode in me.validLanguages) {
+                // TODO intersection between existing languages in langObject and validLanguages
+                let _option = document.createElement('option');
+                _option.value = langCode;
+                _option.text = me.validLanguages[langCode];
+                _languageSelector.appendChild(+_option);
+            }
+            
+            let _inputColumn = document.createElement('div');
+            _inputColumn.classList.add('col-md-9');
+            _row.appendChild(_inputColumn);
+        });
+        
+        _addLanguageContainer.appendChild(_addLanguageLink);
+    }
+}
+
+MultiLanguageField.prototype.updateHiddenFields = function () {
+    this._hiddenInputs.textContent = '';
+
+    let me = this;
+    this._modal.querySelectorAll('input[data-lang]').forEach(function (input) {
+        let lang = input.dataset.lang;
+
+        let _hiddenInput = document.createElement('input');
+        _hiddenInput.type = 'hidden';
+        _hiddenInput.value = input.value;
+        _hiddenInput.name = me.fieldName;
+
+        if (me.hasFallback && lang === me.FALLBACK_CODE) {
+            // also update main field
+            me._input.value = input.value;
+        } else {
+            _hiddenInput.name += '-' + lang;
+
+        }
+
+        me._hiddenInputs.appendChild(_hiddenInput);
+    })
+}
+
+MultiLanguageField.prototype.getLanguageName = function (langCode) {
+    return this.validLanguages[langCode];
+}
+
+MultiLanguageField.prototype.getFlagImage = function (langCode) {
+    if (langCode === this.FALLBACK_CODE) {
+        // globe icon
+        return String.fromCodePoint(127758);
+    } else {
+        // TODO apply baseUrl
+        return '<img src=\'/assets/images/flags/' + langCode + '.png\' alt=\'' + langCode + ' \'/>';
+    }
+}
