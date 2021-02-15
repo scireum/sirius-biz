76faeb857ee29bc9954e0f5ec9b7808e0d06cbf3
Jan Müller <jmu@scireum.de>
2021-02-15 11:38:43 +0100

    Implement a handler to replace blob:// URIs in PDFs
    This can be used instead of referencing the blobs via http in case the
    server is having problems resolving its own domain.
    
    Fixes: SE-8489

---
+++ src/main/java/sirius/biz/web/BlobPdfReplaceHandler.java
@@ -0,0 +1,59 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.web;
+
+import org.xhtmlrenderer.extend.FSImage;
+import org.xhtmlrenderer.extend.UserAgentCallback;
+import sirius.biz.storage.layer1.FileHandle;
+import sirius.biz.storage.layer2.BlobStorage;
+import sirius.kernel.commons.Strings;
+import sirius.kernel.di.std.Part;
+import sirius.kernel.di.std.Register;
+import sirius.web.templates.pdf.handlers.PdfReplaceHandler;
+
+import javax.annotation.Nullable;
+import java.util.Optional;
+
+/**
+ * Responsible for resolving blob:// URIs and resizing the image while maintaining the image ratio.
+ * <p>
+ * The format of the URI needs to match blob://space/variant/blobKey.
+ */
+@Register
+public class BlobPdfReplaceHandler extends PdfReplaceHandler {
+
+    @Part
+    private BlobStorage storage;
+
+    @Override
+    public boolean accepts(String protocol) {
+        return "blob".equals(protocol);
+    }
+
+    @Nullable
+    @Override
+    public FSImage resolveUri(String uri, UserAgentCallback userAgentCallback, int cssWidth, int cssHeight)
+            throws Exception {
+        String[] blobInfo = Strings.split(uri, "://").getSecond().split("/");
+
+        if (blobInfo.length != 3) {
+            throw new IllegalArgumentException("The URI is required to match the format 'blob://space/variant/blobKey'");
+        }
+
+        Optional<FileHandle> fileHandle = storage.getSpace(blobInfo[0]).download(blobInfo[2], blobInfo[1]);
+
+        if (fileHandle.isPresent()) {
+            FSImage image = resolveResource(userAgentCallback, fileHandle.get().getFile().toURI().toURL());
+
+            return resizeImage(image, cssWidth, cssHeight);
+        }
+
+        return null;
+    }
+}
