c0c5946f6c57cf54658a2c7bf171d09f6a3eb0b2
aha <aha@scireum.de>
2021-03-15 10:25:17 +0100

    Adds a test to check if serialization to and from SQL/JDBC works.

---
+++ src/test/java/sirius/biz/translations/SQLMultiLanguageStringPropertySpec.groovy
@@ -0,0 +1,58 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.translations
+
+import sirius.db.jdbc.OMA
+import sirius.kernel.BaseSpecification
+import sirius.kernel.async.CallContext
+import sirius.kernel.di.std.Part
+
+class SQLMultiLanguageStringPropertySpec extends BaseSpecification {
+
+    @Part
+    private static OMA oma
+
+    def "store retrieve and validate"() {
+        given:
+        def entity = new SQLMultiLanguageStringEntity()
+        entity.getMultiLangText().addText("de", "Schmetterling")
+        entity.getMultiLangText().addText("en", "Butterfly")
+        oma.update(entity)
+
+        when:
+        def output = oma.refreshOrFail(entity)
+
+        then:
+        output.getMultiLangText().size() == 2
+        output.getMultiLangText().hasText("de")
+        !output.getMultiLangText().hasText("fr")
+        output.getMultiLangText().fetchText("de") == "Schmetterling"
+        output.getMultiLangText().fetchText("fr") == null
+        output.getMultiLangText().fetchText("de", "en") == "Schmetterling"
+        output.getMultiLangText().fetchText("fr", "en") == "Butterfly"
+        output.getMultiLangText().fetchText("fr", "es") == null
+        output.getMultiLangText().getText("de") == Optional.of("Schmetterling")
+        output.getMultiLangText().getText("fr") == Optional.empty()
+
+        when:
+        CallContext.getCurrent().setLang("en")
+
+        then:
+        output.getMultiLangText().fetchText() == "Butterfly"
+        output.getMultiLangText().getText() == Optional.of("Butterfly")
+
+        when:
+        CallContext.getCurrent().setLang("fr")
+
+        then:
+        output.getMultiLangText().fetchText() == null
+        output.getMultiLangText().getText() == Optional.empty()
+    }
+
+}
