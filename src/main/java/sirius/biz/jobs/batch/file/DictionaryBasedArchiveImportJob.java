dcfcb26e253078d6fc0235d343d1f043f84dedd5
Idevaldo De Lira <ili@scireum.de>
2021-05-03 17:31:45 +0200

    Uses a UnitOfWork instead

--- src/main/java/sirius/biz/jobs/batch/file/DictionaryBasedArchiveImportJob.java
+++ src/main/java/sirius/biz/jobs/batch/file/DictionaryBasedArchiveImportJob.java
@@ -14,6 +14,7 @@ import sirius.biz.util.ExtractedFile;
 import sirius.kernel.commons.Callback;
 import sirius.kernel.commons.Context;
 import sirius.kernel.commons.Tuple;
+import sirius.kernel.commons.UnitOfWork;
 import sirius.web.data.LineBasedProcessor;
 
 import javax.annotation.CheckReturnValue;
@@ -39,7 +40,7 @@ public abstract class DictionaryBasedArchiveImportJob extends ArchiveImportJob {
         protected boolean required;
         protected boolean ignoreEmptyFields;
         protected Callback<Tuple<Integer, Context>> rowHandler;
-        protected Callback<ImportFile> completionHandler;
+        protected UnitOfWork completionHandler;
 
         /**
          * Creates a new instance for the given name.
@@ -60,7 +61,7 @@ public abstract class DictionaryBasedArchiveImportJob extends ArchiveImportJob {
          * @param completionHandler the handler to be called
          * @return the file itself for fluent method calls
          */
-        public ImportFile withCompletionHandler(Callback<ImportFile> completionHandler) {
+        public ImportFile withCompletionHandler(UnitOfWork completionHandler) {
             this.completionHandler = completionHandler;
             return this;
         }
@@ -152,7 +153,7 @@ public abstract class DictionaryBasedArchiveImportJob extends ArchiveImportJob {
         }
 
         if (importFile.completionHandler != null) {
-            importFile.completionHandler.invoke(importFile);
+            importFile.completionHandler.execute();
         }
     }
 }
