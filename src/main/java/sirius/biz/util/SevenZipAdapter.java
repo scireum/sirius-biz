04a4d38c15c149fd981f12f1a2b179dc08683d24
Idevaldo De Lira <ili@scireum.de>
2021-06-30 11:04:25 +0200

    Resolves cast error parsing 7-zip entries
    PropID.LAST_MODIFICATION_TIME returns a Date (or null) which can't be parsed as Long.
    
    Therefore we use getTime or assume the Instant.now in the unlikely event where the modification date isn't present in the archive
    
    Fixes: SIRI-373

--- src/main/java/sirius/biz/util/SevenZipAdapter.java
+++ src/main/java/sirius/biz/util/SevenZipAdapter.java
@@ -25,6 +25,7 @@ import java.io.IOException;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Date;
 import java.util.function.Predicate;
 
 /**
@@ -82,12 +83,15 @@ class SevenZipAdapter implements IArchiveExtractCallback {
         }
 
         currentFilePath = fetchFilePath(index);
-        currentLastModified = Instant.ofEpochSecond((Long) inArchive.getProperty(index, PropID.LAST_MODIFICATION_TIME));
 
         // Enforce the filename filter...
         if (!filter.test(currentFilePath)) {
             return null;
         }
+
+        Date lastModificationTime = (Date) inArchive.getProperty(index, PropID.LAST_MODIFICATION_TIME);
+        currentLastModified =
+                lastModificationTime == null ? Instant.now() : Instant.ofEpochSecond(lastModificationTime.getTime());
 
         // We actually want to extract this file - setup the shared buffer properly.
         // Note that this might (sooner or later) create a temporary file. Therefore it is essential that this stream
