054395e299d3a77dc5dae1ade7a91a6049690447
Andreas Haufler <aha@scireum.de>
2021-08-09 12:30:20 +0200

    Provides a message expander for knowledgebase entries.
    These can be referenced via kba:ID (in help texts or error messages).

---
+++ src/main/java/sirius/biz/tycho/kb/KnowledgeBaseMessageExpander.java
@@ -0,0 +1,52 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.tycho.kb;
+
+import sirius.kernel.commons.Strings;
+import sirius.kernel.di.std.Part;
+import sirius.kernel.di.std.Register;
+import sirius.kernel.nls.NLS;
+import sirius.web.controller.MessageExpander;
+
+import java.util.regex.Pattern;
+
+@Register
+public class KnowledgeBaseMessageExpander implements MessageExpander {
+
+    @Part
+    private KnowledgeBase knowledgeBase;
+
+    private static final Pattern LOCKED_KBA_PATTERN = Pattern.compile("\\[(.*?)kba:([a-zA-Z0-9]+)(.*)]");
+    private static final Pattern KBA_PATTERN = Pattern.compile("kba:([a-zA-Z0-9]+)");
+
+    @Override
+    public String expand(String message) {
+        message = LOCKED_KBA_PATTERN.matcher(message).replaceAll(match -> {
+            return knowledgeBase.resolve(NLS.getCurrentLang(), match.group(2), false).map(kba -> {
+                return match.group(1) + Strings.apply("""
+                                                              <span class="text-small d-inline-flex flex-row align-items-baseline">
+                                                                  <i class="fa fa-lightbulb"></i><a class="pl-1" href="/kba/%s/%s">%s</a>
+                                                              </span>
+                                                              """,
+                                                      kba.getLanguage(),
+                                                      kba.getArticleId(),
+                                                      kba.getTitle()) + match.group(3);
+            }).orElse("");
+        });
+        return KBA_PATTERN.matcher(message).replaceAll(match -> {
+            return knowledgeBase.resolve(NLS.getCurrentLang(), match.group(1), true).map(kba -> {
+                return Strings.apply("""
+                                             <span class="text-small d-inline-flex flex-row align-items-baseline">
+                                                 <i class="fa fa-lightbulb"></i><a class="pl-1" href="/kba/%s/%s">%s</a>
+                                             </span>
+                                             """, kba.getLanguage(), kba.getArticleId(), kba.getTitle());
+            }).orElse("kba:" + match.group());
+        });
+    }
+}
