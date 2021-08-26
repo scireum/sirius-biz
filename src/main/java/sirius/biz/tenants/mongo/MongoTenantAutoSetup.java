4ef991594fe2b7f79def81bf0b6fd71a0e2a678e
Andreas Haufler <aha@scireum.de>
2021-08-26 16:02:43 +0200

    Enhances the tenant auto setup rule.
    We now permit to update the SAML data of the system tenant via the
    system configuration - when in turn, can be supplied by configuration
    management tools like puppet.
    
    Fixes: SIRI-419

--- src/main/java/sirius/biz/tenants/mongo/MongoTenantAutoSetup.java
+++ src/main/java/sirius/biz/tenants/mongo/MongoTenantAutoSetup.java
@@ -8,20 +8,25 @@
 
 package sirius.biz.tenants.mongo;
 
+import sirius.biz.tenants.BaseTenantAutoSetup;
 import sirius.db.mongo.Mango;
 import sirius.db.mongo.Mongo;
 import sirius.db.mongo.MongoEntity;
 import sirius.kernel.AutoSetup;
 import sirius.kernel.AutoSetupRule;
+import sirius.kernel.commons.Strings;
 import sirius.kernel.di.std.Part;
-import sirius.kernel.di.std.Priorized;
 import sirius.kernel.di.std.Register;
 
 /**
  * Creates an initial tenant and user if none are available.
+ * <p>
+ * Also, if SAML settings are present in the system config, the system tenant is updated accordingly. This can
+ * be used to distribute SAML settings via the system configuration and thus use configuration management tools
+ * like puppet.
  */
-@Register(framework = MongoTenants.FRAMEWORK_TENANTS_MONGO)
-public class MongoTenantAutoSetup implements AutoSetupRule {
+@Register(classes = AutoSetupRule.class, framework = MongoTenants.FRAMEWORK_TENANTS_MONGO)
+public class MongoTenantAutoSetup extends BaseTenantAutoSetup {
 
     @Part
     private Mango mango;
@@ -32,6 +37,7 @@ public class MongoTenantAutoSetup implements AutoSetupRule {
     @Override
     public void setup() throws Exception {
         if (mango.select(MongoTenant.class).exists()) {
+            updateSystemTenantIfNecessary();
             return;
         }
         if (mango.select(MongoUserAccount.class).exists()) {
@@ -40,33 +46,48 @@ public class MongoTenantAutoSetup implements AutoSetupRule {
 
         AutoSetup.LOG.INFO("Creating system tenant....");
         MongoTenant tenant = new MongoTenant();
-        tenant.getTenantData().setName("System Tenant");
+        setupTenantData(tenant);
         mango.update(tenant);
 
         // Fix the ID of the system tenant to be "1" - This should normally be avoided
         // at all cost, but in this case it greatly simplifies the system config...
-        mongo.update().where(MongoEntity.ID, tenant.getId()).set(MongoEntity.ID, "1").executeForOne(MongoTenant.class);
-        tenant = mango.find(MongoTenant.class, "1")
+        String systemTenantId = "1";
+        if (tenants != null) {
+            systemTenantId = tenants.getSystemTenantId();
+        }
+        mongo.update()
+             .where(MongoEntity.ID, tenant.getId())
+             .set(MongoEntity.ID, systemTenantId)
+             .executeForOne(MongoTenant.class);
+
+        tenant = mango.find(MongoTenant.class, systemTenantId)
                       .orElseThrow(() -> new IllegalStateException(
                               "Failed to resolve the system tenant after changing its ID!"));
 
-        AutoSetup.LOG.INFO("Creating user 'system' with password 'system'....");
-        MongoUserAccount ua = new MongoUserAccount();
-        ua.getTenant().setValue(tenant);
-        ua.getUserAccountData().setEmail("system@localhost.local");
-        ua.getUserAccountData().getLogin().setUsername("system");
-        ua.getUserAccountData().getLogin().setCleartextPassword("system");
-        ua.getTrace().setSilent(true);
-
-        // This should be enough to grant us more roles via the UI
-        ua.getUserAccountData().getPermissions().getPermissions().add("administrator");
-        ua.getUserAccountData().getPermissions().getPermissions().add("user-administrator");
-        ua.getUserAccountData().getPermissions().getPermissions().add("system-administrator");
-        mango.update(ua);
+        // We only create the system user, if no SAML settings are present...
+        if (Strings.isEmpty(tenant.getTenantData().getSamlRequestIssuerName())) {
+            AutoSetup.LOG.INFO("Creating user 'system' with password 'system'....");
+            MongoUserAccount ua = new MongoUserAccount();
+            ua.getTenant().setValue(tenant);
+            setupUserData(ua);
+            mango.update(ua);
+        }
     }
 
-    @Override
-    public int getPriority() {
-        return Priorized.DEFAULT_PRIORITY;
+    private void updateSystemTenantIfNecessary() {
+        if (tenants == null) {
+            return;
+        }
+
+        MongoTenant tenant = mango.find(MongoTenant.class, tenants.getSystemTenantId()).orElse(null);
+        if (tenant != null) {
+            updateSamlData(tenant);
+        }
+
+        if (tenant.isAnyMappingChanged()) {
+            AutoSetup.LOG.INFO(
+                    "Updating the SAML settings of the system tenant using the data from the system config...");
+            mango.update(tenant);
+        }
     }
 }
