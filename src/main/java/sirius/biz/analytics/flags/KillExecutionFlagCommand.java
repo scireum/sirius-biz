b7bbdef406848eecd7a10cd8fb739fb46b230fa1
Andreas Haufler <aha@scireum.de>
2021-09-10 10:23:06 +0200

    Permits to kill an execution flag manually.

---
+++ src/main/java/sirius/biz/analytics/flags/KillExecutionFlagCommand.java
@@ -0,0 +1,52 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.analytics.flags;
+
+import sirius.kernel.di.std.Part;
+import sirius.kernel.health.console.Command;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.time.Period;
+
+/**
+ * Permits to kill an execution flag manually.
+ */
+public class KillExecutionFlagCommand implements Command {
+
+    @Part
+    @Nullable
+    private ExecutionFlags executionFlags;
+
+    @Override
+    public void execute(Output output, String... args) throws Exception {
+        if (executionFlags == null) {
+            output.line("No ExecutionFlags provide enabled...");
+            return;
+        }
+
+        if (args.length != 2) {
+            output.line("Usage: kill-execution-flag <reference> <flag>");
+            return;
+        }
+
+        executionFlags.storeExecutionFlag(args[0], args[1], null, Period.ZERO);
+    }
+
+    @Override
+    public String getDescription() {
+        return "Kills the execution flag with the given name";
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "kill-execution-flag";
+    }
+}
