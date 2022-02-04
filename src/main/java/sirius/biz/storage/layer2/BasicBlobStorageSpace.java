ee891b9011acd5102665f99cb871556bc1ac3f01
Marc Egenrieder <meg@scireum.de>
2022-02-04 11:34:42 +0100

    Properly notes connectivyIssue on retry
    This enhances commit 69b93d1c0e3222d3349874d3209bf154a577e94c

--- src/main/java/sirius/biz/storage/layer2/BasicBlobStorageSpace.java
+++ src/main/java/sirius/biz/storage/layer2/BasicBlobStorageSpace.java
@@ -1759,6 +1759,9 @@ public abstract class BasicBlobStorageSpace<B extends Blob & OptimisticCreate, D
             response.tunnel(url.get().toString(), errorCode -> {
                 if (errorCode == HttpResponseStatus.INTERNAL_SERVER_ERROR.code() && conversionHosts.size() > 1) {
                     // Re-try conversion as the uplink conversion host might be down, but another is alive...
+                    conversionHostLastConnectivityIssue.put(url.get().getHost(), LocalDateTime.now());
+                    StorageUtils.LOG.WARN("Layer 2: Detected a connectivity issue for conversion host %s!",
+                                          url.get().getHost());
                     delegateConversion(blobKey, variant, response, maxAttempts - 1);
                 } else {
                     response.error(HttpResponseStatus.valueOf(errorCode));
