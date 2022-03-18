237f530a9dff68b751954468dea2d448b87376ff
Christian Schierle <csc@scireum.de>
2022-03-18 12:12:19 +0100

    Assigns a system process to the system tenant if it is started while spying another user or tenant
    Fixes: SIRI-547

---
+++ src/main/java/sirius/biz/web/SpyUser.java
@@ -0,0 +1,27 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.web;
+
+import sirius.kernel.di.transformers.Composable;
+import sirius.web.security.UserInfo;
+
+/**
+ * Holds a reference to the {@link UserInfo root user}, if currently spying another user or tenant.
+ */
+public class SpyUser extends Composable {
+    private final UserInfo rootUser;
+
+    public SpyUser(UserInfo rootUser) {
+        this.rootUser = rootUser;
+    }
+
+    public UserInfo getRootUser() {
+        return rootUser;
+    }
+}
