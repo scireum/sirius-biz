6aefebf96e021cbc55e2028480555fbb54273b63
Marc Egenrieder <meg@scireum.de>
2022-03-28 14:58:05 +0200

    Allow setting the next value of a sequence in console

--- src/main/java/sirius/biz/sequences/SequencesCommand.java
+++ src/main/java/sirius/biz/sequences/SequencesCommand.java
@@ -13,6 +13,7 @@ import sirius.kernel.di.std.Register;
 import sirius.kernel.health.console.Command;
 
 import javax.annotation.Nonnull;
+import java.util.Locale;
 
 /**
  * Provides a console command to display all managed sequences.
@@ -27,6 +28,42 @@ public class SequencesCommand implements Command {
 
     @Override
     public void execute(Output output, String... params) throws Exception {
+        if (params.length == 0) {
+            listSequences(output);
+            return;
+        }
+        if (params.length == 3) {
+            setSequence(output, params);
+            return;
+        }
+        outputUsage(output);
+    }
+
+    private void outputUsage(Output output) {
+        output.apply("Usage:");
+        output.apply("'sequences' to list all known sequences");
+        output.apply("'sequences set NAME NEXTVALUE' to set the next value for a sequence");
+        output.apply("'sequences force NAME NEXTVALUE' to force the next value for a sequence !DANGEROUS! as it can lead to duplicate ids");
+    }
+
+    private void setSequence(Output output, String... params) {
+        boolean force;
+        String forceParam = params[0].toLowerCase(Locale.ROOT);
+        if ("force".equals(forceParam)) {
+            force = true;
+        } else if ("set".equals(forceParam)) {
+            force = false;
+        } else {
+            outputUsage(output);
+            return;
+        }
+        String sequence = params[1];
+        long nextValue = Long.parseLong(params[2]);
+        sequences.setNextValue(sequence, nextValue, force);
+        output.apply("Sequence %s set to %d.", sequence, nextValue);
+    }
+
+    private void listSequences(Output output) {
         output.apply("%-40s %12s", "NAME", "NEXT VALUE");
         output.separator();
         sequences.getKnownSequences().forEach(sequence -> {
