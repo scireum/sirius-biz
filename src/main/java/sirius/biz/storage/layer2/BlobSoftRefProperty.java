0f7c2acf591128e34edd4552e9473e1358a73344
Idevaldo De Lira <ili@scireum.de>
2022-05-27 10:14:47 +0200

    Returns early if entity is not changed
    Fixes: OX-8698

--- src/main/java/sirius/biz/storage/layer2/BlobSoftRefProperty.java
+++ src/main/java/sirius/biz/storage/layer2/BlobSoftRefProperty.java
@@ -201,8 +201,12 @@ public class BlobSoftRefProperty extends BlobRefProperty {
 
     @Override
     protected void onBeforeSaveChecks(Object entity) {
+        if (!isChanged(entity)) {
+            return;
+        }
+
         BlobSoftRef ref = (BlobSoftRef) getRef(entity);
-        if (isChanged(entity) && ref.isFilled() && !ref.isURL() && ref.getBlob().isTemporary()) {
+        if (ref.isFilled() && !ref.isURL() && ref.getBlob().isTemporary()) {
             throw Exceptions.handle()
                             .to(StorageUtils.LOG)
                             .withSystemErrorMessage(
