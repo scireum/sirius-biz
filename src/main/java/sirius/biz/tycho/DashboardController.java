42318e6dd01d2ca5a58eadc8919b15bd55eaf1b9
Andreas Haufler <aha@scireum.de>
2022-06-29 20:48:42 +0200

    Collects a list of icons and links for open source technologies being used.
    Fixes: SIRI-589

--- src/main/java/sirius/biz/tycho/DashboardController.java
+++ src/main/java/sirius/biz/tycho/DashboardController.java
@@ -12,11 +12,18 @@ import io.netty.handler.codec.http.QueryStringDecoder;
 import sirius.biz.analytics.events.EventRecorder;
 import sirius.biz.analytics.events.PageImpressionEvent;
 import sirius.biz.web.BizController;
+import sirius.kernel.commons.Tuple;
 import sirius.kernel.di.std.Part;
+import sirius.kernel.di.std.PriorityParts;
 import sirius.kernel.di.std.Register;
+import sirius.web.controller.Controller;
 import sirius.web.controller.Routed;
 import sirius.web.http.WebContext;
 import sirius.web.security.LoginRequired;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Provides a default route for "/admin" and "/system/dashboard" as these are the common entry points of the
@@ -27,11 +34,16 @@ import sirius.web.security.LoginRequired;
  * decide which URIs are handled by themselves (and used for other purposes) and which are used for the main
  * entrypoint of the backed.
  */
-@Register
+@Register(classes = {Controller.class, DashboardController.class})
 public class DashboardController extends BizController {
 
     @Part
     private EventRecorder eventRecorder;
+
+    @PriorityParts(TechStackInfo.class)
+    private List<TechStackInfo> techStack;
+
+    private List<Tuple<String, String>> activeTechnologies;
 
     /**
      * Provides an entry point via "/admin".
@@ -55,5 +67,24 @@ public class DashboardController extends BizController {
     @Routed(value = "/system/dashboard", priority = 999)
     public void dashboard(WebContext webContext) {
         admin(webContext);
+    }
+
+    /**
+     * Provides a list of tuples (image url + link) for all active open source technologies in this product.
+     * <p>
+     * These are provided via {@link TechStackInfo TechstackInfos}.
+     *
+     * @return a list of all active technologies to show
+     */
+    public List<Tuple<String, String>> getTechStack() {
+        if (activeTechnologies == null) {
+            List<Tuple<String, String>> technologies = new ArrayList<>();
+            techStack.forEach(provider -> provider.fetchActiveTechnologies((image, link) -> technologies.add(Tuple.create(
+                    image,
+                    link))));
+            activeTechnologies = technologies;
+        }
+
+        return Collections.unmodifiableList(activeTechnologies);
     }
 }
