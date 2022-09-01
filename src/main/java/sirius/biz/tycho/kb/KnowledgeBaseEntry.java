9b024097370d8cbcf8335a31c27de9e44a89f06d
Andreas Haufler <aha@scireum.de>
2022-09-01 14:26:53 +0200

    Adds a proper toString method for KnowledgeBaseEntry.

--- src/main/java/sirius/biz/tycho/kb/KnowledgeBaseEntry.java
+++ src/main/java/sirius/biz/tycho/kb/KnowledgeBaseEntry.java
@@ -114,6 +114,11 @@ public class KnowledgeBaseEntry extends SearchableEntity {
         setSearchableContent(getArticleId());
     }
 
+    @Override
+    public String toString() {
+        return getArticleId() + ": " + getTitle();
+    }
+
     /**
      * Checks if the permissions required by this article are met.
      *
