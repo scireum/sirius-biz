c2d7ff2fe9cdd03d578c4bab7639c33515a13598
mko <mko@scireum.de>
2022-09-16 12:23:26 +0200

    Adds a platform distribution chart for the knowledge base
    Fixes: OX-9081

---
+++ src/main/java/sirius/biz/tycho/kb/KnowledgeBaseUserAgentsPlatformDistributionChart.java
@@ -0,0 +1,92 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.tycho.kb;
+
+import sirius.biz.analytics.charts.explorer.ChartFactory;
+import sirius.biz.analytics.charts.explorer.ChartObjectResolver;
+import sirius.biz.analytics.charts.explorer.TimeSeriesChartFactory;
+import sirius.biz.analytics.charts.explorer.TimeSeriesComputer;
+import sirius.biz.analytics.charts.explorer.UserAgentsPlatformDistributionTimeSeriesComputer;
+import sirius.biz.jobs.StandardCategories;
+import sirius.biz.tenants.TenantUserManager;
+import sirius.kernel.commons.Callback;
+import sirius.kernel.commons.Strings;
+import sirius.kernel.di.std.Register;
+import sirius.web.security.Permission;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.function.Consumer;
+
+/**
+ * Implements a time series chart for the knowledge base, showing the platform distribution.
+ * <p></p>
+ * The chart visualizes the platforms which have been used in order to access the knowledge base by extracting the required data from the user agents dataset.
+ */
+@Register(framework = KnowledgeBase.FRAMEWORK_KNOWLEDGE_BASE)
+@Permission(TenantUserManager.PERMISSION_SYSTEM_TENANT_MEMBER)
+public class KnowledgeBaseUserAgentsPlatformDistributionChart extends TimeSeriesChartFactory<Object> {
+
+    private static final String SQL_QUERY = Strings.apply(
+            // language=SQL
+            """
+                    SELECT %s, %s, plus(%s, plus(%s, plus(%s, %s))) as desktop, YEAR(eventDate) as year, MONTH(eventDate) AS month [:daily , DAY(eventDate) AS day]
+                    FROM pageimpressionevent
+                    WHERE aggregationUri = '/kba' AND eventDate >= ${start} AND eventDate <= ${end}
+                    GROUP BY [:daily DAY(eventDate), ] MONTH(eventDate), YEAR(eventDate)""",
+            UserAgentsPlatformDistributionTimeSeriesComputer.COUNT_IOS,
+            UserAgentsPlatformDistributionTimeSeriesComputer.COUNT_ANDROID,
+            UserAgentsPlatformDistributionTimeSeriesComputer.COUNT_MACINTOSH,
+            UserAgentsPlatformDistributionTimeSeriesComputer.COUNT_WINDOWS,
+            UserAgentsPlatformDistributionTimeSeriesComputer.COUNT_LINUX,
+            UserAgentsPlatformDistributionTimeSeriesComputer.COUNT_UNIX);
+
+    @Nullable
+    @Override
+    protected Class<? extends ChartObjectResolver<Object>> getResolver() {
+        return null;
+    }
+
+    @Override
+    public String getCategory() {
+        return StandardCategories.MISC;
+    }
+
+    @Override
+    protected void collectReferencedCharts(Consumer<Class<? extends ChartFactory<Object>>> referenceChartConsumer) {
+        // intentionally empty -> there are no referenced charts
+    }
+
+    @Override
+    protected void computers(boolean hasComparisonPeriod,
+                             boolean isComparisonPeriod,
+                             Callback<TimeSeriesComputer<Object>> executor) throws Exception {
+        if (isComparisonPeriod) {
+            return;
+        }
+
+        executor.invoke(new UserAgentsPlatformDistributionTimeSeriesComputer<>(ignored -> SQL_QUERY));
+    }
+
+    @Nonnull
+    @Override
+    public String getName() {
+        return "GlobalKnowledgeBaseUserAgentsPlatformDistributionChart";
+    }
+
+    @Override
+    public int getPriority() {
+        return 900;
+    }
+
+    @Override
+    protected boolean stackValues(boolean hasComparisonPeriod) {
+        return true;
+    }
+}
