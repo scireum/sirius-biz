aabcde92fe7bf3fe1d77d3c335c94633ff65b6ac
Tobias Beeh <tbe@scireum.de>
2022-12-16 16:10:35 +0100

    Improve ftp log messages when a handled exception is thrown

--- src/main/java/sirius/biz/storage/layer3/downlink/ssh/sftp/BridgeSftpSubsystem.java
+++ src/main/java/sirius/biz/storage/layer3/downlink/ssh/sftp/BridgeSftpSubsystem.java
@@ -8,7 +8,9 @@
 
 package sirius.biz.storage.layer3.downlink.ssh.sftp;
 
+import org.apache.sshd.common.util.buffer.Buffer;
 import org.apache.sshd.server.channel.ChannelSession;
+import org.apache.sshd.sftp.server.SftpErrorStatusDataHandler;
 import org.apache.sshd.sftp.server.SftpSubsystem;
 import org.apache.sshd.sftp.server.SftpSubsystemConfigurator;
 import sirius.biz.storage.layer3.downlink.ssh.BridgeSession;
@@ -16,12 +18,14 @@ import sirius.biz.storage.util.StorageUtils;
 import sirius.kernel.commons.Strings;
 import sirius.kernel.di.std.Part;
 import sirius.kernel.health.Exceptions;
+import sirius.kernel.health.HandledException;
 import sirius.web.security.ScopeDetector;
 import sirius.web.security.ScopeInfo;
 import sirius.web.security.UserContext;
 import sirius.web.security.UserInfo;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
 
 /**
  * Wraps the SFTP subsystem to execute as the correct {@link UserInfo user}.
@@ -54,6 +58,21 @@ class BridgeSftpSubsystem extends SftpSubsystem {
                       .error(e)
                       .withSystemErrorMessage("Layer 3/SFTP: An error occurred: %s (%s)")
                       .handle();
+        }
+    }
+
+    @Override
+    protected void sendStatus(Buffer buffer, int id, Throwable e, int cmd, Object... args) throws IOException {
+        if (e instanceof HandledException handledException) {
+            SftpErrorStatusDataHandler handler = getErrorStatusDataHandler();
+            int subStatus = handler.resolveSubStatus(this, id, e, cmd, args);
+            String message = Strings.apply("%s: %s",
+                                           handler.resolveErrorMessage(this, id, e, subStatus, cmd, args),
+                                           handledException.getLocalizedMessage());
+            String lang = handler.resolveErrorLanguage(this, id, e, subStatus, cmd, args);
+            sendStatus(buffer, id, subStatus, message, lang);
+        } else {
+            super.sendStatus(buffer, id, e, cmd, args);
         }
     }
 }
