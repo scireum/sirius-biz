45cc1f9a95ad88c9fb41696300e28e1da0aaaa99
Idevaldo De Lira <ili@scireum.de>
2023-02-02 08:17:34 +0100

    Improves JavaDoc
    Fixes: SIRI-689

--- src/main/java/sirius/biz/jobs/batch/file/SyncSourceDeleteMode.java
+++ src/main/java/sirius/biz/jobs/batch/file/SyncSourceDeleteMode.java
@@ -8,26 +8,36 @@
 
 package sirius.biz.jobs.batch.file;
 
+import sirius.biz.importer.txn.ImportTransactionalEntity;
 import sirius.kernel.nls.NLS;
+
+import java.util.function.Consumer;
 
 /**
  * Declares the deletion modes for a {@link RelationalEntityImportJob} using {@linkplain RelationalEntityImportJob#SYNC_MODE_PARAMETER}
  * equals {@linkplain SyncMode#SYNC}.
+ * <p>
+ * The deletion performed in {@link sirius.biz.importer.txn.ImportTransactionHelper#deleteUnmarked(Class, Consumer, Consumer)},
+ * attempts to delete all records not {@linkplain sirius.biz.importer.txn.ImportTransactionHelper#mark(ImportTransactionalEntity) marked}
+ * during an import and are considered obsolete.
+ * <p>
+ * Depending on this mode, the deletion clause is fine-tuned to consider the {@link sirius.biz.importer.txn.ImportTransactionData#SOURCE}
+ * field in different ways.
  */
 public enum SyncSourceDeleteMode {
 
     /**
-     * Deletes non-matching records with the same source.
+     * Deletes unmarked records with the same source.
      */
     SAME_SOURCE,
 
     /**
-     * Deletes non-matching records with the same source or empty source.
+     * Deletes unmarked records with the same source or empty source.
      */
     SAME_SOURCE_OR_EMPTY,
 
     /**
-     * Deletes non-matching records independent on source.
+     * Deletes unmarked records independent on source.
      */
     ALL;
 
