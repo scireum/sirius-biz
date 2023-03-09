a98b1c99b993be38abf5256a2219a3edfe7c5690
bko <bko@scireum.de>
2023-03-09 23:36:08 +0100

    Enables possibility to add a URL for a softRefField
    Fixes SIRI-740

--- src/main/resources/default/assets/scripts/blob-upload-url-popover.js
+++ src/main/resources/default/assets/scripts/blob-upload-url-popover.js
@@ -9,80 +9,94 @@
  * @param i18n Contains information about the label, the ok-Button and the cancel-Button of the popover
  */
 function initUrlUploadPopover(triggerElement, uploadContainer, resetButton, i18n) {
+    const body = buildBody(triggerElement, uploadContainer, resetButton);
+    const container = buildContainer(triggerElement.parentElement);
     $(triggerElement).popover({
         html: true,
         trigger: 'manual',
-        content: buildBody(triggerElement, uploadContainer, resetButton),
-        container: buildContainer(triggerElement.parentElement),
+        content: body.container,
+        container: container,
         placement: 'top'
     });
 
-    triggerElement.addEventListener("click", function() {
+    triggerElement.addEventListener('click', function() {
         $(triggerElement).popover('toggle');
     });
 
+    resetButton.addEventListener('click', function() {
+        body.inputField.value = '';
+    });
+
     function buildContainer() {
-        const container = document.createElement("div");
-        container.classList.add("popover-content-max");
+        const container = document.createElement('div');
+        container.classList.add('popover-content-max');
         triggerElement.parentElement.appendChild(container);
         return container;
     }
 
     function buildBody() {
-        const container = document.createElement("div");
-        container.classList.add("row");
+        const body = {};
+        buildInputFormGroupCol(body);
+        buildOkButtonCol(body);
+        buildCancelButtonCol(body);
+        buildErrorCol(body);
+        buildBodyContainer(body);
+        return body;
+    }
 
-        // Input field
-        const inputCol = document.createElement("div");
-        inputCol.classList.add("col", "col-12");
+    function buildInputFormGroupCol(body) {
+        body.inputLabel = document.createElement('label');
+        body.inputLabel.innerHTML = i18n.label;
 
-        const formGroup = document.createElement("div");
-        formGroup.classList.add("form-group");
+        body.inputField = document.createElement('input');
+        body.inputField.classList.add('form-control');
+        body.inputField.setAttribute('type', 'text');
+        body.inputField.setAttribute('placeholder', 'https://');
 
-        const inputLabel = document.createElement("label");
-        inputLabel.innerHTML = i18n.label;
+        body.formGroup = document.createElement('div');
+        body.formGroup.classList.add('form-group');
+        body.formGroup.append(body.inputLabel, body.inputField);
 
-        const inputField = document.createElement("input");
-        inputField.classList.add("form-control");
-        inputField.setAttribute("type", "text");
+        body.inputCol = document.createElement('div');
+        body.inputCol.classList.add('col', 'col-12');
+        body.inputCol.appendChild(body.formGroup);
+    }
 
-        formGroup.append(inputLabel, inputField);
-        inputCol.appendChild(formGroup);
-
-        // Button OK
-        const leftCol = document.createElement("div");
-        leftCol.classList.add("col", "col-12", "col-md-6");
-
-        const buttonOk = document.createElement("button");
-        buttonOk.classList.add("btn", "btn-block", "btn-primary", "btn-apply");
-        buttonOk.innerHTML = i18n.ok + ' <i class="fa fa-check"></i>';
-        buttonOk.addEventListener("click", function(event) {
+    function buildOkButtonCol(body) {
+        body.buttonOk = document.createElement('button');
+        body.buttonOk.classList.add('btn', 'btn-block', 'btn-primary', 'btn-apply');
+        body.buttonOk.innerHTML = i18n.ok + ' <i class="fas fa-check"></i>';
+        body.buttonOk.addEventListener('click', function (event) {
             event.preventDefault();
-            updateURL(inputField.value);
-        })
+            updateURL(body.inputField.value);
+        });
 
-        leftCol.appendChild(buttonOk);
+        body.leftCol = document.createElement('div');
+        body.leftCol.classList.add('col', 'col-12', 'col-md-6');
+        body.leftCol.appendChild(body.buttonOk);
+    }
 
-        // Button cancel
-        const rightCol = document.createElement("div");
-        rightCol.classList.add("col" , "col-12", "col-md-6");
-
-        const buttonCancel = document.createElement("div");
-        buttonCancel.classList.add("btn", "btn-block", "btn-close", "btn-outline-secondary");
-        buttonCancel.innerHTML = i18n.cancel + ' <i class="fa fa-close"></i>';
-        buttonCancel.addEventListener("click", function() {
+    function buildCancelButtonCol(body) {
+        body.buttonCancel = document.createElement('div');
+        body.buttonCancel.classList.add('btn', 'btn-block', 'btn-close', 'btn-outline-secondary');
+        body.buttonCancel.innerHTML = i18n.cancel + ' <i class="fas fa-close"></i>';
+        body.buttonCancel.addEventListener('click', function () {
             $(triggerElement).popover('toggle');
         });
+        body.rightCol = document.createElement('div');
+        body.rightCol.classList.add('col', 'col-12', 'col-md-6');
+        body.rightCol.appendChild(body.buttonCancel);
+    }
 
-        resetButton.addEventListener("click", function() {
-            inputField.value = "";
-        });
+    function buildErrorCol(body) {
+        body.errorCol = document.createElement('div');
+        body.errorCol.classList.add('col', 'col-12');
+    }
 
-        rightCol.appendChild(buttonCancel);
-
-        container.append(inputCol, leftCol, rightCol);
-
-        return container;
+    function buildBodyContainer(body) {
+        body.container = document.createElement('div');
+        body.container.classList.add('row');
+        body.container.append(body.inputCol, body.errorCol, body.leftCol, body.rightCol);
     }
 
     /**
@@ -92,15 +106,34 @@ function initUrlUploadPopover(triggerElement, uploadContainer, resetButton, i18n
      */
     const updateURL = function (url) {
         if (url.startsWith('http://') || url.startsWith('https://')) {
-            let img = uploadContainer.querySelector(".img-preview img");
-            img.setAttribute("src", url);
+            let img = uploadContainer.querySelector('.img-preview img');
+            img.setAttribute('src', url);
             img.onerror = function() {
-                img.setAttribute("src", "/assets/frontend/defaulticons/default-item.png");
+                img.setAttribute('src', '/assets/frontend/defaulticons/default-item.png');
             }
-            uploadContainer.querySelector(".sirius-upload-content input[name]").value = url;
-            resetButton.classList.remove("d-none");
+            uploadContainer.querySelector('input[name]').value = url;
+            resetButton.classList.remove('d-none');
+            body.errorCol.innerHTML = '';
             $(triggerElement).popover('hide');
+        } else {
+            addErrorMessage(i18n.errorMsg);
         }
     };
+
+    /**
+     * Adds an error message next to the input field.
+     * </p>
+     * Only one message can be shown at the same time.
+     *
+     * @param message the message that is shown next to the input field
+     */
+    function addErrorMessage(message) {
+        let msg = document.createElement('div');
+        msg.innerHTML = '<div class="card full-border border-sirius-red-dark mb-3"><div class="card-body p-2 msgContent"></div></div>';
+        msg.querySelector('.msgContent').textContent = message;
+        body.errorCol.innerHTML = '';
+        body.errorCol.appendChild(msg);
+        $(triggerElement).popover('update');
+    }
 }
 
