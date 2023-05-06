108ea04762219d0cedc3eabaa17103cdaec7b8c0
Christian Schierle <csc@scireum.de>
2023-05-06 15:49:13 +0200

    Updates to the latest Gradle (8.1.1) and sirius-parent (11.0.1) and revamps project config
    Fixes: SIRI-648

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,92 +6,62 @@
  * http://www.scireum.de - info@scireum.de
  */
 
-group = "sirius-biz"
-version = "DEVELOPMENT-SNAPSHOT"
-
-buildscript {
-    repositories {
-        mavenLocal()
-        maven("https://mvn.scireum.com")
-        maven("https://mvn-closed.scireum.com")
-    }
-    dependencies {
-        classpath("com.scireum:sirius-parent:1.0-RC1")
-    }
-}
-
 plugins {
-    application
+    id("java-library")
+    id("com.scireum.sirius-parent") version "11.0.1"
+    id("org.sonarqube") version "3.4.0.2513"
 }
 
 apply(plugin = "com.scireum.sirius-parent")
 
-application {
-    mainClass.set("sirius.kernel.Setup")
-}
-
 dependencies {
-    val kernelVersion = "dev-31.3.0"
-    val webVersion = "dev-52.5.0"
-    val dbVersion = "dev-41.10.1"
-
-    implementation("com.scireum:sirius-kernel") {
-        version {
-            strictly(kernelVersion)
+    api("com.scireum:sirius-kernel:${property("sirius-kernel")}")
+    testImplementation("com.scireum:sirius-kernel:${property("sirius-kernel")}") {
+        artifact {
+            classifier = "tests"
         }
     }
-    implementation("com.scireum:sirius-web") {
-        version {
-            strictly(webVersion)
+    api("com.scireum:sirius-web:${property("sirius-web")}")
+    testImplementation("com.scireum:sirius-web:${property("sirius-web")}") {
+        artifact {
+            classifier = "tests"
         }
     }
-    implementation("com.scireum:sirius-db") {
-        version {
-            strictly(dbVersion)
-        }
-    }
-
-    testImplementation(group = "com.scireum", name = "sirius-kernel", classifier = "tests") {
-        version {
-            strictly(kernelVersion)
-        }
-    }
-    testImplementation(group = "com.scireum", name = "sirius-web", classifier = "tests") {
-        version {
-            strictly(webVersion)
-        }
-    }
-    testImplementation(group = "com.scireum", name = "sirius-db", classifier = "tests") {
-        version {
-            strictly(dbVersion)
+    api("com.scireum:sirius-db:${property("sirius-db")}")
+    testImplementation("com.scireum:sirius-db:${property("sirius-db")}") {
+        artifact {
+            classifier = "tests"
         }
     }
 
-    implementation("org.mariadb.jdbc:mariadb-java-client:2.7.4")
+
+    implementation("org.mariadb.jdbc:mariadb-java-client:3.0.6")
 
     implementation("net.sf.jt400:jt400:10.7")
 
-    implementation("ru.yandex.clickhouse:clickhouse-jdbc:0.2.6")
+    implementation("ru.yandex.clickhouse:clickhouse-jdbc:0.3.2-patch11")
     // Required as the version brought by clickhouse-jdbc contains security issues
-    implementation("com.fasterxml.jackson.core:jackson-core:2.13.3")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
+    implementation("commons-codec:commons-codec:1.13")
+    // Required as the version brought by clickhouse-jdbc contains security issues
+    implementation("com.fasterxml.jackson.core:jackson-core:2.14.0-rc1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0-rc1")
 
-    implementation("org.apache.ftpserver:ftpserver-core:1.1.2")
+    implementation("org.apache.ftpserver:ftpserver-core:1.2.0")
 
-    implementation("org.apache.sshd:sshd-core:2.8.0")
-    implementation("org.apache.sshd:sshd-sftp:2.8.0")
-    implementation("org.apache.sshd:sshd-scp:2.8.0")
+    implementation("org.apache.sshd:sshd-core:2.9.2")
+    implementation("org.apache.sshd:sshd-sftp:2.9.2")
+    implementation("org.apache.sshd:sshd-scp:2.9.2")
 
-    implementation("org.codelibs:jcifs:2.1.31")
+    implementation("org.codelibs:jcifs:2.1.32")
     // Required as the version provided by jcifs has security issues
     implementation("org.bouncycastle:bcprov-jdk15on:1.70")
 
-    implementation("com.amazonaws:aws-java-sdk-s3:1.12.261")
+    implementation("com.amazonaws:aws-java-sdk-s3:1.12.272")
+    implementation("javax.xml.bind:jaxb-api:2.3.1")
     implementation("commons-io:commons-io:2.11.0")
-    implementation("commons-net:commons-net:3.8.0")
+    implementation("commons-net:commons-net:3.9.0")
     implementation("com.rometools:rome:1.18.0")
     implementation("net.sf.sevenzipjbinding:sevenzipjbinding:16.02-2.01")
     implementation("net.sf.sevenzipjbinding:sevenzipjbinding-all-platforms:16.02-2.01")
-    implementation("org.yaml:snakeyaml:1.30")
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,20 @@
-rootProject.name = "sirius-biz"
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
 
+pluginManagement {
+    repositories {
+        mavenLocal()
+        maven {
+            name = "scireum-mvn"
+            url = uri("https://mvn.scireum.com")
+        }
+        gradlePluginPortal()
+    }
+}
+
+rootProject.name = "sirius-biz"
