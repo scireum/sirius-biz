bc8645bea6d9db1bb91dc8ccef89c49da5644adf
Christian Schierle <csc@scireum.de>
2023-05-17 16:58:02 +0200

    Groups dependencies by type
    Fixes: SIRI-648

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,10 +34,20 @@ dependencies {
         }
     }
 
+    api("com.amazonaws:aws-java-sdk-s3:1.12.272")
+    api("net.sf.sevenzipjbinding:sevenzipjbinding:16.02-2.01")
 
+    implementation("net.sf.sevenzipjbinding:sevenzipjbinding-all-platforms:16.02-2.01")
     implementation("org.mariadb.jdbc:mariadb-java-client:3.0.6")
-
     implementation("net.sf.jt400:jt400:10.7")
+    implementation("org.apache.ftpserver:ftpserver-core:1.2.0")
+    implementation("org.apache.sshd:sshd-core:2.9.2")
+    implementation("org.apache.sshd:sshd-sftp:2.9.2")
+    implementation("org.apache.sshd:sshd-scp:2.9.2")
+    implementation("javax.xml.bind:jaxb-api:2.3.1")
+    implementation("commons-io:commons-io:2.11.0")
+    implementation("commons-net:commons-net:3.9.0")
+    implementation("com.rometools:rome:1.18.0")
 
     implementation("com.clickhouse:clickhouse-jdbc:0.3.2-patch11")
     // Required as the version brought by clickhouse-jdbc contains security issues
@@ -46,23 +56,9 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-core")
     implementation("com.fasterxml.jackson.core:jackson-databind")
 
-    implementation("org.apache.ftpserver:ftpserver-core:1.2.0")
-
-    implementation("org.apache.sshd:sshd-core:2.9.2")
-    implementation("org.apache.sshd:sshd-sftp:2.9.2")
-    implementation("org.apache.sshd:sshd-scp:2.9.2")
-
     implementation("org.codelibs:jcifs:2.1.32")
     // Required as the version provided by jcifs has security issues
     implementation("org.bouncycastle:bcprov-jdk15on:1.70")
-
-    api("com.amazonaws:aws-java-sdk-s3:1.12.272")
-    implementation("javax.xml.bind:jaxb-api:2.3.1")
-    implementation("commons-io:commons-io:2.11.0")
-    implementation("commons-net:commons-net:3.9.0")
-    implementation("com.rometools:rome:1.18.0")
-    api("net.sf.sevenzipjbinding:sevenzipjbinding:16.02-2.01")
-    implementation("net.sf.sevenzipjbinding:sevenzipjbinding-all-platforms:16.02-2.01")
 }
 
 sonarqube {
