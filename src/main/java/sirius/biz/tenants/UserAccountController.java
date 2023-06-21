3ff0bdbf4ceaf77e175423500fc84f606e324a73
Christian Schierle <csc@scireum.de>
2023-06-21 08:30:59 +0200

    Adds filter for locked users to select user template
    Fixes: OX-8902

--- src/main/java/sirius/biz/tenants/UserAccountController.java
+++ src/main/java/sirius/biz/tenants/UserAccountController.java
@@ -125,7 +125,9 @@ public abstract class UserAccountController<I extends Serializable, T extends Ba
                 getUsersAsPage(webContext).addBooleanFacet(UserAccount.USER_ACCOUNT_DATA.inner(UserAccountData.LOGIN)
                                                                                         .inner(LoginData.ACCOUNT_LOCKED)
                                                                                         .toString(),
-                                                           NLS.get("LoginData.accountLocked")).withTotalCount().asPage();
+                                                           NLS.get("LoginData.accountLocked"))
+                                          .withTotalCount()
+                                          .asPage();
 
         webContext.respondWith().template("/templates/biz/tenants/user-accounts.html.pasta", accounts, getUserClass());
     }
@@ -201,7 +203,7 @@ public abstract class UserAccountController<I extends Serializable, T extends Ba
 
                                                             List<String> accessiblePermissions = getRoles();
                                                             packages.loadAccessiblePermissions(webContext.getParameters(
-                                                                    "roles"),
+                                                                                                       "roles"),
                                                                                                accessiblePermissions::contains,
                                                                                                userAccount.getUserAccountData()
                                                                                                           .getPermissions()
@@ -600,7 +602,14 @@ public abstract class UserAccountController<I extends Serializable, T extends Ba
     @LoginRequired
     @Permission(TenantUserManager.PERMISSION_SELECT_USER_ACCOUNT)
     public void selectUserAccounts(WebContext webContext) {
-        Page<U> selectableUsers = getSelectableUsersAsPage().withContext(webContext).asPage();
+        Page<U> selectableUsers = getSelectableUsersAsPage().withContext(webContext)
+                                                            .addBooleanFacet(UserAccount.USER_ACCOUNT_DATA.inner(
+                                                                                                UserAccountData.LOGIN)
+                                                                                                          .inner(LoginData.ACCOUNT_LOCKED)
+                                                                                                          .toString(),
+                                                                             NLS.get("LoginData.accountLocked"))
+                                                            .withTotalCount()
+                                                            .asPage();
         fillTenantsFromCache(selectableUsers);
 
         webContext.respondWith()
