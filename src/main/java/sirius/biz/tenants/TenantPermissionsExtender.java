c0a5f17455399d6a43e09c39c8df24a3077e7ca7
Jakob Vogel <jvo@scireum.de>
2023-06-29 12:29:39 +0200

    Extends tenant export to include permissions 🚓
    OX-10040

---
+++ src/main/java/sirius/biz/tenants/TenantPermissionsExtender.java
@@ -0,0 +1,66 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.tenants;
+
+import sirius.biz.importer.BaseImportHandler;
+import sirius.biz.importer.EntityImportHandlerExtender;
+import sirius.biz.importer.ImporterContext;
+import sirius.biz.importer.format.FieldDefinition;
+import sirius.biz.tenants.jdbc.SQLTenantImportHandler;
+import sirius.biz.tenants.mongo.MongoTenantImportHandler;
+import sirius.db.mixing.BaseEntity;
+import sirius.db.mixing.EntityDescriptor;
+import sirius.db.mixing.Mapping;
+import sirius.kernel.commons.Explain;
+import sirius.kernel.di.std.Register;
+
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+
+@Register
+public class TenantPermissionsExtender implements EntityImportHandlerExtender {
+
+    private static final Mapping PERMISSIONS = Mapping.named("permissions");
+
+    @Override
+    public FieldDefinition resolveCustomField(BaseImportHandler<? extends BaseEntity<?>> handler,
+                                              EntityDescriptor descriptor,
+                                              String field) {
+        if (checkHandler(handler) && PERMISSIONS.getName().equals(field)) {
+            return FieldDefinition.stringField(field).withLabel("$Tenant.permissions").addAlias("permissions");
+        }
+        return null;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    @Explain("We know that the extractor is only called for the correct type.")
+    public <E extends BaseEntity<?>> Function<? super E, ?> createExtractor(BaseImportHandler<E> handler,
+                                                                            EntityDescriptor descriptor,
+                                                                            ImporterContext context,
+                                                                            String fieldToExport) {
+        if (checkHandler(handler) && PERMISSIONS.getName().equals(fieldToExport)) {
+            return tenant -> String.join(",", tenant.as(Tenant.class).getPermissions());
+        }
+        return null;
+    }
+
+    @Override
+    public void collectDefaultExportableMappings(BaseImportHandler<? extends BaseEntity<?>> handler,
+                                                 EntityDescriptor descriptor,
+                                                 BiConsumer<Integer, Mapping> collector) {
+        if (checkHandler(handler)) {
+            collector.accept(185, PERMISSIONS);
+        }
+    }
+
+    private boolean checkHandler(BaseImportHandler<? extends BaseEntity<?>> handler) {
+        return handler instanceof MongoTenantImportHandler || handler instanceof SQLTenantImportHandler;
+    }
+}
