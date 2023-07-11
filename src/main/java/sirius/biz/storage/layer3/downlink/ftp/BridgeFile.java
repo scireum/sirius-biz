2ed126c6ac023e9dbb90d520a4ad25b5dac62624
Matthias Keck <mke@scireum.de>
2023-07-11 10:07:06 +0200

    Fixes ftp upload
    - checks readonly state of non-existing file at parent
    
    Fixes: SIRI-839

--- src/main/java/sirius/biz/storage/layer3/downlink/ftp/BridgeFile.java
+++ src/main/java/sirius/biz/storage/layer3/downlink/ftp/BridgeFile.java
@@ -88,12 +88,19 @@ class BridgeFile implements FtpFile {
 
     @Override
     public boolean isRemovable() {
-        return !file.readOnly();
+        return isWritable();
     }
 
+    // Hier
     @Override
     public boolean isWritable() {
-        return !file.readOnly();
+        if (file != null) {
+            return !file.readOnly();
+        }
+        if (parent != null) {
+            return !parent.readOnly();
+        }
+        throw new IllegalArgumentException("BridgeFile not initialized properly");
     }
 
     @Override
