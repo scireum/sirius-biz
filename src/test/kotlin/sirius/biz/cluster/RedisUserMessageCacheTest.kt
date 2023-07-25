b7874a171d54060814f9bef752ecdcf40d0078d8
Sascha Bieberstein <sbi@scireum.de>
2023-07-25 14:43:10 +0200

    Migrates some tests to jUnit5 + Kotlin

---
+++ src/test/kotlin/sirius/biz/cluster/RedisUserMessageCacheTest.kt
@@ -0,0 +1,42 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.cluster
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.kernel.SiriusExtension
+import sirius.kernel.di.std.Part
+import sirius.web.controller.Message
+import sirius.web.controller.MessageLevel
+import sirius.web.http.DistributedUserMessageCache
+import java.util.*
+
+@ExtendWith(SiriusExtension::class)
+class RedisUserMessageCacheTest {
+
+    companion object {
+        @Part
+        private lateinit var cache: DistributedUserMessageCache
+    }
+
+    @Test
+    fun `test put value and then get value works, but second get is empty`() {
+        val key = "key" + System.currentTimeMillis()
+
+        cache.put(key, Collections.singletonList(Message.error().withHTMLMessage("Test error message")))
+
+        val result = cache.getAndRemove(key)
+        assertEquals(1, result.size)
+        assertTrue(result[0].html.contains("Test error message"))
+        assertEquals(MessageLevel.PROBLEM, result[0].type)
+        assertTrue(cache.getAndRemove(key).isEmpty())
+    }
+}
---
+++ src/test/kotlin/sirius/biz/elastic/SearchableEntityTest.kt
@@ -0,0 +1,85 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.elastic
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import sirius.kernel.SiriusExtension
+import java.util.*
+import java.util.stream.Stream
+
+@ExtendWith(SiriusExtension::class)
+class SearchableEntityTest {
+
+    companion object {
+        @JvmStatic
+        fun generateTokenSplittingTestCases(): Stream<Arguments> {
+            return Stream.of(
+                    Arguments.of(
+                            "max.mustermann@website.com",
+                            listOf(
+                                    "com",
+                                    "max",
+                                    "max.mustermann",
+                                    "max.mustermann@website.com",
+                                    "mustermann",
+                                    "website",
+                                    "website.com"
+                            )
+                    ),
+                    Arguments.of("test-foobar", listOf("foobar", "test", "test-foobar")),
+                    Arguments.of(
+                            "test123@bla-bar.foo",
+                            listOf("bar", "bla", "bla-bar.foo", "foo", "test123", "test123@bla-bar.foo")
+                    )
+            )
+        }
+    }
+
+    @Test
+    fun `Tokenizing works`() {
+        val entity = SearchableTestEntity()
+        entity.test = "This is a test"
+        entity.unsearchableTest = "Secret Content"
+        entity.searchableContent =
+                "email:test@test.local 12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+        entity.updateSearchField()
+
+        val tokens = entity.searchField.split(" ").toSet()
+        assertTrue(tokens.contains("this"))
+        assertTrue(tokens.contains("test"))
+        assertTrue(tokens.contains("email"))
+        assertFalse(tokens.contains("secret"))
+        assertFalse(tokens.contains("content"))
+        assertTrue(tokens.contains("test"))
+        assertTrue(tokens.contains("local"))
+        assertFalse(
+                tokens.contains(
+                        "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890"
+                )
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("generateTokenSplittingTestCases")
+    fun `Splitting tokens works as intended`(input: String, output: List<String>) {
+        val entity = SearchableTestEntity()
+        entity.test = input
+        entity.updateSearchField()
+
+        assertEquals(
+                output,
+                entity.searchField.split(" ").filter { it.isNotEmpty() }.toMutableList().sorted(),
+        )
+    }
+}
