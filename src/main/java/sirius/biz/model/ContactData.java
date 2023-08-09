6394a1ec1774610c919968638d3923b1938cf2b4
Sascha Bieberstein <sbi@scireum.de>
2023-08-09 14:27:22 +0200

    Uses property validator for contact phone number fields
    Fixes: SIRI-854

--- src/main/java/sirius/biz/model/ContactData.java
+++ src/main/java/sirius/biz/model/ContactData.java
@@ -10,6 +10,7 @@ package sirius.biz.model;
 
 import sirius.biz.importer.AutoImport;
 import sirius.biz.tenants.EmailAddressValidator;
+import sirius.biz.tenants.PhoneNumberValidator;
 import sirius.biz.web.Autoloaded;
 import sirius.db.mixing.Composite;
 import sirius.db.mixing.Mapping;
@@ -18,16 +19,7 @@ import sirius.db.mixing.annotations.LowerCase;
 import sirius.db.mixing.annotations.NullAllowed;
 import sirius.db.mixing.annotations.Trim;
 import sirius.db.mixing.annotations.ValidatedBy;
-import sirius.kernel.commons.Strings;
-import sirius.kernel.di.std.Part;
-import sirius.kernel.health.Exceptions;
-import sirius.kernel.health.HandledException;
-import sirius.kernel.nls.NLS;
 import sirius.pasta.noodle.sandbox.NoodleSandbox;
-import sirius.web.mails.Mails;
-
-import java.util.function.Consumer;
-import java.util.regex.Pattern;
 
 /**
  * Provides various contact information for a person or company which can be embedded into other entities or mixins.
@@ -39,17 +31,6 @@ import java.util.regex.Pattern;
  * a {@link sirius.db.mixing.BaseEntity#isChanged(Mapping...)} check to only validate or verify new values.
  */
 public class ContactData extends Composite {
-
-    /**
-     * Matches a part of a phone number like <tt>55 55</tt> or <tt>55</tt>.
-     */
-    private static final String NUMERIC_PART = "( *\\d+)*+";
-
-    /**
-     * Validates a phone number.
-     */
-    public static final Pattern VALID_PHONE_NUMBER =
-            Pattern.compile("\\+?\\d+" + NUMERIC_PART + "( */" + NUMERIC_PART + ")?( *-" + NUMERIC_PART + ")?");
 
     /**
      * Contains an email address.
@@ -78,6 +59,7 @@ public class ContactData extends Composite {
     @Autoloaded
     @AutoImport
     @Length(150)
+    @ValidatedBy(PhoneNumberValidator.class)
     private String phone;
 
     /**
@@ -92,6 +74,7 @@ public class ContactData extends Composite {
     @Autoloaded
     @AutoImport
     @Length(150)
+    @ValidatedBy(PhoneNumberValidator.class)
     private String fax;
 
     /**
@@ -106,102 +89,8 @@ public class ContactData extends Composite {
     @Autoloaded
     @AutoImport
     @Length(150)
+    @ValidatedBy(PhoneNumberValidator.class)
     private String mobile;
-
-    /**
-     * Verifies that the given phone number is valid using {@link #VALID_PHONE_NUMBER}.
-     * <p>
-     * This is intended to be invoked within a {@link sirius.db.mixing.annotations.BeforeSave} handler.
-     * Note that this will skip empty values.
-     *
-     * @throws HandledException in case of an invalid phone number
-     */
-    public void verifyPhoneNumber() {
-        if (Strings.isFilled(phone) && !VALID_PHONE_NUMBER.matcher(phone).matches()) {
-            throw invalidPhoneException(NLS.get("ContactData.phone"), phone);
-        }
-    }
-
-    /**
-     * Validates that the given phone number is valid using {@link #VALID_PHONE_NUMBER}.
-     * <p>
-     * This is intended to be invoked within a {@link sirius.db.mixing.annotations.OnValidate} handler.
-     * Note that this will skip empty values.
-     *
-     * @param validationMessageConsumer the consumer which is used to collect validation messages. This is normally
-     *                                  passed into the on validate method and can simply be forwarded here.
-     */
-    public void validatePhoneNumber(Consumer<String> validationMessageConsumer) {
-        if (Strings.isFilled(phone) && !VALID_PHONE_NUMBER.matcher(phone).matches()) {
-            validationMessageConsumer.accept(invalidPhoneException(NLS.get("ContactData.phone"), phone).getMessage());
-        }
-    }
-
-    /**
-     * Verifies that the given mobile phone number is valid using {@link #VALID_PHONE_NUMBER}.
-     * <p>
-     * This is intended to be invoked within a {@link sirius.db.mixing.annotations.BeforeSave} handler.
-     * Note that this will skip empty values.
-     *
-     * @throws HandledException in case of an invalid mobile phone number
-     */
-    public void verifyMobileNumber() {
-        if (Strings.isFilled(phone) && !VALID_PHONE_NUMBER.matcher(phone).matches()) {
-            throw invalidPhoneException(NLS.get("ContactData.mobile"), phone);
-        }
-    }
-
-    /**
-     * Validates that the given mobile phone number is valid using {@link #VALID_PHONE_NUMBER}.
-     * <p>
-     * This is intended to be invoked within a {@link sirius.db.mixing.annotations.OnValidate} handler.
-     * Note that this will skip empty values.
-     *
-     * @param validationMessageConsumer the consumer which is used to collect validation messages. This is normally
-     *                                  passed into the on validate method and can simply be forwarded here.
-     */
-    public void validateMobileNumber(Consumer<String> validationMessageConsumer) {
-        if (Strings.isFilled(mobile) && !VALID_PHONE_NUMBER.matcher(mobile).matches()) {
-            validationMessageConsumer.accept(invalidPhoneException(NLS.get("ContactData.mobile"), mobile).getMessage());
-        }
-    }
-
-    /**
-     * Verifies that the given fax number is valid using {@link #VALID_PHONE_NUMBER}.
-     * <p>
-     * This is intended to be invoked within a {@link sirius.db.mixing.annotations.BeforeSave} handler.
-     * Note that this will skip empty values.
-     *
-     * @throws HandledException in case of an invalid fax number
-     */
-    public void verifyFaxNumber() {
-        if (Strings.isFilled(fax) && !VALID_PHONE_NUMBER.matcher(fax).matches()) {
-            throw invalidPhoneException(NLS.get("ContactData.fax"), fax);
-        }
-    }
-
-    /**
-     * Validates that the given fax number is valid using {@link #VALID_PHONE_NUMBER}.
-     * <p>
-     * This is intended to be invoked within a {@link sirius.db.mixing.annotations.OnValidate} handler.
-     * Note that this will skip empty values.
-     *
-     * @param validationMessageConsumer the consumer which is used to collect validation messages. This is normally
-     *                                  passed into the on validate method and can simply be forwarded here.
-     */
-    public void validateFaxNumber(Consumer<String> validationMessageConsumer) {
-        if (Strings.isFilled(fax) && !VALID_PHONE_NUMBER.matcher(fax).matches()) {
-            validationMessageConsumer.accept(invalidPhoneException(NLS.get("ContactData.fax"), fax).getMessage());
-        }
-    }
-
-    private HandledException invalidPhoneException(String field, String value) {
-        return Exceptions.createHandled()
-                         .withNLSKey("ContactData.invalidPhone")
-                         .set("field", field)
-                         .set("value", value)
-                         .handle();
-    }
 
     @NoodleSandbox(NoodleSandbox.Accessibility.GRANTED)
     public String getEmail() {
