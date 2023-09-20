50e9b0004f31f7fd334de8af00de4a9c5de85419
bko <bko@scireum.de>
2023-09-20 12:11:19 +0200

    Refactors javascript code
    The JavaScript code is pulled out of the .pasta file and style classes are extended with the -js postfix to show that they are used in the JavaScript.
    
    SIRI-877

---
+++ src/main/resources/default/assets/scripts/list-layout-switch.js
@@ -0,0 +1,23 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Remshalden, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+function extendUrlWithDisplayMode(currentLocation, value) {
+    const key = "display-mode";
+    const url = new URL(currentLocation);
+    url.searchParams.delete(key);
+    url.searchParams.append(key, value);
+    return url.toString();
+}
+
+sirius.ready(function() {
+    document.querySelectorAll("a.list-link-cards-js").forEach(function (_link) {
+        _link.href = extendUrlWithDisplayMode(window.location.href, "CARDS");
+    });
+    document.querySelectorAll("a.list-link-list-js").forEach(function (_link) {
+        _link.href = extendUrlWithDisplayMode(window.location.href, "LIST");
+    });
+});
