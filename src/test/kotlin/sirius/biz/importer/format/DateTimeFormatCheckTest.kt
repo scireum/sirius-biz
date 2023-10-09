62f8f507d5e98e14fb22aa1614209258bc35e7d1
Sascha Bieberstein <sbi@scireum.de>
2023-10-09 17:34:20 +0200

    Improves grouping of test cases
    Fixes: OX-10357

--- src/test/kotlin/sirius/biz/importer/format/DateTimeFormatCheckTest.kt
+++ src/test/kotlin/sirius/biz/importer/format/DateTimeFormatCheckTest.kt
@@ -8,7 +8,6 @@
 
 package sirius.biz.importer.format
 
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
@@ -16,41 +15,16 @@ import sirius.kernel.commons.Value
 
 class DateTimeFormatCheckTest {
 
-    @Test
-    fun `valid dates throws no exception`() {
-        DateTimeFormatCheck("dd.MM.uuuu").perform(Value.of("23.10.2019"))
-        DateTimeFormatCheck("dd.MM.uuuu").perform(Value.of("01.05.1854"))
-        DateTimeFormatCheck("dd.MM.uuuu").perform(Value.of("24.12.9000"))
-    }
-
-    @Test
-    fun `invalid date throws exception`() {
-        assertThrows<IllegalArgumentException> {
-            DateTimeFormatCheck("dd.MM.uuuu").perform(Value.of("31.09.2019"))
-        }
-    }
-
-    @Test
-    fun `date with too few numbers is invalid`() {
-        assertThrows<IllegalArgumentException> {
-            DateTimeFormatCheck("dd.MM.uuuu").perform(Value.of("4.4.19"))
-        }
-    }
-
-    @Test
-    fun `date with too many numbers is invalid`() {
-        assertThrows<IllegalArgumentException> {
-            DateTimeFormatCheck("dd.MM.uuuu").perform(Value.of("4.011.19"))
-        }
-    }
-
     @CsvSource(
             delimiter = '|', useHeadersInDisplayName = true, textBlock = """
         format   | date
         ddMMuuuu | 1092019
         ddMMuuuu | '1092019'
         ddMMuuuu | 'TEST'
-        ddMMuuuu | '01.09.2019'"""
+        ddMMuuuu | '01.09.2019'
+        dd.MM.uuuu | 31.09.2019
+        dd.MM.uuuu | 4.4.19
+        dd.MM.uuuu | 4.011.19"""
     )
     @ParameterizedTest
     fun `dates not matching the provided format are correctly marked as invalid`(format: String, date: String) {
@@ -65,7 +39,10 @@ class DateTimeFormatCheckTest {
         ddMMuuuu   | 11092019
         ddMMuuuu   | '01092019'
         ddMMuuuu   | '11092019'
-        dd.MM.uuuu | '01.09.2019'"""
+        dd.MM.uuuu | '01.09.2019'
+        dd.MM.uuuu | 23.10.2019
+        dd.MM.uuuu | 01.05.1854
+        dd.MM.uuuu | 24.12.9000"""
     )
     @ParameterizedTest
     fun `dates matching the provided format are correctly marked as valid`(format: String, date: String) {
