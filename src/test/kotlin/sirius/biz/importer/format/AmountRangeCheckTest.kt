4e640559105c89074c351b5de2c950acec37b2a5
Sascha Bieberstein <sbi@scireum.de>
2023-10-09 17:18:45 +0200

    Converts syntax of test to Kotlin
    Fixes: OX-10357

--- src/test/kotlin/sirius/biz/importer/format/AmountRangeCheckTest.kt
+++ src/test/kotlin/sirius/biz/importer/format/AmountRangeCheckTest.kt
@@ -8,104 +8,100 @@
 
 package sirius.biz.importer.format
 
-class AmountRangeCheckTest extends BaseSpecification {
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import sirius.kernel.commons.Amount
+import sirius.kernel.commons.NumberFormat
+import sirius.kernel.commons.Value
 
-    def "values below the inclusive lower limit are correctly marked as invalid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitInclusive(Amount.ONE).perform(Value.of(0))
-        then:
-        thrown IllegalArgumentException
+/**
+ * Tests the [AmountRangeCheck] class.
+ */
+class AmountRangeCheckTest {
+
+    @Test
+    fun `values below the inclusive lower limit are correctly marked as invalid`() {
+        assertThrows<IllegalArgumentException> {
+            AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitInclusive(Amount.ONE).perform(Value.of(0))
+        }
     }
 
-    def "values equal to the inclusive lower limit are correctly marked as valid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitInclusive(Amount.ONE).perform(Value.of(1))
-        then:
-        noExceptionThrown()
+    @Test
+    fun `values equal to the inclusive lower limit are correctly marked as valid`() {
+        AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitInclusive(Amount.ONE).perform(Value.of(1))
     }
 
-    def "values above the inclusive lower limit are correctly marked as valid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitInclusive(Amount.ONE).perform(Value.of(2))
-        then:
-        noExceptionThrown()
+    @Test
+    fun `values above the inclusive lower limit are correctly marked as valid`() {
+        AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitInclusive(Amount.ONE).perform(Value.of(2))
     }
 
-    def "values below the exclusive lower limit are correctly marked as invalid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitExclusive(Amount.ONE).perform(Value.of(0))
-        then:
-        thrown IllegalArgumentException
+    @Test
+    fun `values below the exclusive lower limit are correctly marked as invalid`() {
+        assertThrows<IllegalArgumentException> {
+            AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitExclusive(Amount.ONE)
+                    .perform(Value.of(0))
+        }
     }
 
-    def "values equal to the exclusive lower limit are correctly marked as invalid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitExclusive(Amount.ONE).perform(Value.of(1))
-        then:
-        thrown IllegalArgumentException
+    @Test
+    fun `values equal to the exclusive lower limit are correctly marked as invalid`() {
+        assertThrows<IllegalArgumentException> {
+            AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitExclusive(Amount.ONE)
+                    .perform(Value.of(1))
+        }
     }
 
-    def "values above the exclusive lower limit are correctly marked as valid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitExclusive(Amount.ONE).perform(Value.of(2))
-        then:
-        noExceptionThrown()
+    @Test
+    fun `values above the exclusive lower limit are correctly marked as valid`() {
+        AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitExclusive(Amount.ONE).perform(Value.of(2))
     }
 
-    def "values above the inclusive upper limit are correctly marked as invalid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitInclusive(Amount.ONE_HUNDRED).
-        perform(Value.of(101))
-        then:
-        thrown IllegalArgumentException
+    @Test
+    fun `values above the inclusive upper limit are correctly marked as invalid`() {
+        assertThrows<IllegalArgumentException> {
+            AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitInclusive(Amount.ONE_HUNDRED)
+                    .perform(Value.of(101))
+        }
     }
 
-    def "values equal to the inclusive upper limit are correctly marked as valid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitInclusive(Amount.ONE_HUNDRED).
-        perform(Value.of(100))
-        then:
-        noExceptionThrown()
+    @Test
+    fun `values equal to the inclusive upper limit are correctly marked as valid`() {
+        AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitInclusive(Amount.ONE_HUNDRED)
+                .perform(Value.of(100))
     }
 
-    def "values below the inclusive upper limit are correctly marked as valid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitInclusive(Amount.ONE_HUNDRED).
-        perform(Value.of(99))
-        then:
-        noExceptionThrown()
+    @Test
+    fun `values below the inclusive upper limit are correctly marked as valid`() {
+        AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitInclusive(Amount.ONE_HUNDRED)
+                .perform(Value.of(99))
     }
 
-    def "values above the exclusive upper limit are correctly marked as invalid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitExclusive(Amount.ONE_HUNDRED).
-        perform(Value.of(101))
-        then:
-        thrown IllegalArgumentException
+    @Test
+    fun `values above the exclusive upper limit are correctly marked as invalid`() {
+        assertThrows<IllegalArgumentException> {
+            AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitExclusive(Amount.ONE_HUNDRED)
+                    .perform(Value.of(101))
+        }
     }
 
-    def "values equal to the exclusive upper limit are correctly marked as invalid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitExclusive(Amount.ONE_HUNDRED).
-        perform(Value.of(100))
-        then:
-        thrown IllegalArgumentException
+    @Test
+    fun `values equal to the exclusive upper limit are correctly marked as invalid`() {
+        assertThrows<IllegalArgumentException> {
+            AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitExclusive(Amount.ONE_HUNDRED)
+                    .perform(Value.of(100))
+        }
     }
 
-    def "values below the exclusive upper limit are correctly marked as valid"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitExclusive(Amount.ONE_HUNDRED).
-        perform(Value.of(99))
-        then:
-        noExceptionThrown()
+    @Test
+    fun `values below the exclusive upper limit are correctly marked as valid`() {
+        AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withUpperLimitExclusive(Amount.ONE_HUNDRED)
+                .perform(Value.of(99))
     }
 
-    def "empty values are ignored"() {
-        when:
-        new AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitInclusive(Amount.ONE).
-        withUpperLimitExclusive(Amount.ONE_HUNDRED).
-        perform(Value.of(Amount.NOTHING))
-        then:
-        noExceptionThrown()
+    @Test
+    fun `empty values are ignored`() {
+        AmountRangeCheck(NumberFormat.NO_DECIMAL_PLACES).withLowerLimitInclusive(Amount.ONE)
+                .withUpperLimitExclusive(Amount.ONE_HUNDRED).perform(Value.of(Amount.NOTHING))
     }
 }
