ef7da42cdb40770f23555f92601ef7208a17f393
Sascha Bieberstein <sbi@scireum.de>
2023-10-10 08:56:42 +0200

    Converts syntax of test to Kotlin
    Fixes: OX-10357

--- src/test/kotlin/sirius/biz/importer/format/ValueCheckTest.kt
+++ src/test/kotlin/sirius/biz/importer/format/ValueCheckTest.kt
@@ -8,114 +8,138 @@
 
 package sirius.biz.importer.format
 
-class ValueCheckTest extends BaseSpecification {
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import sirius.kernel.commons.Value
 
-    def "amount scale check correctly marks non numeric values as invalid"() {
-        when:
-        AmountScaleCheck scaleCheck = new AmountScaleCheck(5, 2)
-        Value number = Value.of("TEST")
-        and:
-        scaleCheck.perform(number)
-        then:
-        thrown IllegalArgumentException
+/**
+ * Tests the [ValueCheck] class.
+ */
+class ValueCheckTest {
+
+    @Test
+    fun `amount scale check correctly marks non numeric values as invalid`() {
+        val scaleCheck = AmountScaleCheck(5, 2)
+        val number = Value.of("TEST")
+        assertThrows<IllegalArgumentException> {
+            scaleCheck.perform(number)
+        }
     }
 
-    def "numbers exceeding the provided precision are correctly marked as invalid"() {
-        when:
-        new AmountScaleCheck(5, 2).perform(Value.of(number))
-        then:
-        thrown IllegalArgumentException
-                where:
-        number << [1234, "1234", 123456, "123456", 1234.56, "1234.56"]
+    @CsvSource(
+            delimiter = '|', useHeadersInDisplayName = true, textBlock = """
+        number
+        1234
+        '1234'
+        123456
+        '123456'
+        1234.56
+        '1234.56'"""
+    )
+    @ParameterizedTest
+    fun `numbers exceeding the provided precision are correctly marked as invalid`(number: String) {
+        assertThrows<IllegalArgumentException> {
+            AmountScaleCheck(5, 2).perform(Value.of(number))
+        }
     }
 
-    def "numbers exceeding the provided scale are correctly marked as invalid"() {
-        when:
-        new AmountScaleCheck(5, 2).perform(Value.of(number))
-        then:
-        thrown IllegalArgumentException
-                where:
-        number << [0.123, "0.123", 12.345, "12.345"]
+    @CsvSource(
+            delimiter = '|', useHeadersInDisplayName = true, textBlock = """
+        number
+        0.123
+        '0.123'
+        12.345
+        '12.345'"""
+    )
+    @ParameterizedTest
+    fun `numbers exceeding the provided scale are correctly marked as invalid`(number: String) {
+        assertThrows<IllegalArgumentException> {
+            AmountScaleCheck(5, 2).perform(Value.of(number))
+        }
     }
 
-    def "numbers matching the provided scale and precission are correctly marked as valid"() {
-        expect:
-        new AmountScaleCheck(precision, scale).perform(Value.of(number))
-        where:
+    @CsvSource(
+            delimiter = '|', useHeadersInDisplayName = true, textBlock = """
         precision | scale | number
         10        | 0     | 1234512345
-        10        | 0     | "1234512345"
+        10        | 0     | '1234512345'
         5         | 2     | 0.12
-        5         | 2     | "0.12"
+        5         | 2     | '0.12'
         5         | 2     | 1.23
-        5         | 2     | "1.23"
+        5         | 2     | '1.23'
         5         | 2     | 12.34
-        5         | 2     | "12.34"
+        5         | 2     | '12.34'
         5         | 2     | 123.45
-        5         | 2     | "123.45"
+        5         | 2     | '123.45'
         5         | 2     | 1
-        5         | 2     | "1"
+        5         | 2     | '1'
         5         | 2     | 12
-        5         | 2     | "12"
+        5         | 2     | '12'
         5         | 2     | 123
-        5         | 2     | "123"
+        5         | 2     | '123'"""
+    )
+    @ParameterizedTest
+    fun `numbers matching the provided scale and precision are correctly marked as valid`(
+            precision: Int,
+            scale: Int,
+            number: String
+    ) {
+        AmountScaleCheck(precision, scale).perform(Value.of(number))
     }
 
-    def "LenghCheck works as expected"() {
-        when: "Check with proper lengths work and perform an auto-trim"
-        new LengthCheck(5).perform(Value.of("     55555"))
-        new LengthCheck(5).perform(Value.of("55555"))
-        then:
-        noExceptionThrown()
-        when: "Without trimming, the check fails appropriately"
-        new LengthCheck(5).checkUntrimmed().perform(Value.of("     55555"))
-        then:
-        thrown(IllegalArgumentException)
-        when: "The check fails for an input which is too long"
-        new LengthCheck(5).checkUntrimmed().perform(Value.of("555555555"))
-        then:
-        thrown(IllegalArgumentException)
+    @Test
+    fun `LenghCheck works as expected`() {
+        // Check with proper lengths work and perform an auto-trim
+        LengthCheck(5).perform(Value.of("     55555"))
+        LengthCheck(5).perform(Value.of("55555"))
+        // Without trimming, the check fails appropriately
+        assertThrows<IllegalArgumentException> {
+            LengthCheck(5).checkUntrimmed().perform(Value.of("     55555"))
+        }
+        // The check fails for an input which is too long
+        assertThrows<IllegalArgumentException> {
+            LengthCheck(5).checkUntrimmed().perform(Value.of("555555555"))
+        }
     }
 
-    def "RequiredCheck works as expected"() {
-        when: "Check accepts values and whitespaces (when checking untrimmed)"
-        new RequiredCheck().perform(Value.of("55555"))
-        new RequiredCheck().checkUntrimmed().perform(Value.of("  "))
-        then:
-        noExceptionThrown()
-        when: "Check detects an empty input"
-        new RequiredCheck().perform(Value.of(""))
-        then:
-        thrown(IllegalArgumentException)
-        when: "Check detects a null input"
-        new RequiredCheck().perform(Value.EMPTY)
-        then:
-        thrown(IllegalArgumentException)
-        when: "Check detects a whitespace input (when trimming is enabled)"
-        new RequiredCheck().perform(Value.of("  "))
-        then:
-        thrown(IllegalArgumentException)
+    @Test
+    fun `RequiredCheck works as expected`() {
+        // Check accepts values and whitespaces (when checking untrimmed)
+        RequiredCheck().perform(Value.of("55555"))
+        RequiredCheck().checkUntrimmed().perform(Value.of("  "))
+        // Check detects an empty input
+        assertThrows<IllegalArgumentException> {
+            RequiredCheck().perform(Value.of(""))
+        }
+        // Check detects a null input
+        assertThrows<IllegalArgumentException> {
+            RequiredCheck().perform(Value.EMPTY)
+        }
+        // Check detects a whitespace input (when trimming is enabled)
+        assertThrows<IllegalArgumentException> {
+            RequiredCheck().perform(Value.of("  "))
+        }
     }
 
-    def "ValueInListCheck works as expected"() {
-        when: "Check accepts values and whitespaces (when checking trimmed)"
-        new ValueInListCheck("A","B").perform(Value.of("A"))
-        new ValueInListCheck("A","B").perform(Value.of(" A"))
-        new ValueInListCheck("A","B").perform(Value.of("B"))
-        new ValueInListCheck("A","B").perform(Value.of("B "))
-        and: "Check ignores an empty value"
-        new ValueInListCheck("A","B").perform(Value.of(""))
-        new ValueInListCheck("A","B").perform(Value.EMPTY)
-        then:
-        noExceptionThrown()
-        when: "Check detects an invalid input"
-        new ValueInListCheck("A","B").perform(Value.of("C"))
-        then:
-        thrown(IllegalArgumentException)
-        when: "Check detects an invalid input when checking untrimmed"
-        new ValueInListCheck("A","B").checkUntrimmed().perform(Value.of("A "))
-        then:
-        thrown(IllegalArgumentException)
+    @Test
+    fun `ValueInListCheck works as expected`() {
+        // Check accepts values and whitespaces (when checking trimmed)
+        ValueInListCheck("A", "B").perform(Value.of("A"))
+        ValueInListCheck("A", "B").perform(Value.of(" A"))
+        ValueInListCheck("A", "B").perform(Value.of("B"))
+        ValueInListCheck("A", "B").perform(Value.of("B "))
+        // Check ignores an empty value
+        ValueInListCheck("A", "B").perform(Value.of(""))
+        ValueInListCheck("A", "B").perform(Value.EMPTY)
+        // Check detects an invalid input
+        assertThrows<IllegalArgumentException> {
+            ValueInListCheck("A", "B").perform(Value.of("C"))
+        }
+        // Check detects an invalid input when checking untrimmed
+        assertThrows<IllegalArgumentException> {
+            ValueInListCheck("A", "B").checkUntrimmed().perform(Value.of("A "))
+        }
     }
-
 }
