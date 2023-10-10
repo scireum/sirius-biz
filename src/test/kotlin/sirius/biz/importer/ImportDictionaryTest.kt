241c59d44ca0508d3aa360b663f134d252ba8eb9
Sascha Bieberstein <sbi@scireum.de>
2023-10-10 09:09:55 +0200

    Converts syntax of test to Kotlin
    Fixes: OX-10357

--- src/test/kotlin/sirius/biz/importer/ImportDictionaryTest.kt
+++ src/test/kotlin/sirius/biz/importer/ImportDictionaryTest.kt
@@ -8,118 +8,112 @@
 
 package sirius.biz.importer
 
-class ImportDictionaryTest extends BaseSpecification {
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.biz.importer.format.FieldDefinition
+import sirius.biz.importer.format.ImportDictionary
+import sirius.kernel.SiriusExtension
+import sirius.kernel.commons.Tuple
+import sirius.kernel.commons.Values
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
-    def "detectHeaderProblems detects a skipped column"() {
-        when:
-        ImportDictionary dict = new ImportDictionary()
-        dict.useMapping(Arrays.asList("A", "B", "C", "D", "E"))
-        and:
-        def problems = new ArrayList()
-        then:
-        dict.detectHeaderProblems(Values.of("A", "C", "D", "E"), { problem, _ -> problems.add(problem) }, true)
-        and:
-        problems.get(0) == "Die Spalte 2 ('B') fehlt."
-        problems.size() == 1
+/**
+ * Tests the [ImportDictionary] class.
+ */
+@ExtendWith(SiriusExtension::class)
+class ImportDictionaryTest {
+
+    @Test
+    fun `detectHeaderProblems detects a skipped column`() {
+        val dict = ImportDictionary()
+        dict.useMapping(listOf("A", "B", "C", "D", "E"))
+        val problems = mutableListOf<String>()
+        dict.detectHeaderProblems(Values.of(listOf("A", "C", "D", "E")), { problem, _ -> problems.add(problem) }, true)
+        assertEquals("Die Spalte 2 ('B') fehlt.", problems[0])
+        assertEquals(1, problems.size)
     }
 
-    def "detectHeaderProblems detects two skipped columns"() {
-        when:
-        ImportDictionary dict = new ImportDictionary()
-        dict.useMapping(Arrays.asList("A", "B", "C", "D", "E"))
-        and:
-        def problems = new ArrayList()
-        then:
-        dict.detectHeaderProblems(Values.of("A", "D", "E"), { problem, _ -> problems.add(problem) }, true)
-        and:
-        problems.get(0) == "Die Spalte 2 ('B') fehlt."
-        problems.get(1) == "Die Spalte 3 ('C') fehlt."
-        problems.size() == 2
+    @Test
+    fun `detectHeaderProblems detects two skipped columns`() {
+        val dict = ImportDictionary()
+        dict.useMapping(listOf("A", "B", "C", "D", "E"))
+        val problems = mutableListOf<String>()
+        dict.detectHeaderProblems(Values.of(listOf("A", "D", "E")), { problem, _ -> problems.add(problem) }, true)
+        assertEquals("Die Spalte 2 ('B') fehlt.", problems[0])
+        assertEquals("Die Spalte 3 ('C') fehlt.", problems[1])
+        assertEquals(2, problems.size)
     }
 
-    def "detectHeaderProblems detects a superfluous column"() {
-        when:
-        ImportDictionary dict = new ImportDictionary()
-        dict.useMapping(Arrays.asList("A", "B", "C"))
-        and:
-        def problems = new ArrayList()
-        then:
-        dict.detectHeaderProblems(Values.of("A", "A1", "B", "C"), { problem, _ -> problems.add(problem) }, true)
-        and:
-        problems.get(0) == "Die Spalte 2 ('A1') ist unerwartet."
-        problems.size() == 1
+    @Test
+    fun `detectHeaderProblems detects a superfluous column`() {
+        val dict = ImportDictionary()
+        dict.useMapping(listOf("A", "B", "C"))
+        val problems = mutableListOf<String>()
+        dict.detectHeaderProblems(Values.of(listOf("A", "A1", "B", "C")), { problem, _ -> problems.add(problem) }, true)
+        assertEquals("Die Spalte 2 ('A1') ist unerwartet.", problems[0])
+        assertEquals(1, problems.size)
     }
 
-    def "detectHeaderProblems detects two superfluous columns"() {
-        when:
-        ImportDictionary dict = new ImportDictionary()
-        dict.useMapping(Arrays.asList("A", "B", "C"))
-        and:
-        def problems = new ArrayList()
-        then:
-        dict.detectHeaderProblems(Values.of("A", "A1", "A2", "B", "C"), { problem, _ -> problems.add(problem) }, true)
-        and:
-        problems.get(0) == "Die Spalte 2 ('A1') ist unerwartet."
-        problems.get(1) == "Die Spalte 3 ('A2') ist unerwartet."
-        problems.size() == 2
+    @Test
+    fun `detectHeaderProblems detects two superfluous columns`() {
+        val dict = ImportDictionary()
+        dict.useMapping(listOf("A", "B", "C"))
+        val problems = mutableListOf<String>()
+        dict.detectHeaderProblems(
+                Values.of(listOf("A", "A1", "A2", "B", "C")),
+                { problem, _ -> problems.add(problem) },
+                true
+        )
+        assertEquals("Die Spalte 2 ('A1') ist unerwartet.", problems[0])
+        assertEquals("Die Spalte 3 ('A2') ist unerwartet.", problems[1])
+        assertEquals(2, problems.size)
     }
 
-    def "detectHeaderProblems detects wrong column"() {
-        when:
-        ImportDictionary dict = new ImportDictionary()
-        dict.useMapping(Arrays.asList("A", "B", "C", "D"))
-        and:
-        def problems = new ArrayList()
-        then:
-        dict.detectHeaderProblems(Values.of("A", "F", "G", "H"), { problem, _ -> problems.add(problem) }, true)
-        and:
-        problems.get(0) == "In der Spalte 2 wurde 'B' erwartet, aber 'F' gefunden."
-        problems.get(1) == "In der Spalte 3 wurde 'C' erwartet, aber 'G' gefunden."
-        problems.get(2) == "In der Spalte 4 wurde 'D' erwartet, aber 'H' gefunden."
-        problems.size() == 3
+    @Test
+    fun `detectHeaderProblems detects wrong column`() {
+        val dict = ImportDictionary()
+        dict.useMapping(listOf("A", "B", "C", "D"))
+        val problems = mutableListOf<String>()
+        dict.detectHeaderProblems(Values.of(listOf("A", "F", "G", "H")), { problem, _ -> problems.add(problem) }, true)
+        assertEquals("In der Spalte 2 wurde 'B' erwartet, aber 'F' gefunden.", problems[0])
+        assertEquals("In der Spalte 3 wurde 'C' erwartet, aber 'G' gefunden.", problems[1])
+        assertEquals("In der Spalte 4 wurde 'D' erwartet, aber 'H' gefunden.", problems[2])
+        assertEquals(3, problems.size)
     }
 
-    def "detectHeaderProblems warns for aliased columns"() {
-        when:
-        ImportDictionary dict = new ImportDictionary()
+    @Test
+    fun `detectHeaderProblems warns for aliased columns`() {
+        val dict = ImportDictionary()
         dict.addField(FieldDefinition.stringField("A", 25).addAlias("XA"))
-        dict.useMapping(Arrays.asList("A", "B", "C", "D"))
-        and:
-        def problems = new ArrayList()
-        def problemDetected = dict.detectHeaderProblems(Values.of("XA", "B", "C", "D"), { problem, errorFlag ->
-        problems.add(Tuple.create(problem, errorFlag))
-    }, true)
-        then:
-        problemDetected == false
-        and:
-        problems.get(0).getFirst() == "In der Spalte 1 wurde 'A' erwartet, aber 'XA' gefunden."
-        problems.get(0).getSecond() == false
-        problems.size() == 1
+        dict.useMapping(listOf("A", "B", "C", "D"))
+        val problems = mutableListOf<Tuple<String, Boolean>>()
+        val problemDetected = dict.detectHeaderProblems(Values.of(listOf("XA", "B", "C", "D")), { problem, errorFlag ->
+            problems.add(Tuple.create(problem, errorFlag))
+        }, true)
+        assertFalse(problemDetected)
+        assertEquals("In der Spalte 1 wurde 'A' erwartet, aber 'XA' gefunden.", problems[0].first)
+        assertEquals(false, problems[0].second)
+        assertEquals(1, problems.size)
     }
 
-    def "detectHeaderProblems detects additional columns"() {
-        when:
-        ImportDictionary dict = new ImportDictionary()
-        dict.useMapping(Arrays.asList("A", "B", "C"))
-        and:
-        def problems = new ArrayList()
-        then:
-        dict.detectHeaderProblems(Values.of("A", "B", "C", "D"), { problem, _ -> problems.add(problem) }, true)
-        and:
-        problems.get(0) == "Die Spalte 4 ('D') ist unerwartet."
-        problems.size() == 1
+    @Test
+    fun `detectHeaderProblems detects additional columns`() {
+        val dict = ImportDictionary()
+        dict.useMapping(listOf("A", "B", "C"))
+        val problems = mutableListOf<String>()
+        dict.detectHeaderProblems(Values.of(listOf("A", "B", "C", "D")), { problem, _ -> problems.add(problem) }, true)
+        assertEquals("Die Spalte 4 ('D') ist unerwartet.", problems[0])
+        assertEquals(1, problems.size)
     }
 
-    def "detectHeaderProblems detects missing columns"() {
-        when:
-        ImportDictionary dict = new ImportDictionary()
-        dict.useMapping(Arrays.asList("A", "B", "C", "D"))
-        and:
-        def problems = new ArrayList()
-        then:
-        dict.detectHeaderProblems(Values.of("A", "B", "C"), { problem, _ -> problems.add(problem) }, true)
-        and:
-        problems.get(0) == "Die Spalte 4 ('D') fehlt."
-        problems.size() == 1
+    @Test
+    fun `detectHeaderProblems detects missing columns`() {
+        val dict = ImportDictionary()
+        dict.useMapping(listOf("A", "B", "C", "D"))
+        val problems = mutableListOf<String>()
+        dict.detectHeaderProblems(Values.of(listOf("A", "B", "C")), { problem, _ -> problems.add(problem) }, true)
+        assertEquals("Die Spalte 4 ('D') fehlt.", problems[0])
+        assertEquals(1, problems.size)
     }
 }
