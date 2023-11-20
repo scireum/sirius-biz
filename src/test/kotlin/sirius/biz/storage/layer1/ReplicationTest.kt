a0f7a69ff91779cb4525f0e4e72ec03447de814e
MOOOOOSER <moser.sandro222@googlemail.com>
2023-11-20 14:54:58 +0100

    Adapted Kotlin Syntax

--- src/test/kotlin/sirius/biz/storage/layer1/ReplicationTest.kt
+++ src/test/kotlin/sirius/biz/storage/layer1/ReplicationTest.kt
@@ -9,56 +9,70 @@
 package sirius.biz.storage.layer1
 
 import org.junit.jupiter.api.Tag
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.biz.storage.layer1.replication.ReplicationBackgroundLoop
+import sirius.biz.storage.s3.ObjectStores
 import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.Tags
 import sirius.kernel.async.BackgroundLoop
 import sirius.kernel.commons.Wait
 import sirius.kernel.di.std.Part
+import java.io.ByteArrayInputStream
+import java.io.InputStreamReader
 
 import java.nio.charset.StandardCharsets
 import java.time.Duration
-
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+/**
+ * Tests the [ReplicationBackgroundLoop].
+ */
 @Tag(Tags.NIGHTLY)
-class ReplicationSpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class ReplicationTest {
+    @Test
+    fun `updates are replicated correctly`() {
+        val testData = "test".toByteArray(StandardCharsets.UTF_8)
+        storage.getSpace("repl-primary")
+            .upload("repl-update-test", ByteArrayInputStream(testData), testData.size.toLong())
+        awaitReplication()
+        val downloaded = storage.getSpace("reply-secondary").download("repl-update-test")
+        assertTrue { downloaded.isPresent() }
 
-    @Part
-    private static ObjectStorage storage
-
-            def awaitReplication() {
-        BackgroundLoop.nextExecution(ReplicationBackgroundLoop.class).await(Duration.ofMinutes(1))
-                // Give the sync some time to actually complete its tasks...
-                Wait.seconds(10)
+        val res = InputStreamReader(downloaded.get().getInputStream(), StandardCharsets.UTF_8)
+        assertEquals("test", InputStreamReader(downloaded.get().getInputStream(), StandardCharsets.UTF_8).readText())
     }
 
-    def "updates are replicated correctly"() {
-        given:
-        def testData = "test".getBytes(StandardCharsets.UTF_8)
-        when:
-        storage.getSpace("repl-primary").upload("repl-update-test", new ByteArrayInputStream(testData), testData.length)
-        and:
-        awaitReplication()
-        def downloaded = storage.getSpace("reply-secondary").download("repl-update-test")
-        then:
-        downloaded.isPresent()
-        and:
-        new InputStreamReader(downloaded.get().getInputStream(), StandardCharsets.UTF_8).readLine() == "test"
-    }
+    @Test
+    fun `deletes are replicated correctly`() {
 
-    def "deletes are replicated correctly"() {
-        given:
-        def testData = "test".getBytes(StandardCharsets.UTF_8)
-        when:
-        storage.getSpace("repl-primary").upload("repl-delete-test", new ByteArrayInputStream(testData), testData.length)
-        and:
+        val testData = "test".toByteArray(StandardCharsets.UTF_8)
+        storage.getSpace("repl-primary")
+            .upload("repl-delete-test", ByteArrayInputStream(testData), testData.size.toLong())
+
         awaitReplication()
-        and:
         storage.getSpace("repl-primary").delete("repl-delete-test")
-        and:
         awaitReplication()
-        def downloaded = storage.getSpace("reply-secondary").download("repl-delete-test")
-        then:
-        !downloaded.isPresent()
+        val downloaded = storage.getSpace("reply-secondary").download("repl-delete-test")
+        assertFalse { downloaded.isPresent() }
+    }
+
+    companion object {
+        @Part
+        @JvmStatic
+        private lateinit var storage: ObjectStorage
+
+        fun awaitReplication() {
+            BackgroundLoop.nextExecution(
+                ReplicationBackgroundLoop::class.java
+            ).await(Duration.ofMinutes(1))
+            // Give the sync some time to actually complete its tasks...
+            Wait.seconds(10.0)
+        }
+
     }
 
 }
