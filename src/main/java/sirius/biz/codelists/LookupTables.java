scireum/sirius-biz

commit 6e23dfb39d94fb8f380f701d842e649b7eefa1ef

Enables ConfigLookupTable outside of test scope

src/main/java/sirius/biz/codelists/LookupTables.java
@@ -66,20 +66,13 @@ public class LookupTables {
         // If no IDB config is present or Jupiter is disabled, ...
         if (Strings.isEmpty(baseTable) || jupiter == null) {
             String codeList = extension.get(CONFIG_KEY_CODE_LIST).asString();
-            if (Sirius.isTest()) {
-                String json = extension.get(TestJsonLookupTable.CONFIG_KEY_TEST_DATA_JSON).asString();
-                if (Strings.isEmpty(codeList) && Strings.isEmpty(json)) {
-                    // ...and we are running in test mode and no codeList is given
-                    return new ConfigLookupTable(extension);
-                }
-                if (Strings.isFilled(json)) {
-                    // ...but a json table is given
-                    return new TestJsonLookupTable(extension);
-                }
+            if (Strings.isEmpty(codeList)) {
+                // ...and no codeList is given
+                return new ConfigLookupTable(extension);
+            } else {
+                // ...we resort to code list based tables
+                return new CodeListLookupTable(extension, Strings.firstFilled(codeList, name));
             }
-
-            // ...we resort to code list based tables
-            return new CodeListLookupTable(extension, Strings.firstFilled(codeList, name));
         }
 
         // Note: to ensure the configured tables are loaded in the correct order, the base table needs to be resolved

src/main/java/sirius/biz/codelists/ConfigLookupTable.java
@@ -8,7 +8,6 @@
 
 package sirius.biz.codelists;
 
-import sirius.kernel.Sirius;
 import sirius.kernel.commons.Explain;
 import sirius.kernel.commons.Limit;
 import sirius.kernel.commons.Strings;
@@ -23,7 +22,7 @@ import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
- * Provides an implementation for {@link LookupTable} based on {@link Extension} for testing purposes.
+ * Provides an implementation for {@link LookupTable} based on {@link Extension config data}.
  */
 class ConfigLookupTable extends LookupTable {
 
@@ -31,9 +30,6 @@ class ConfigLookupTable extends LookupTable {
 
     ConfigLookupTable(Extension extension) {
         super(extension);
-        if (!Sirius.isTest()) {
-            throw new IllegalStateException(getClass().getSimpleName() + " only supported while in test mode.");
-        }
     }
 
     @Override

commit 980e3b569bb89e24f1b566edbcba8052e12c9427

Changes/fixes priority of lookup table fallbacks
- in the past the 'lookup-tables' extension 'name' was used as fallback using a Strings#firstFilled
- this was broken in https://github.com/scireum/sirius-biz/commit/6e23dfb39d94fb8f380f701d842e649b7eefa1ef
  as by now, only a configured code list in the 'lookup-tables' extension field 'codeList' does lead to the usage
  of the code list implementation
- as this break did not cause major damage, lets stay with the current behavior
- so by now, configuration of lookup-tables.XYZ.codeList.XYZ will remain mandatory for code lists
- and the code gets cleaned for clarity

Fixes: SIRI-908

src/main/java/sirius/biz/codelists/LookupTables.java
@@ -66,12 +66,12 @@ public class LookupTables {
         // If no IDB config is present or Jupiter is disabled, ...
         if (Strings.isEmpty(baseTable) || jupiter == null) {
             String codeList = extension.get(CONFIG_KEY_CODE_LIST).asString();
-            if (Strings.isEmpty(codeList)) {
-                // ...and no codeList is given
-                return new ConfigLookupTable(extension);
+            if (Strings.isFilled(codeList)) {
+                // ...if configured, use code list based tables
+                return new CodeListLookupTable(extension, codeList);
             } else {
-                // ...we resort to code list based tables
-                return new CodeListLookupTable(extension, Strings.firstFilled(codeList, name));
+                // ...or fallback to configuration based table
+                return new ConfigLookupTable(extension);
             }
         }
 