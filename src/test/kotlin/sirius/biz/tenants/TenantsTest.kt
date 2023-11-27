05408bc40111c7adc97bba0d07aacf5f5374bfab
MOOOOOSER <moser.sandro222@googlemail.com>
2023-11-27 10:23:03 +0100

    removed redundant imports

--- src/test/kotlin/sirius/biz/tenants/TenantsTest.kt
+++ src/test/kotlin/sirius/biz/tenants/TenantsTest.kt
@@ -8,14 +8,10 @@
 
 package sirius.biz.codelists
 
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
-import sirius.biz.codelists.jdbc.SQLCodeList
-import sirius.biz.codelists.jdbc.SQLCodeListEntry
-import sirius.biz.tenants.PhoneNumberValidator
+
 import sirius.biz.tenants.TenantsHelper
 import sirius.db.jdbc.OMA
 import sirius.kernel.SiriusExtension
@@ -25,7 +21,7 @@ import sirius.web.security.UserInfo
 import java.time.Duration
 
 /**
- * Tests the [SQLTenants].
+ * Tests the [TenantsHelper].
  */
 @ExtendWith(SiriusExtension::class)
 class TenantsTest {
