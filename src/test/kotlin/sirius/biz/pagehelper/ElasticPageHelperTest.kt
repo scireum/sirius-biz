007ef6347ef986eaead54d6caf169f19a1bc127e
Sascha Bieberstein <sbi@scireum.de>
2023-12-01 16:25:39 +0100

    Adapts test to Kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/biz/pagehelper/ElasticPageHelperTest.kt
+++ src/test/kotlin/sirius/biz/pagehelper/ElasticPageHelperTest.kt
@@ -8,95 +8,119 @@
 
 package sirius.biz.pagehelper
 
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.biz.web.ElasticPageHelper
+import sirius.biz.web.pagehelper.ElasticPageHelperEntity
 import sirius.db.es.Elastic
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.async.CallContext
 import sirius.kernel.di.std.Part
 import sirius.web.http.WebContext
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-class ElasticPageHelperSpec extends BaseSpecification {
+/**
+ * Tests the [ElasticPageHelper] class.
+ */
+@ExtendWith(SiriusExtension::class)
+class ElasticPageHelperTest {
 
-    @Part
-    private static Elastic elastic
-
-            def setupSpec() {
-        ElasticPageHelperEntity entity1 = new ElasticPageHelperEntity()
-        entity1.setBooleanField(true)
-        entity1.setStringField("field-value-a")
-        elastic.update(entity1)
-
-        ElasticPageHelperEntity entity2 = new ElasticPageHelperEntity()
-        entity2.setBooleanField(true)
-        entity2.setStringField("field-value-b")
-        elastic.update(entity2)
-
-        ElasticPageHelperEntity entity3 = new ElasticPageHelperEntity()
-        entity3.setBooleanField(true)
-        entity3.setStringField("field-value-a")
-        elastic.update(entity3)
-
-        ElasticPageHelperEntity entity4 = new ElasticPageHelperEntity()
-        entity4.setBooleanField(false)
-        entity4.setStringField("field-value-b")
-        elastic.update(entity4)
-
-        ElasticPageHelperEntity entity5 = new ElasticPageHelperEntity()
-        entity5.setBooleanField(false)
-        entity5.setStringField("field-value-a")
-        elastic.update(entity5)
-
-        elastic.refresh(ElasticPageHelperEntity.class)
+    @Test
+    fun `test boolean aggregation without value selected`() {
+        val pageHelper = ElasticPageHelper.withQuery(
+                elastic.select(ElasticPageHelperEntity::class.java)
+        )
+        val webContext = CallContext.getCurrent().get(WebContext::class.java)
+        webContext.javaClass.getDeclaredField("queryString").apply {
+            isAccessible = true
+            set(webContext, mapOf<String, List<String>>())
+        }
+        pageHelper.withContext(webContext)
+        pageHelper.addBooleanAggregation(ElasticPageHelperEntity.BOOLEAN_FIELD)
+        val page = pageHelper.asPage()
+        assertTrue { page.hasFacets() }
+        assertEquals(1, page.facets.size)
+        val facet = page.facets[0]
+        assertEquals("Bool Feld", facet.title)
+        assertEquals("booleanField", facet.name)
+        val facetItems = facet.allItems
+        assertEquals(2, facetItems.size)
+        assertEquals("1", facetItems[0].key)
+        assertEquals(3, facetItems[0].count)
+        assertEquals(false, facetItems[0].isActive)
+        assertEquals("0", facetItems[1].key)
+        assertEquals(2, facetItems[1].count)
+        assertEquals(false, facetItems[1].isActive)
     }
 
-    def "test boolean aggregation without value selected"() {
-        given:
-        ElasticPageHelper<ElasticPageHelperEntity> ph = ElasticPageHelper.withQuery(elastic.select(ElasticPageHelperEntity.class))
-                WebContext wc = CallContext.getCurrent().get(WebContext.class)
-                wc.queryString = [:]
-        ph.withContext(wc)
-        ph.addBooleanAggregation(ElasticPageHelperEntity.BOOLEAN_FIELD)
-        when:
-        def page = ph.asPage()
-        then:
-        page.hasFacets() == true
-        page.getFacets().size() == 1
-        and:
-        def facet = page.getFacets().get(0)
-        facet.getTitle() == "Bool Feld"
-        facet.getName() == "booleanField"
-        and:
-        def facetItems = facet.getAllItems()
-        facetItems.size() == 2
-        facetItems.get(0).getKey() == "1"
-        facetItems.get(0).getCount() == 3
-        facetItems.get(0).isActive() == false
-        facetItems.get(1).getKey() == "0"
-        facetItems.get(1).getCount() == 2
-        facetItems.get(1).isActive() == false
+    @Test
+    fun `test boolean aggregation with value selected`() {
+        val pageHelper = ElasticPageHelper.withQuery(
+                elastic.select(ElasticPageHelperEntity::class.java)
+        )
+        val webContext = CallContext.getCurrent().get(
+                WebContext::class.java
+        )
+        webContext.javaClass.getDeclaredField("queryString").apply {
+            isAccessible = true
+            set(webContext, mapOf("booleanField" to listOf("1")))
+        }
+        pageHelper.withContext(webContext)
+        pageHelper.addBooleanAggregation(ElasticPageHelperEntity.BOOLEAN_FIELD)
+        val page = pageHelper.asPage()
+        assertTrue { page.hasFacets() }
+        assertEquals(1, page.facets.size)
+        val facet = page.facets[0]
+        assertEquals("Bool Feld", facet.title)
+        assertEquals("booleanField", facet.name)
+        val facetItems = facet.allItems
+        assertEquals(1, facetItems.size)
+        assertEquals("1", facetItems[0].key)
+        assertEquals(3, facetItems[0].count)
+        assertEquals(true, facetItems[0].isActive)
     }
 
-    def "test boolean aggregation with value selected"() {
-        given:
-        ElasticPageHelper<ElasticPageHelperEntity> ph = ElasticPageHelper.withQuery(elastic.select(ElasticPageHelperEntity.class))
-                WebContext wc = CallContext.getCurrent().get(WebContext.class)
-                wc.queryString = ["booleanField": [1]]
-        ph.withContext(wc)
-        ph.addBooleanAggregation(ElasticPageHelperEntity.BOOLEAN_FIELD)
-        when:
-        def page = ph.asPage()
-        then:
-        page.hasFacets() == true
-        page.getFacets().size() == 1
-        and:
-        def facet = page.getFacets().get(0)
-        facet.getTitle() == "Bool Feld"
-        facet.getName() == "booleanField"
-        and:
-        def facetItems = facet.getAllItems()
-        facetItems.size() == 1
-        facetItems.get(0).getKey() == "1"
-        facetItems.get(0).getCount() == 3
-        facetItems.get(0).isActive() == true
+    companion object {
+        @Part
+        @JvmStatic
+        private lateinit var elastic: Elastic
+
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            ElasticPageHelperEntity().apply {
+                isBooleanField = true
+                stringField = "field-value-a"
+                elastic.update(this)
+            }
+
+            ElasticPageHelperEntity().apply {
+                isBooleanField = true
+                stringField = "field-value-b"
+                elastic.update(this)
+            }
+
+            ElasticPageHelperEntity().apply {
+                isBooleanField = true
+                stringField = "field-value-a"
+                elastic.update(this)
+            }
+
+            ElasticPageHelperEntity().apply {
+                isBooleanField = false
+                stringField = "field-value-b"
+                elastic.update(this)
+            }
+
+            ElasticPageHelperEntity().apply {
+                isBooleanField = false
+                stringField = "field-value-a"
+                elastic.update(this)
+            }
+
+            elastic.refresh(ElasticPageHelperEntity::class.java)
+        }
     }
 }
--- src/test/kotlin/sirius/biz/pagehelper/MongoPageHelperTest.kt
+++ src/test/kotlin/sirius/biz/pagehelper/MongoPageHelperTest.kt
@@ -8,144 +8,170 @@
 
 package sirius.biz.pagehelper
 
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import sirius.biz.web.MongoPageHelper
+import sirius.biz.web.pagehelper.MongoPageHelperEntity
 import sirius.db.mongo.Mango
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.async.CallContext
 import sirius.kernel.di.std.Part
 import sirius.web.http.WebContext
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
-class MongoPageHelperSpec extends BaseSpecification {
+/**
+ * Tests the [MongoPageHelper] class.
+ */
+@ExtendWith(SiriusExtension::class)
+class MongoPageHelperTest {
 
-    @Part
-    private static Mango mango
-
-            def setupSpec() {
-        MongoPageHelperEntity entity1 = new MongoPageHelperEntity()
-        entity1.setBooleanField(true)
-        entity1.setStringField("field-value-a")
-        mango.update(entity1)
-
-        MongoPageHelperEntity entity2 = new MongoPageHelperEntity()
-        entity2.setBooleanField(true)
-        entity2.setStringField("field-value-b")
-        mango.update(entity2)
-
-        MongoPageHelperEntity entity3 = new MongoPageHelperEntity()
-        entity3.setBooleanField(true)
-        entity3.setStringField("field-value-a")
-        mango.update(entity3)
-
-        MongoPageHelperEntity entity4 = new MongoPageHelperEntity()
-        entity4.setBooleanField(false)
-        entity4.setStringField("field-value-b")
-        mango.update(entity4)
-
-        MongoPageHelperEntity entity5 = new MongoPageHelperEntity()
-        entity5.setBooleanField(false)
-        entity5.setStringField("field-value-a")
-        mango.update(entity5)
+    @Test
+    fun `test boolean aggregation without value selected`() {
+        val pageHelper = MongoPageHelper.withQuery(
+                mango.select(MongoPageHelperEntity::class.java)
+        )
+        val webContext = CallContext.getCurrent().get(WebContext::class.java)
+        webContext.javaClass.getDeclaredField("queryString").apply {
+            isAccessible = true
+            set(webContext, mapOf<String, List<String>>())
+        }
+        pageHelper.withContext(webContext)
+        pageHelper.addBooleanAggregation(MongoPageHelperEntity.BOOLEAN_FIELD)
+        val page = pageHelper.asPage()
+        assertTrue { page.hasFacets() }
+        assertEquals(1, page.facets.size)
+        val facet = page.facets[0]
+        assertEquals("Bool Feld", facet.title)
+        assertEquals("booleanField", facet.name)
+        val facetItems = facet.allItems
+        assertEquals(2, facetItems.size)
+        assertEquals("true", facetItems[0].key)
+        assertEquals(3, facetItems[0].count)
+        assertEquals(false, facetItems[0].isActive)
+        assertEquals("false", facetItems[1].key)
+        assertEquals(2, facetItems[1].count)
+        assertEquals(false, facetItems[1].isActive)
     }
 
-    def "test boolean aggregation without value selected"() {
-        given:
-        MongoPageHelper<MongoPageHelperEntity> ph = MongoPageHelper.withQuery(mango.select(MongoPageHelperEntity.class))
-                WebContext wc = CallContext.getCurrent().get(WebContext.class)
-                wc.queryString = [:]
-        ph.withContext(wc)
-        ph.addBooleanAggregation(MongoPageHelperEntity.BOOLEAN_FIELD)
-        when:
-        def page = ph.asPage()
-        then:
-        page.hasFacets() == true
-        page.getFacets().size() == 1
-        and:
-        def facet = page.getFacets().get(0)
-        facet.getTitle() == "Bool Feld"
-        facet.getName() == "booleanField"
-        and:
-        def facetItems = facet.getAllItems()
-        facetItems.size() == 2
-        facetItems.get(0).getKey() == "true"
-        facetItems.get(0).getCount() == 3
-        facetItems.get(0).isActive() == false
-        facetItems.get(1).getKey() == "false"
-        facetItems.get(1).getCount() == 2
-        facetItems.get(1).isActive() == false
+    @Test
+    fun `test boolean aggregation with value selected`() {
+        val pageHelper = MongoPageHelper.withQuery(
+                mango.select(
+                        MongoPageHelperEntity::class.java
+                )
+        )
+        val webContext = CallContext.getCurrent().get(WebContext::class.java)
+        webContext.javaClass.getDeclaredField("queryString").apply {
+            isAccessible = true
+            set(webContext, mapOf("booleanField" to listOf("true")))
+        }
+        pageHelper.withContext(webContext)
+        pageHelper.addBooleanAggregation(MongoPageHelperEntity.BOOLEAN_FIELD)
+        val page = pageHelper.asPage()
+        assertTrue { page.hasFacets() }
+        assertEquals(1, page.facets.size)
+        val facet = page.facets[0]
+        assertEquals("Bool Feld", facet.title)
+        assertEquals("booleanField", facet.name)
+        val facetItems = facet.allItems
+        assertEquals(1, facetItems.size)
+        assertEquals("true", facetItems[0].key)
+        assertEquals(3, facetItems[0].count)
+        assertEquals(true, facetItems[0].isActive)
     }
 
-    def "test boolean aggregation with value selected"() {
-        given:
-        MongoPageHelper<MongoPageHelperEntity> ph = MongoPageHelper.withQuery(mango.select(MongoPageHelperEntity.class))
-                WebContext wc = CallContext.getCurrent().get(WebContext.class)
-                wc.queryString = ["booleanField": [true]]
-        ph.withContext(wc)
-        ph.addBooleanAggregation(MongoPageHelperEntity.BOOLEAN_FIELD)
-        when:
-        def page = ph.asPage()
-        then:
-        page.hasFacets() == true
-        page.getFacets().size() == 1
-        and:
-        def facet = page.getFacets().get(0)
-        facet.getTitle() == "Bool Feld"
-        facet.getName() == "booleanField"
-        and:
-        def facetItems = facet.getAllItems()
-        facetItems.size() == 1
-        facetItems.get(0).getKey() == "true"
-        facetItems.get(0).getCount() == 3
-        facetItems.get(0).isActive() == true
+    @Test
+    fun `test term aggregation without value selected`() {
+        val pageHelper = MongoPageHelper.withQuery(
+                mango.select(MongoPageHelperEntity::class.java)
+        )
+        val webContext = CallContext.getCurrent().get(WebContext::class.java)
+        webContext.javaClass.getDeclaredField("queryString").apply {
+            isAccessible = true
+            set(webContext, mapOf<String, List<String>>())
+        }
+        pageHelper.withContext(webContext)
+        pageHelper.addTermAggregation(MongoPageHelperEntity.STRING_FIELD)
+        val page = pageHelper.asPage()
+        assertTrue { page.hasFacets() }
+        assertEquals(1, page.facets.size)
+        val facet = page.facets[0]
+        assertEquals("String Feld", facet.title)
+        assertEquals("stringField", facet.name)
+        val facetItems = facet.allItems
+        assertEquals(2, facetItems.size)
+        assertEquals("field-value-a", facetItems[0].key)
+        assertEquals(3, facetItems[0].count)
+        assertEquals(false, facetItems[0].isActive)
+        assertEquals("field-value-b", facetItems[1].key)
+        assertEquals(2, facetItems[1].count)
+        assertEquals(false, facetItems[1].isActive)
     }
 
-    def "test term aggregation without value selected"() {
-        given:
-        MongoPageHelper<MongoPageHelperEntity> ph = MongoPageHelper.withQuery(mango.select(MongoPageHelperEntity.class))
-                WebContext wc = CallContext.getCurrent().get(WebContext.class)
-                wc.queryString = [:]
-        ph.withContext(wc)
-        ph.addTermAggregation(MongoPageHelperEntity.STRING_FIELD)
-        when:
-        def page = ph.asPage()
-        then:
-        page.hasFacets() == true
-        page.getFacets().size() == 1
-        and:
-        def facet = page.getFacets().get(0)
-        facet.getTitle() == "String Feld"
-        facet.getName() == "stringField"
-        and:
-        def facetItems = facet.getAllItems()
-        facetItems.size() == 2
-        facetItems.get(0).getKey() == "field-value-a"
-        facetItems.get(0).getCount() == 3
-        facetItems.get(0).isActive() == false
-        facetItems.get(1).getKey() == "field-value-b"
-        facetItems.get(1).getCount() == 2
-        facetItems.get(1).isActive() == false
+    @Test
+    fun `test term aggregation with value selected`() {
+        val pageHelper = MongoPageHelper.withQuery(
+                mango.select(MongoPageHelperEntity::class.java)
+        )
+        val webContext = CallContext.getCurrent().get(WebContext::class.java)
+        webContext.javaClass.getDeclaredField("queryString").apply {
+            isAccessible = true
+            set(webContext, mapOf("stringField" to listOf("field-value-a")))
+        }
+        pageHelper.withContext(webContext)
+        pageHelper.addTermAggregation(MongoPageHelperEntity.STRING_FIELD)
+        val page = pageHelper.asPage()
+        assertTrue { page.hasFacets() }
+        assertEquals(1, page.facets.size)
+        val facet = page.facets[0]
+        assertEquals("String Feld", facet.title)
+        assertEquals("stringField", facet.name)
+        val facetItems = facet.allItems
+        assertEquals(1, facetItems.size)
+        assertEquals("field-value-a", facetItems[0].key)
+        assertEquals(3, facetItems[0].count)
+        assertEquals(true, facetItems[0].isActive)
     }
 
-    def "test term aggregation with value selected"() {
-        given:
-        MongoPageHelper<MongoPageHelperEntity> ph = MongoPageHelper.withQuery(mango.select(MongoPageHelperEntity.class))
-                WebContext wc = CallContext.getCurrent().get(WebContext.class)
-                wc.queryString = ["stringField": ["field-value-a"]]
-        ph.withContext(wc)
-        ph.addTermAggregation(MongoPageHelperEntity.STRING_FIELD)
-        when:
-        def page = ph.asPage()
-        then:
-        page.hasFacets() == true
-        page.getFacets().size() == 1
-        and:
-        def facet = page.getFacets().get(0)
-        facet.getTitle() == "String Feld"
-        facet.getName() == "stringField"
-        and:
-        def facetItems = facet.getAllItems()
-        facetItems.size() == 1
-        facetItems.get(0).getKey() == "field-value-a"
-        facetItems.get(0).getCount() == 3
-        facetItems.get(0).isActive() == true
+    companion object {
+        @Part
+        @JvmStatic
+        private lateinit var mango: Mango
+
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            MongoPageHelperEntity().apply {
+                isBooleanField = true
+                stringField = "field-value-a"
+                mango.update(this)
+            }
+
+            MongoPageHelperEntity().apply {
+                isBooleanField = true
+                stringField = "field-value-b"
+                mango.update(this)
+            }
+
+            MongoPageHelperEntity().apply {
+                isBooleanField = true
+                stringField = "field-value-a"
+                mango.update(this)
+            }
+
+            MongoPageHelperEntity().apply {
+                isBooleanField = false
+                stringField = "field-value-b"
+                mango.update(this)
+            }
+
+            MongoPageHelperEntity().apply {
+                isBooleanField = false
+                stringField = "field-value-a"
+                mango.update(this)
+            }
+        }
     }
 }
