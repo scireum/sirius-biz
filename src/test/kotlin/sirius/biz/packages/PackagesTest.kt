ea47bee4d243fda1edb22659640d9e8f3a8bb33f
Sascha Bieberstein <sbi@scireum.de>
2023-12-01 15:17:08 +0100

    Adapts test to Kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/biz/packages/PackagesTest.kt
+++ src/test/kotlin/sirius/biz/packages/PackagesTest.kt
@@ -8,39 +8,64 @@
 
 package sirius.biz.packages
 
-class PackagesSpec extends BaseSpecification {
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import sirius.kernel.SiriusExtension
+import sirius.kernel.di.std.Part
+import java.util.stream.Stream
+import kotlin.test.assertEquals
 
-    @Part
-    static Packages packages
+/**
+ * Tests the [Packages] class.
+ */
+@ExtendWith(SiriusExtension::class)
+class PackagesTest {
 
-    def "get packages for diffrent scopes"() {
-        when:
-        def scope1 = packages.getPackages("test-scope-1")
-        def scope2 = packages.getPackages("test-scope-2")
-        then:
-        scope1 == ["package1", "package2"]
-        scope2 == ["package3"]
+    @Test
+    fun `Get packages for different scopes`() {
+        val scope1 = packages.getPackages("test-scope-1")
+        val scope2 = packages.getPackages("test-scope-2")
+        assertEquals(listOf("package1", "package2"), scope1)
+        assertEquals(listOf("package3"), scope2)
     }
 
-    def "get upgrades for diffrent scopes"() {
-        when:
-        def scope1 = packages.getUpgrades("test-scope-1")
-        def scope2 = packages.getUpgrades("test-scope-2")
-        then:
-        scope1 == ["upgradeA", "upgradeB", "upgradeC"]
-        scope2 == ["upgradeD", "upgradeE"]
+    @Test
+    fun `Get upgrades for different scopes`() {
+        val scope1 = packages.getUpgrades("test-scope-1")
+        val scope2 = packages.getUpgrades("test-scope-2")
+        assertEquals(listOf("upgradeA", "upgradeB", "upgradeC"), scope1)
+        assertEquals(listOf("upgradeD", "upgradeE"), scope2)
     }
 
-    def "test hasRequiredPermissionForRole for current user"() {
-        expect:
-        packages.hasRequiredPermissionForPermission(role, { key -> permissions.contains(key) }) == expectedResult
-        where:
-        role     | expectedResult | permissions
-        "role2"  | true           | [] as Set<String>
-        "role1a" | false          | [] as Set<String>
-        "role1b" | false          | [] as Set<String>
-        "role1a" | true           | ["permission1"] as Set<String>
-        "role1b" | true           | ["permission1"] as Set<String>
-        "role1b" | false          | ["permission2"] as Set<String>
+    @ParameterizedTest
+    @MethodSource("provideTestRoles")
+    fun `Check hasRequiredPermissionForRole for current user`(
+            role: String,
+            expectedResult: Boolean,
+            permissions: Set<String>
+    ) {
+        assertEquals(
+                expectedResult,
+                packages.hasRequiredPermissionForPermission(role) { key -> permissions.contains(key) }
+        )
+    }
+
+    companion object {
+        @Part
+        @JvmStatic
+        private lateinit var packages: Packages
+
+        @JvmStatic
+        fun provideTestRoles(): Stream<Arguments> = Stream.of(
+                Arguments.of("role2", true, setOf<String>()),
+                Arguments.of("role1a", false, setOf<String>()),
+                Arguments.of("role1b", false, setOf<String>()),
+                Arguments.of("role1a", true, setOf("permission1")),
+                Arguments.of("role1b", true, setOf("permission1")),
+                Arguments.of("role1b", false, setOf("permission2")),
+        )
     }
 }
