db54792405544e711deb18ead5889ef2e2e242aa
Sascha Bieberstein <sbi@scireum.de>
2023-12-01 16:49:01 +0100

    Adapts test to Kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/biz/mongo/PrefixSearchableEntityTest.kt
+++ src/test/kotlin/sirius/biz/mongo/PrefixSearchableEntityTest.kt
@@ -8,120 +8,124 @@
 
 package sirius.biz.mongo
 
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import sirius.db.mongo.Mango
 import sirius.db.mongo.QueryBuilder
-import sirius.kernel.BaseSpecification
+import sirius.kernel.SiriusExtension
 import sirius.kernel.di.std.Part
+import java.util.stream.Stream
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
-class PrefixSearchableEntitySpec extends BaseSpecification {
+/**
+ * Tests the [PrefixSearchableEntity] implementation.
+ */
+@ExtendWith(SiriusExtension::class)
+class PrefixSearchableEntityTest {
 
-    @Part
-    private static Mango mango
-
-            def "tokenizing works"() {
-        given:
-
-        PrefixSearchableTestEntity e = new PrefixSearchableTestEntity()
-        when:
-        e.setTest("This is a test")
-        e.setUnsearchableTest("Secret Content")
-        and:
-        e.updateSearchField()
-        and:
-        def tokens = e.getSearchPrefixes().data()
-        then:
-        tokens.contains("this")
-        tokens.contains("is")
-        tokens.contains("a")
-        tokens.contains("test")
-        !tokens.contains("secret")
-        !tokens.contains("content")
+    @Test
+    fun `Tokenizing works`() {
+        val entity = PrefixSearchableTestEntity()
+        entity.test = "This is a test"
+        entity.unsearchableTest = "Secret Content"
+        entity.updateSearchField()
+        entity.searchPrefixes.data().apply {
+            assertTrue { contains("this") }
+            assertTrue { contains("is") }
+            assertTrue { contains("a") }
+            assertTrue { contains("test") }
+            assertFalse { contains("secret") }
+            assertFalse { contains("content") }
+        }
     }
 
-    def "tokenizing string map works"() {
-        given:
-
-        PrefixSearchableTestEntity e = new PrefixSearchableTestEntity()
-        when:
-        e.getMap().put("color", "Indigo blue");
-        e.getMap().put("shape", "Triangle");
-        and:
-        e.updateSearchField()
-        and:
-        def tokens = e.getSearchPrefixes().data()
-        then:
-        tokens.contains("color")
-        tokens.contains("blue")
-        tokens.contains("indigo")
-        tokens.contains("indigo blue")
-        tokens.contains("shape")
-        tokens.contains("triangle")
+    @Test
+    fun `Tokenizing string map works`() {
+        val entity = PrefixSearchableTestEntity()
+        entity.map.put("color", "Indigo blue")
+        entity.map.put("shape", "Triangle")
+        entity.updateSearchField()
+        entity.searchPrefixes.data().apply {
+            assertTrue { contains("color") }
+            assertTrue { contains("blue") }
+            assertTrue { contains("indigo") }
+            assertTrue { contains("indigo blue") }
+            assertTrue { contains("shape") }
+            assertTrue { contains("triangle") }
+        }
     }
 
-    def "tokenizing multi language works"() {
-        given:
-        PrefixSearchableTestEntity e = new PrefixSearchableTestEntity()
-        when:
-        e.getMultiLanguageText().addText("de", "Schmetterling")
-        e.getMultiLanguageText().addText("en", "Nice butterfly")
-        and:
-        e.updateSearchField()
-        and:
-        def tokens = e.getSearchPrefixes().data()
-        then:
-        tokens.contains("schmetterling")
-        tokens.contains("nice")
-        tokens.contains("butterfly")
-        tokens.contains("nice butterfly")
-        !tokens.contains("de")
-        !tokens.contains("en")
+    @Test
+    fun `Tokenizing multi language works`() {
+        val entity = PrefixSearchableTestEntity()
+        entity.multiLanguageText.addText("de", "Schmetterling")
+        entity.multiLanguageText.addText("en", "Nice butterfly")
+        entity.updateSearchField()
+        entity.searchPrefixes.data().apply {
+            assertTrue { contains("schmetterling") }
+            assertTrue { contains("nice") }
+            assertTrue { contains("butterfly") }
+            assertTrue { contains("nice butterfly") }
+            assertFalse { contains("de") }
+            assertFalse { contains("en") }
+        }
     }
 
-    def "tokenizing string list works"() {
-        given:
-        PrefixSearchableTestEntity e = new PrefixSearchableTestEntity()
-        when:
-        e.getList().add("Grumpy Cat").add("Dog")
-        and:
-        e.updateSearchField()
-        and:
-        def tokens = e.getSearchPrefixes().data()
-        then:
-        tokens.contains("dog")
-        tokens.contains("grumpy")
-        tokens.contains("cat")
-        tokens.contains("grumpy cat")
+    @Test
+    fun `Tokenizing string list works`() {
+        val entity = PrefixSearchableTestEntity()
+        entity.list.add("Grumpy Cat").add("Dog")
+        entity.updateSearchField()
+        entity.searchPrefixes.data().apply {
+            assertTrue { contains("grumpy") }
+            assertTrue { contains("cat") }
+            assertTrue { contains("dog") }
+            assertTrue { contains("grumpy cat") }
+        }
     }
 
-    def "splitting with emails works"() {
-        given:
-        PrefixSearchableTestEntity e = new PrefixSearchableTestEntity()
-        when:
-        e.setTest(input)
-        and:
-        e.updateSearchField()
-        and:
-        List<String> tokens = e.getSearchPrefixes().modify().sort()
-        output.sort()
-        then:
-        tokens == output
-        where:
-        input      | output
-        "a.b@c.de" | ["a", "b", "c", "de", "a.b", "c.de", "a.b@c.de"]
-        "a24@x.de" | ["a24", "a", "x", "de", "x.de", "a24@x.de"]
+    @ParameterizedTest
+    @MethodSource("provideTestEmails")
+    fun `Splitting with emails works`(input: String, expectedTokens: List<String>) {
+        val entity = PrefixSearchableTestEntity()
+        entity.test = input
+        entity.updateSearchField()
+        assertEquals(expectedTokens.sorted(), entity.searchPrefixes.modify().sorted())
     }
 
-    def "searching works"() {
-        when:
-        PrefixSearchableTestEntity e = new PrefixSearchableTestEntity()
-        e.setTest("Some Test")
-        mango.update(e)
-        then:
-        mango.select(PrefixSearchableTestEntity.class).
-        where(QueryBuilder.FILTERS.prefix(PrefixSearchableEntity.SEARCH_PREFIXES, "som")).first().isPresent()
-                and:
-                mango.select(PrefixSearchableTestEntity.class).
-        where(QueryBuilder.FILTERS.prefix(PrefixSearchableEntity.SEARCH_PREFIXES, "Test")).first().isPresent()
+    @Test
+    fun `Searching works`() {
+        val entity = PrefixSearchableTestEntity()
+        entity.test = "Some Test"
+        mango.update(entity)
+        assertTrue {
+            mango.select(PrefixSearchableTestEntity::class.java)
+                    .where(QueryBuilder.FILTERS.prefix(PrefixSearchableEntity.SEARCH_PREFIXES, "som"))
+                    .first()
+                    .isPresent
+        }
+        assertTrue {
+            mango.select(PrefixSearchableTestEntity::class.java)
+                    .where(QueryBuilder.FILTERS.prefix(PrefixSearchableEntity.SEARCH_PREFIXES, "Test"))
+                    .first()
+                    .isPresent
+        }
     }
 
+    companion object {
+        @Part
+        @JvmStatic
+        private lateinit var mango: Mango
+
+        @JvmStatic
+        fun provideTestEmails(): Stream<Arguments> = Stream.of(
+                Arguments.of("a.b@c.de", listOf("a", "b", "c", "de", "a.b", "c.de", "a.b@c.de")),
+                Arguments.of("a24@x.de", listOf("a24", "a", "x", "de", "x.de", "a24@x.de")),
+        )
+    }
 }
