30ff9b286e1735d16923908a8c1a5e43a05f6f6b
Sascha Bieberstein <sbi@scireum.de>
2023-12-01 15:23:53 +0100

    Adapts test to Kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/biz/packages/PackageDataTest.kt
+++ src/test/kotlin/sirius/biz/packages/PackageDataTest.kt
@@ -8,77 +8,79 @@
 
 package sirius.biz.packages
 
-import sirius.kernel.BaseSpecification
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.kernel.SiriusExtension
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
-class PackageDataSpec extends BaseSpecification {
+/**
+ * Tests the [PackageData] class.
+ */
+@ExtendWith(SiriusExtension::class)
+class PackageDataTest {
 
-    def "test hasPermission returns true for permission granted by upgrades"() {
-        when:
-        def packageData = new PackageData(null, null)
-        packageData.getUpgrades().add("upgradeA") // grants permission1 and permission2
-        and:
-        def permissions = packageData.computeExpandedPermissions()
-        then:
-        permissions.contains("permission1")
-        permissions.contains("permission2")
+    @Test
+    fun `test hasPermission returns true for permission granted by upgrades`() {
+        val packageData = PackageData(null, null).apply {
+            upgrades.add("upgradeA") // grants permission1 and permission2
+        }
+        val permissions = packageData.computeExpandedPermissions()
+        assertTrue { permissions.contains("permission1") }
+        assertTrue { permissions.contains("permission2") }
     }
 
-    def "test hasPermission returns false for permission revoked by revoked permissions"() {
-        when:
-        def packageData = new PackageData(null, null)
-        packageData.getUpgrades().add("upgradeA") // grants permission1 and permission2
-        packageData.getRevokedPermissions().add("permission2")
-        and:
-        def permissions = packageData.computeExpandedPermissions()
-        then:
-        permissions.contains("permission1")
-        !permissions.contains("permission2")
+    @Test
+    fun `test hasPermission returns false for permission revoked by revoked permissions`() {
+        val packageData = PackageData(null, null).apply {
+            upgrades.add("upgradeA") // grants permission1 and permission2
+            revokedPermissions.add("permission2")
+        }
+        val permissions = packageData.computeExpandedPermissions()
+        assertTrue { permissions.contains("permission1") }
+        assertFalse { permissions.contains("permission2") }
     }
 
-    def "test hasPermission returns true for permission granted by additional permissions"() {
-        when:
-        def packageData = new PackageData(null, null)
-        packageData.getAdditionalPermissions().add("permission3")
-        and:
-        def permissions = packageData.computeExpandedPermissions()
-        then:
-        permissions.contains("permission3")
+    @Test
+    fun `test hasPermission returns true for permission granted by additional permissions`() {
+        val packageData = PackageData(null, null).apply {
+            additionalPermissions.add("permission3")
+        }
+        val permissions = packageData.computeExpandedPermissions()
+        assertTrue { permissions.contains("permission3") }
     }
 
-    def "test hasPermission returns false for not granted permissions"() {
-        when:
-        def packageData = new PackageData(null, null)
-        packageData.getUpgrades().add("upgradeA") // grants permission1 and permission2
-        packageData.getAdditionalPermissions().add("permission3")
-        and:
-        def permissions = packageData.computeExpandedPermissions()
-        then:
-        !permissions.contains("not-granted-permission")
+    @Test
+    fun `test hasPermission returns false for not granted permissions`() {
+        val packageData = PackageData(null, null).apply {
+            upgrades.add("upgradeA") // grants permission1 and permission2
+            additionalPermissions.add("permission3")
+        }
+        val permissions = packageData.computeExpandedPermissions()
+        assertFalse { permissions.contains("not-granted-permission") }
     }
 
-    def "test hasPermission returns true for permission granted by package"() {
-        when:
-        def packageData = new PackageData(null, null)
-        packageData.setPackage("packageBasic") // grants permission3
-        and:
-        def permissions = packageData.computeExpandedPermissions()
-        then:
-        permissions.contains("permission3")
+    @Test
+    fun `test hasPermission returns true for permission granted by package`() {
+        val packageData = PackageData(null, null).apply {
+            `package` = "packageBasic" // grants permission3
+        }
+        val permissions = packageData.computeExpandedPermissions()
+        assertTrue { permissions.contains("permission3") }
     }
 
-    def "test hasPermission for complex test case"() {
-        when:
-        def packageData = new PackageData(null, null)
-        packageData.setPackage("packageBasic") // grants permission3
-        packageData.getUpgrades().add("upgradeA") // grants permission1 and permission2
-        packageData.getAdditionalPermissions().add("permission4")
-        packageData.getRevokedPermissions().add("permission2")
-        and:
-        def permissions = packageData.computeExpandedPermissions()
-        then:
-        permissions.contains("permission1")
-        !permissions.contains("permission2")
-        permissions.contains("permission3")
-        permissions.contains("permission4")
+    @Test
+    fun `test hasPermission for complex test case`() {
+        val packageData = PackageData(null, null).apply {
+            `package` = "packageBasic" // grants permission3
+            upgrades.add("upgradeA") // grants permission1 and permission2
+            additionalPermissions.add("permission4")
+            revokedPermissions.add("permission2")
+        }
+        val permissions = packageData.computeExpandedPermissions()
+        assertTrue { permissions.contains("permission1") }
+        assertFalse { permissions.contains("permission2") }
+        assertTrue { permissions.contains("permission3") }
+        assertTrue { permissions.contains("permission4") }
     }
 }
