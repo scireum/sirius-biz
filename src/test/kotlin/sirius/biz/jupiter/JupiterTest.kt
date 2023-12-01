fc1cf28cf37356c39ae0e9023a6f04c346f231fe
Sascha Bieberstein <sbi@scireum.de>
2023-12-01 16:36:01 +0100

    Adapts test to Kotlin syntax
    Fixes: OX-10357

--- src/test/kotlin/sirius/biz/jupiter/JupiterTest.kt
+++ src/test/kotlin/sirius/biz/jupiter/JupiterTest.kt
@@ -8,98 +8,101 @@
 
 package sirius.biz.jupiter
 
-import sirius.kernel.BaseSpecification
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import sirius.kernel.SiriusExtension
 import sirius.kernel.commons.Wait
 import sirius.kernel.di.std.Part
 import sirius.web.resources.Resources
-
 import java.util.stream.Collectors
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 
 /**
- * Provides a simple test for the communication with the Jupiter repository.
+ * Provides a simple test for the communication with the [Jupiter] repository.
  */
-class JupiterSpec extends BaseSpecification {
+@ExtendWith(SiriusExtension::class)
+class JupiterTest {
 
-    @Part
-    private static Jupiter jupiter
+    @Test
+    fun `LRU get, put & remove works`() {
+        val cache = jupiter.getDefault().lru("test")
+        cache.put("test", "value")
+        assertEquals("value", cache.get("test").get())
+        assertEquals("value", cache.extendedGet("test") { null })
+        assertEquals("value1", cache.extendedGet("test1") { "value1" })
+        cache.remove("test")
+        assertFalse { cache.get("test").isPresent }
+    }
 
-            @Part
-            private static Resources resources
+    @Test
+    fun `IDB show_tables works`() {
+        val list = jupiter.getDefault().idb().showTables()
+        assertEquals(1, list.size)
+    }
 
-            // In case this test starts too early, Jupiter might not have processed its repository contents.
-            // We therefore give it some time to do so.
-            def setupSpec() {
-        jupiter.getDefault().updateConfig(resources.resolve("jupiter-test/settings.yml").get().getContentAsString())
-        jupiter.
-        getDefault().
-        repository().
-        store("/countries.yml", resources.resolve("jupiter-test/countries.yml").get().getContentAsString())
-        jupiter.
-        getDefault().
-        repository().
-        store("/loaders/countries.yml", resources.resolve("jupiter-test/countries_loader.yml").get().getContentAsString())
-        int attempts = 10
-        while (attempts-- > 0) {
-            Wait.seconds(1)
-            if (jupiter.getDefault().idb().showTables().size() > 0) {
-                return
+    @Test
+    fun `IDB queries work`() {
+        val table = jupiter.getDefault().idb().table("countries")
+        assertEquals(2, table.query().count())
+        assertEquals(1, table.query().searchPaths("name").searchValue("Deutschland").count())
+        assertEquals(1, table.query().searchPaths("name").searchValue("deutschland").count())
+        assertEquals(0, table.query().searchPaths("name").searchValue("xxx").count())
+        assertEquals(1, table.query().lookupPaths("code").searchValue("D").count())
+        assertEquals(0, table.query().lookupPaths("code").searchValue("X").count())
+        assertEquals(
+                "D",
+                table.query().lookupPaths("name").searchValue("Deutschland").singleRow("code").get().at(0).asString()
+        )
+        assertEquals(
+                "D", table.query().lookupPaths("code").searchValue("D").singleRow("code").get().at(0).asString()
+        )
+
+        val row = table.query().searchInAllFields().searchValue("de").translate("de").singleRow("code", "name").get()
+        assertEquals("D", row.at(0).asString())
+        assertEquals("Deutschland", row.at(1).asString())
+
+        assertEquals(
+                "Deutschland", table.query().searchInAllFields().searchValue("de").translate("de").allRows("name").map {
+            it.at(0).asString()
+        }.collect(Collectors.joining(","))
+        )
+        assertEquals(
+                "Deutschland,Österreich", table.query().translate("de").allRows("name").map {
+            it.at(0).asString()
+        }.collect(Collectors.joining(","))
+        )
+    }
+
+    companion object {
+        @Part
+        @JvmStatic
+        private lateinit var jupiter: Jupiter
+
+        @Part
+        @JvmStatic
+        private lateinit var resources: Resources
+
+        // In case this test starts too early, Jupiter might not have processed its repository contents.
+        // We therefore give it some time to do so.
+        @BeforeAll
+        @JvmStatic
+        fun setup() {
+            jupiter.getDefault().updateConfig(resources.resolve("jupiter-test/settings.yml").get().contentAsString)
+            jupiter.getDefault().repository()
+                    .store("/countries.yml", resources.resolve("jupiter-test/countries.yml").get().contentAsString)
+            jupiter.getDefault().repository().store(
+                    "/loaders/countries.yml",
+                    resources.resolve("jupiter-test/countries_loader.yml").get().contentAsString
+            )
+            var attempts = 10
+            while (attempts-- > 0) {
+                Wait.seconds(1.0)
+                if (jupiter.getDefault().idb().showTables().size > 0) {
+                    return
+                }
             }
         }
     }
-
-
-    def "LRU get/put/remove works"() {
-        given:
-        def cache = jupiter.getDefault().lru("test")
-        when:
-        cache.put("test", "value")
-        then:
-        cache.get("test").get() == "value"
-        and:
-        cache.extendedGet("test", { ignored -> null }) == "value"
-        and:
-        cache.extendedGet("test1", { ignored -> "value1" }) == "value1"
-        when:
-        cache.remove("test")
-        then:
-        !cache.get("test").isPresent()
-    }
-
-    def "IDB show_tables works"() {
-        when:
-        def list = jupiter.getDefault().idb().showTables()
-        then:
-        list.size() == 1
-    }
-
-    def "IDB queries work"() {
-        when:
-        def table = jupiter.getDefault().idb().table("countries")
-        then:
-        table.query().count() == 2
-        table.query().searchPaths("name").searchValue("Deutschland").count() == 1
-        table.query().searchPaths("name").searchValue("deutschland").count() == 1
-        table.query().searchPaths("name").searchValue("xxx").count() == 0
-        table.query().lookupPaths("code").searchValue("D").count() == 1
-        table.query().lookupPaths("code").searchValue("X").count() == 0
-        and:
-        table.query().lookupPaths("name").searchValue("Deutschland").singleRow("code").get().at(0) == "D"
-        table.query().lookupPaths("code").searchValue("D").singleRow("code").get().at(0) == "D"
-        when:
-        def row = table.query().searchInAllFields().searchValue("de").translate("de").singleRow("code", "name").get()
-        then:
-        row.at(0).getString() == "D"
-        row.at(1).getString() == "Deutschland"
-        and:
-        table.query().searchInAllFields().searchValue("de").translate("de").allRows("name").map({ r ->
-            r.at(0).asString()
-        }).collect(
-                Collectors.joining(",")) == "Deutschland"
-        and:
-        table.query().translate("de").allRows("name").map({ r ->
-            r.at(0).asString()
-        }).collect(
-                Collectors.joining(",")) == "Deutschland,Österreich"
-    }
-
 }
