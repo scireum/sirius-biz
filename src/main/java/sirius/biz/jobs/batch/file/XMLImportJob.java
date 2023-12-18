153a88b557d5e46d1165fe22c4348cc14f2194b7
Mauricio Muler Bodemann <mbo@scireum.de>
2023-12-18 15:22:33 +0100

    Improves codestyle
    Fixes: SIRI-820

--- src/main/java/sirius/biz/jobs/batch/file/XMLImportJob.java
+++ src/main/java/sirius/biz/jobs/batch/file/XMLImportJob.java
@@ -67,28 +67,29 @@ public abstract class XMLImportJob extends FileImportJob {
 
     @Override
     protected void executeForStream(String filename, Producer<InputStream> inputSupplier) throws Exception {
-        if (isValid(inputSupplier)) {
-            Monoflop firstStage = Monoflop.create();
-            for (Consumer<BiConsumer<String, NodeHandler>> handlerConsumer : fetchStages()) {
-                if (firstStage.successiveCall()) {
-                    // If an import is split into several stages, we almost always want to await the completion of
-                    // any running side-task before starting the next stage....
-                    process.awaitSideTaskCompletion();
-                }
-
-                try (InputStream inputStream = inputSupplier.create()) {
-                    try {
-                        executeProcessingStage(inputStream, handlerConsumer);
-                    } catch (Exception exception) {
-                        process.handle(exception);
-                        break;
-                    }
-                }
-            }
-        } else {
+        if (!isValid(inputSupplier)) {
             process.log(ProcessLog.error()
                                   .withNLSKey("XMLImportJob.invalidXMLDetected")
                                   .withContext("fileName", filename));
+            return;
+        }
+
+        Monoflop firstStage = Monoflop.create();
+        for (Consumer<BiConsumer<String, NodeHandler>> handlerConsumer : fetchStages()) {
+            if (firstStage.successiveCall()) {
+                // If an import is split into several stages, we almost always want to await the completion of
+                // any running side-task before starting the next stage....
+                process.awaitSideTaskCompletion();
+            }
+
+            try (InputStream inputStream = inputSupplier.create()) {
+                try {
+                    executeProcessingStage(inputStream, handlerConsumer);
+                } catch (Exception exception) {
+                    process.handle(exception);
+                    break;
+                }
+            }
         }
     }
 
