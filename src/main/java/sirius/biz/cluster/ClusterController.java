f9b95d7eccd12fa13bdf19742c2c4f3ac6eb3fd9
Sascha Bieberstein <sbi@scireum.de>
2024-01-10 09:15:59 +0100

    Removes grouping via nodes in cluster view
    This was rarely used and we want to divide the cluster page into sub-pages, which is harder when trying to support that mode.

--- src/main/java/sirius/biz/cluster/ClusterController.java
+++ src/main/java/sirius/biz/cluster/ClusterController.java
@@ -212,7 +212,6 @@ public class ClusterController extends BasicController {
                             jobKeys,
                             descriptions,
                             clusterInfo,
-                            webContext.get("groupByNode").asBoolean(),
                             locks);
     }
 
