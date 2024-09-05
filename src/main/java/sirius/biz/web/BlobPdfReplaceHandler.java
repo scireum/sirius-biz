c3ede0baa3c508b4b93752bcb4285a829436173b
Jakob Vogel <jvo@scireum.de>
2024-09-05 12:37:18 +0200

    Adds conversion of plain blob links 🖇️
    …to PDF blob links, to allow direct use in PDF templates. This will enable reuse of web UI templates for PDF output.
    
    OX-10783

--- src/main/java/sirius/biz/web/BlobPdfReplaceHandler.java
+++ src/main/java/sirius/biz/web/BlobPdfReplaceHandler.java
@@ -11,12 +11,15 @@ package sirius.biz.web;
 import org.xhtmlrenderer.extend.FSImage;
 import org.xhtmlrenderer.extend.UserAgentCallback;
 import sirius.biz.storage.layer1.FileHandle;
+import sirius.biz.storage.layer2.BlobDispatcher;
 import sirius.biz.storage.layer2.BlobStorage;
 import sirius.biz.storage.layer2.BlobStorageSpace;
+import sirius.biz.storage.layer2.BlobUriParser;
 import sirius.biz.storage.layer2.URLBuilder;
 import sirius.biz.storage.layer2.variants.ConversionEngine;
 import sirius.biz.storage.util.StorageUtils;
 import sirius.kernel.commons.Strings;
+import sirius.kernel.di.std.ConfigValue;
 import sirius.kernel.di.std.Part;
 import sirius.kernel.di.std.Register;
 import sirius.web.templates.pdf.handlers.PdfReplaceHandler;
@@ -32,6 +35,14 @@ import java.util.Optional;
  */
 @Register(framework = StorageUtils.FRAMEWORK_STORAGE)
 public class BlobPdfReplaceHandler extends PdfReplaceHandler {
+
+    /**
+     * @see BlobDispatcher#URI_PREFIX
+     */
+    private static final String DASD_PREFIX = BlobDispatcher.URI_PREFIX_TRAILED;
+
+    @ConfigValue("product.baseUrl")
+    private String productBaseUrl;
 
     @Part
     private BlobStorage storage;
@@ -89,5 +100,28 @@ public class BlobPdfReplaceHandler extends PdfReplaceHandler {
             return Optional.empty();
         }
         return Optional.of(variantOrPhysicalKey);
+    }
+
+    @Override
+    public Optional<String> tryRewritePlainUrl(String url) {
+        if (!url.startsWith(productBaseUrl)) {
+            return Optional.empty();
+        }
+
+        int indexOfDasdPrefix = url.indexOf(DASD_PREFIX);
+        if (indexOfDasdPrefix < 0) {
+            return Optional.empty();
+        }
+
+        return BlobUriParser.parseBlobUri(url.substring(indexOfDasdPrefix)).map(blobUri -> {
+            String uri = "blob://" + blobUri.getStorageSpace() + "/" + blobUri.getBlobKey();
+            if (Strings.isFilled(blobUri.getVariant())) {
+                return uri + "/" + blobUri.getVariant();
+            }
+            if (Strings.isFilled(blobUri.getPhysicalKey())) {
+                return uri + "/" + blobUri.getPhysicalKey();
+            }
+            return uri;
+        });
     }
 }
