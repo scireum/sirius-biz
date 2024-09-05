9cf5004cfe22ea30ec89843e76a8150a1b7b2a24
Jakob Vogel <jvo@scireum.de>
2024-09-05 12:35:44 +0200

    Extend blob PDF handler 🪼
    …to also support physical keys, i.e. links like `blob://space/blobKey/physicalKey`
    
    OX-10783

--- src/main/java/sirius/biz/web/BlobPdfReplaceHandler.java
+++ src/main/java/sirius/biz/web/BlobPdfReplaceHandler.java
@@ -12,7 +12,9 @@ import org.xhtmlrenderer.extend.FSImage;
 import org.xhtmlrenderer.extend.UserAgentCallback;
 import sirius.biz.storage.layer1.FileHandle;
 import sirius.biz.storage.layer2.BlobStorage;
+import sirius.biz.storage.layer2.BlobStorageSpace;
 import sirius.biz.storage.layer2.URLBuilder;
+import sirius.biz.storage.layer2.variants.ConversionEngine;
 import sirius.biz.storage.util.StorageUtils;
 import sirius.kernel.commons.Strings;
 import sirius.kernel.di.std.Part;
@@ -23,15 +25,19 @@ import javax.annotation.Nullable;
 import java.util.Optional;
 
 /**
- * Resolves blob:// URIs to resized images while maintaining the image ratios.
+ * Resolves {@code blob://} URIs to resized images while maintaining the image ratios.
  * <p>
- * The format of the URI needs to match blob://space/blobKey or blob://space/blobKey/variant.
+ * The format of the URI needs to match {@code blob://space/blobKey}, {@code blob://space/blobKey/variant} or
+ * {@code blob://space/blobKey/physicalKey}.
  */
 @Register(framework = StorageUtils.FRAMEWORK_STORAGE)
 public class BlobPdfReplaceHandler extends PdfReplaceHandler {
 
     @Part
     private BlobStorage storage;
+
+    @Part
+    protected ConversionEngine conversionEngine;
 
     @Override
     public boolean accepts(String protocol) {
@@ -51,18 +57,37 @@ public class BlobPdfReplaceHandler extends PdfReplaceHandler {
 
         String spaceKey = blobInfo[0];
         String blobKey = blobInfo[1];
-        String variant = blobInfo.length == 3 ? blobInfo[2] : URLBuilder.VARIANT_RAW;
+        String variantOrPhysicalKey = blobInfo.length == 3 ? blobInfo[2] : URLBuilder.VARIANT_RAW;
 
-        Optional<FileHandle> optionalFileHandle = storage.getSpace(spaceKey).download(blobKey, variant);
+        BlobStorageSpace space = storage.getSpace(spaceKey);
+        Optional<FileHandle> optionalFileHandle =
+                tryResolveVariant(variantOrPhysicalKey).flatMap(variant -> space.download(blobKey, variant))
+                                                       .or(() -> tryResolvePhysicalKey(variantOrPhysicalKey).flatMap(
+                                                               physicalKey -> space.getPhysicalSpace()
+                                                                                   .download(physicalKey)));
 
         if (optionalFileHandle.isPresent()) {
             try (FileHandle fileHandle = optionalFileHandle.get()) {
                 FSImage image = resolveResource(userAgentCallback, fileHandle.getFile().toURI().toURL());
-
                 return resizeImage(image, cssWidth, cssHeight);
             }
         }
 
         return null;
+    }
+
+    private Optional<String> tryResolveVariant(String variantOrPhysicalKey) {
+        if (URLBuilder.VARIANT_RAW.equals(variantOrPhysicalKey)
+            || conversionEngine.isKnownVariant(variantOrPhysicalKey)) {
+            return Optional.of(variantOrPhysicalKey);
+        }
+        return Optional.empty();
+    }
+
+    private Optional<String> tryResolvePhysicalKey(String variantOrPhysicalKey) {
+        if (URLBuilder.VARIANT_RAW.equals(variantOrPhysicalKey)) {
+            return Optional.empty();
+        }
+        return Optional.of(variantOrPhysicalKey);
     }
 }
