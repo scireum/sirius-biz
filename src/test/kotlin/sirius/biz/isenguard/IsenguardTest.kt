849713837c782d3b324381c9461cc0f850412691
Jakob Vogel <jvo@scireum.de>
2025-01-03 00:36:13 +0100

    Reformats file 📰
    …using the (strange?) kotlin formatting rules.
    
    MIO-6376

--- src/test/kotlin/sirius/biz/isenguard/IsenguardTest.kt
+++ src/test/kotlin/sirius/biz/isenguard/IsenguardTest.kt
@@ -29,36 +29,42 @@ class IsenguardTest {
         val realm = "test"
 
         val counter = AtomicInteger()
-        isenguard.increaseAndCheckRateLimitReached(scope,
-                realm,
-                Isenguard.USE_LIMIT_FROM_CONFIG,
-                { -> counter.incrementAndGet() },
-                { -> RateLimitingInfo(null, null, null) })
-        isenguard.increaseAndCheckRateLimitReached(scope,
-                realm,
-                Isenguard.USE_LIMIT_FROM_CONFIG,
-                { -> counter.incrementAndGet() },
-                { -> RateLimitingInfo(null, null, null) })
-        isenguard.increaseAndCheckRateLimitReached(scope,
-                realm,
-                Isenguard.USE_LIMIT_FROM_CONFIG,
-                { -> counter.incrementAndGet() },
-                { -> RateLimitingInfo(null, null, null) })
-        val fourth = isenguard.increaseAndCheckRateLimitReached(scope,
-                realm,
-                Isenguard.USE_LIMIT_FROM_CONFIG,
-                { -> counter.incrementAndGet() },
-                { -> RateLimitingInfo(null, null, null) })
-        val fifth = isenguard.increaseAndCheckRateLimitReached(scope,
-                realm,
-                Isenguard.USE_LIMIT_FROM_CONFIG,
-                { -> counter.incrementAndGet() },
-                { -> RateLimitingInfo(null, null, null) })
-        val sixth = isenguard.increaseAndCheckRateLimitReached(scope,
-                realm,
-                Isenguard.USE_LIMIT_FROM_CONFIG,
-                { -> counter.incrementAndGet() },
-                { -> RateLimitingInfo(null, null, null) })
+        isenguard.increaseAndCheckRateLimitReached(
+            scope,
+            realm,
+            Isenguard.USE_LIMIT_FROM_CONFIG,
+            { -> counter.incrementAndGet() },
+            { -> RateLimitingInfo(null, null, null) })
+        isenguard.increaseAndCheckRateLimitReached(
+            scope,
+            realm,
+            Isenguard.USE_LIMIT_FROM_CONFIG,
+            { -> counter.incrementAndGet() },
+            { -> RateLimitingInfo(null, null, null) })
+        isenguard.increaseAndCheckRateLimitReached(
+            scope,
+            realm,
+            Isenguard.USE_LIMIT_FROM_CONFIG,
+            { -> counter.incrementAndGet() },
+            { -> RateLimitingInfo(null, null, null) })
+        val fourth = isenguard.increaseAndCheckRateLimitReached(
+            scope,
+            realm,
+            Isenguard.USE_LIMIT_FROM_CONFIG,
+            { -> counter.incrementAndGet() },
+            { -> RateLimitingInfo(null, null, null) })
+        val fifth = isenguard.increaseAndCheckRateLimitReached(
+            scope,
+            realm,
+            Isenguard.USE_LIMIT_FROM_CONFIG,
+            { -> counter.incrementAndGet() },
+            { -> RateLimitingInfo(null, null, null) })
+        val sixth = isenguard.increaseAndCheckRateLimitReached(
+            scope,
+            realm,
+            Isenguard.USE_LIMIT_FROM_CONFIG,
+            { -> counter.incrementAndGet() },
+            { -> RateLimitingInfo(null, null, null) })
 
         assertFalse { fourth }
         assertTrue { fifth }
