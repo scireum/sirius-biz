48c8550c0dce5248d5db0c7e934b0059461e1180
Jakob Vogel <jvo@scireum.de>
2025-01-20 18:49:36 +0100

    Uses method from `Strings` to check for URL 🎻
    …as requested by @idlira during review and after fixing the respective implementation.
    
    OX-11244

--- src/main/java/sirius/biz/tenants/HttpsUrlValidator.java
+++ src/main/java/sirius/biz/tenants/HttpsUrlValidator.java
@@ -16,8 +16,6 @@ import sirius.kernel.health.Exceptions;
 import sirius.kernel.health.HandledException;
 
 import javax.annotation.Nonnull;
-import java.net.URI;
-import java.net.URL;
 import java.util.function.Consumer;
 
 /**
@@ -28,59 +26,16 @@ public class HttpsUrlValidator implements PropertyValidator {
 
     @Override
     public void validate(Property property, Object value, Consumer<String> validationConsumer) {
-        try {
-            parseHttpsUrl(property, value);
-        } catch (Exception exception) {
-            validationConsumer.accept(exception.getMessage());
+        if (value instanceof String url && Strings.isFilled(url) && !Strings.isHttpsUrl(url)) {
+            validationConsumer.accept(createInvalidHttpsUrlException(property, url).getMessage());
         }
     }
 
     @Override
     public void beforeSave(Property property, Object value) {
-        parseHttpsUrl(property, value);
-    }
-
-    /**
-     * Parses the given value into a URL and makes sure it is a valid HTTPS URL.
-     *
-     * @param property the property to parse
-     * @param value    the value to parse
-     * @return the parsed URL
-     * @see #parseUrl(Property, Object)
-     * @see #createInvalidHttpsUrlException(Property, String)
-     */
-    private URL parseHttpsUrl(Property property, Object value) {
-        URL url = parseUrl(property, value);
-        if (url == null) {
-            return null;
+        if (value instanceof String url && Strings.isFilled(url) && !Strings.isHttpsUrl(url)) {
+            throw createInvalidHttpsUrlException(property, url);
         }
-
-        if (!"https".equalsIgnoreCase(url.getProtocol())) {
-            throw createInvalidHttpsUrlException(property, url.toString());
-        }
-
-        return url;
-    }
-
-    /**
-     * Parses the given value into a URL. Effectively, this method wraps standard methods to throw a handled exception
-     * with a localized error message if the URL is invalid.
-     *
-     * @param property the property to parse
-     * @param value    the value to parse
-     * @return the parsed URL
-     * @see #createInvalidHttpsUrlException(Property, String)
-     */
-    private URL parseUrl(Property property, Object value) {
-        if (value instanceof String url && Strings.isFilled(url)) {
-            try {
-                return URI.create(url).toURL();
-            } catch (Exception exception) {
-                throw createInvalidHttpsUrlException(property, url);
-            }
-        }
-
-        return null;
     }
 
     private HandledException createInvalidHttpsUrlException(Property property, String value) {
