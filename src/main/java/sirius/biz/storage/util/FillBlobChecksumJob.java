1da88b3868f90a35be257b46f59bd2ffec962b27
Idevaldo De Lira <ili@scireum.de>
2025-11-10 14:40:21 +0100

    Adds a job to compute the checksum for existing blobs
    It displays a list of spaces which have a hasher configured. Blobs are queried in blocks of 1000 and processed in the selected number of threads.
    
    The factories can be overridden to provide a custom query tuner if needed.
    
    Fixes: SIRI-1158

---
+++ src/main/java/sirius/biz/storage/util/FillBlobChecksumJob.java
@@ -0,0 +1,244 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Stuttgart, Germany
+ *
+ * Copyright by scireum GmbH
+ * http://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.storage.util;
+
+import sirius.biz.cluster.work.DistributedTaskExecutor;
+import sirius.biz.jobs.StandardCategories;
+import sirius.biz.jobs.batch.BatchJob;
+import sirius.biz.jobs.batch.BatchProcessJobFactory;
+import sirius.biz.jobs.batch.DefaultBatchProcessFactory;
+import sirius.biz.jobs.params.IntParameter;
+import sirius.biz.jobs.params.Parameter;
+import sirius.biz.jobs.params.SelectStringParameter;
+import sirius.biz.jobs.params.StringParameter;
+import sirius.biz.process.PersistencePeriod;
+import sirius.biz.process.ProcessContext;
+import sirius.biz.process.logs.ProcessLog;
+import sirius.biz.storage.layer2.Blob;
+import sirius.biz.storage.layer2.BlobStorage;
+import sirius.biz.storage.layer2.BlobStorageSpace;
+import sirius.biz.storage.layer2.variants.BlobVariant;
+import sirius.db.mixing.BaseEntity;
+import sirius.kernel.async.ParallelTaskExecutor;
+import sirius.kernel.async.TaskContext;
+import sirius.kernel.commons.Hasher;
+import sirius.kernel.commons.Strings;
+import sirius.kernel.di.std.Part;
+import sirius.kernel.health.Exceptions;
+
+import javax.annotation.Nullable;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+
+/**
+ * This job is used to fill the checksum for {@linkplain Blob blobs} and {@linkplain BlobVariant variants}, where
+ * a checksum algorithm was configured after the blob or variant was created.
+ *
+ * @param <B> the type of the blob being checked
+ * @param <I> the type of the ID of the blob being checked
+ */
+public abstract class FillBlobChecksumJob<B extends BaseEntity<I> & Blob, I> extends BatchJob {
+
+    @Part
+    private static BlobStorage blobStorage;
+
+    private static final int DEFAULT_PARALLEL_TASKS = 4;
+
+    /**
+     * The parameter name which specifies the storage space to be checked.
+     */
+    public static final String STORAGE_SPACE_PARAMETER = "space";
+    public static final String PARALLEL_TASKS_PARAMETER = "parallelTasksParameter";
+    public static final String START_FROM_ID_PARAMETER = "startFromId";
+
+    protected BlobStorageSpace storageSpace;
+    private final AtomicInteger totalBlobs = new AtomicInteger(0);
+    private final AtomicInteger totalVariants = new AtomicInteger(0);
+    private final AtomicInteger totalErrors = new AtomicInteger(0);
+
+    /**
+     * Creates a new batch job for the given batch process.
+     * <p>
+     * As a batch job is created per execution, subclasses can define fields and fill those from parameters
+     * defined by their factory.
+     *
+     * @param process the context in which the process will be executed
+     */
+    protected FillBlobChecksumJob(ProcessContext process) {
+        super(process);
+    }
+
+    @Override
+    public void execute() throws Exception {
+        String spaceName = getStorageSpaceName();
+        storageSpace = blobStorage.getSpace(spaceName);
+        int parallelTasks = process.get(PARALLEL_TASKS_PARAMETER).asInt(DEFAULT_PARALLEL_TASKS);
+
+        I lastId = null;
+        I firstIdInBlock = null;
+        while (TaskContext.get().isActive()) {
+            List<B> blobs = fetchNextBlobBatch(lastId);
+            if (blobs.isEmpty()) {
+                break;
+            }
+            firstIdInBlock = fetchId(blobs.getFirst());
+
+            ParallelTaskExecutor executor = new ParallelTaskExecutor(parallelTasks);
+            for (B blob : blobs) {
+                lastId = fetchId(blob);
+                executor.submitTask(() -> processBlob(blob));
+            }
+            executor.shutdownWhenDone();
+            process.tryUpdateState(buildStatusMessage());
+        }
+        process.forceUpdateState(buildStatusMessage());
+
+        if (!TaskContext.get().isActive() && firstIdInBlock != null) {
+            process.log(ProcessLog.warn()
+                                  .withFormattedMessage("Job was cancelled. Resume it starting from ID: %s",
+                                                        firstIdInBlock.toString()));
+        }
+    }
+
+    private String buildStatusMessage() {
+        return Strings.apply("Blobs: %s | Variants: %s | Errors: %s",
+                             totalBlobs.get(),
+                             totalVariants.get(),
+                             totalErrors.get());
+    }
+
+    private <E extends BaseEntity<I>> I fetchId(E entity) {
+        return entity.getId();
+    }
+
+    private void processBlob(B blob) {
+        String blobChecksum = computeChecksum(blob.getPhysicalObjectKey());
+        if (blobChecksum != null) {
+            try {
+                updateBlobChecksum(blob, blobChecksum);
+                totalBlobs.incrementAndGet();
+            } catch (Exception _) {
+                totalErrors.incrementAndGet();
+            }
+        }
+        blob.fetchVariants()
+            .stream()
+            .filter(variant -> Strings.isFilled(variant.getPhysicalObjectKey()))
+            .filter(variant -> Strings.isEmpty(variant.getCheckSum()))
+            .forEach(variant -> {
+                String variantChecksum = computeChecksum(variant.getPhysicalObjectKey());
+                if (variantChecksum != null) {
+                    totalVariants.incrementAndGet();
+                    updateVariantChecksum(variant, variantChecksum);
+                }
+            });
+    }
+
+    private String computeChecksum(String physicalObjectKey) {
+        Hasher hasher = storageSpace.getHasher();
+        try (InputStream inputStream = storageSpace.getPhysicalSpace().getInputStream(physicalObjectKey).orElse(null)) {
+            if (inputStream == null) {
+                totalErrors.incrementAndGet();
+                return null;
+            }
+
+            byte[] bytes = new byte[65536];
+            int read;
+            while ((read = inputStream.read(bytes)) != -1) {
+                hasher.hashBytes(bytes, 0, read);
+            }
+            return hasher.toHexString();
+        } catch (Exception exception) {
+            // Errors are already logged by the framework
+            Exceptions.ignore(exception);
+            totalErrors.incrementAndGet();
+            return null;
+        }
+    }
+
+    protected abstract void updateBlobChecksum(B blob, String checksum);
+
+    protected abstract void updateVariantChecksum(BlobVariant variant, String variantChecksum);
+
+    protected abstract I fetchStartId();
+
+    protected abstract List<B> fetchNextBlobBatch(I lastId);
+
+    protected String getStorageSpaceName() {
+        return process.get(STORAGE_SPACE_PARAMETER).asString();
+    }
+
+    /**
+     * A factory which is used to create new instances of {@link FillBlobChecksumJob}.
+     */
+    public abstract static class FillBlobChecksumJobFactory extends BatchProcessJobFactory {
+
+        @Part
+        private BlobStorage blobStorage;
+
+        @Override
+        protected Class<? extends DistributedTaskExecutor> getExecutor() {
+            return DefaultBatchProcessFactory.DefaultBatchProcessTaskExecutor.class;
+        }
+
+        @Override
+        protected String createProcessTitle(Map<String, String> context) {
+            return getLabel() + ": " + context.get(FillBlobChecksumJob.STORAGE_SPACE_PARAMETER);
+        }
+
+        @Override
+        protected PersistencePeriod getPersistencePeriod() {
+            return PersistencePeriod.THREE_MONTHS;
+        }
+
+        @Override
+        protected void collectParameters(Consumer<Parameter<?>> parameterCollector) {
+            SelectStringParameter spaceParameter =
+                    new SelectStringParameter(STORAGE_SPACE_PARAMETER, "Storage Space").markRequired();
+            blobStorage.getSpaces()
+                       .filter(space -> space.getHasher() != null)
+                       .map(BlobStorageSpace::getName)
+                       .forEach(spaceName -> spaceParameter.withEntry(spaceName, spaceName));
+            parameterCollector.accept(spaceParameter.withDescription("Storage space to scan for missing checksums.")
+                                                    .build());
+            parameterCollector.accept(new IntParameter(PARALLEL_TASKS_PARAMETER, "Parallel Tasks").markRequired()
+                                                                                                  .withDefault(
+                                                                                                          DEFAULT_PARALLEL_TASKS)
+                                                                                                  .withDescription(
+                                                                                                          "Number of parallel tasks used to compute empty checksums.")
+                                                                                                  .build());
+            parameterCollector.accept(new StringParameter(START_FROM_ID_PARAMETER, "Start from ID").withDescription(
+                    "Optional blob ID to start from. Useful for restarting a previous cancelled job.").build());
+        }
+
+        @Override
+        public String getCategory() {
+            return StandardCategories.SYSTEM_ADMINISTRATION;
+        }
+
+        @Override
+        public int getPriority() {
+            return 10800;
+        }
+
+        @Override
+        public String getLabel() {
+            return "Fill Blob Checksums";
+        }
+
+        @Nullable
+        @Override
+        public String getDescription() {
+            return "Computes and store missing checksums for blobs and variants.";
+        }
+    }
+}
---
+++ src/main/java/sirius/biz/storage/util/FillMongoBlobChecksumJob.java
@@ -0,0 +1,128 @@
+/*
+ * Made with all the love in the world
+ * by scireum in Stuttgart, Germany
+ *
+ * Copyright by scireum GmbH
+ * https://www.scireum.de - info@scireum.de
+ */
+
+package sirius.biz.storage.util;
+
+import sirius.biz.jobs.batch.BatchJob;
+import sirius.biz.process.ProcessContext;
+import sirius.biz.storage.layer2.mongo.MongoBlob;
+import sirius.biz.storage.layer2.mongo.MongoBlobStorage;
+import sirius.biz.storage.layer2.mongo.MongoVariant;
+import sirius.biz.storage.layer2.variants.BlobVariant;
+import sirius.biz.tenants.TenantUserManager;
+import sirius.db.mixing.query.BaseQuery;
+import sirius.db.mongo.Mango;
+import sirius.db.mongo.Mongo;
+import sirius.db.mongo.MongoQuery;
+import sirius.kernel.commons.Strings;
+import sirius.kernel.di.std.Part;
+import sirius.kernel.di.std.Register;
+import sirius.web.security.Permission;
+
+import javax.annotation.Nonnull;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Implementation for the {@link FillBlobChecksumJob} using the {@link MongoBlobStorage#FRAMEWORK_MONGO_BLOB_STORAGE}.
+ */
+public class FillMongoBlobChecksumJob extends FillBlobChecksumJob<MongoBlob, String> {
+
+    @Part
+    private static Mango mango;
+
+    @Part
+    private static Mongo mongo;
+
+    private final Consumer<MongoQuery<MongoBlob>> queryTuner;
+
+    /**
+     * Creates a new batch job for the given batch process.
+     * <p>
+     * As a batch job is created per execution, subclasses can define fields and fill those from parameters
+     * defined by their factory.
+     *
+     * @param process the context in which the process will be executed
+     */
+    protected FillMongoBlobChecksumJob(ProcessContext process, Consumer<MongoQuery<MongoBlob>> queryTuner) {
+        super(process);
+        this.queryTuner = queryTuner;
+    }
+
+    @Override
+    protected void updateBlobChecksum(MongoBlob blob, String checksum) {
+        mongo.update()
+             .where(MongoBlob.ID, blob.getId())
+             .set(MongoBlob.CHECKSUM, checksum)
+             .executeForOne(MongoBlob.class);
+    }
+
+    @Override
+    protected void updateVariantChecksum(BlobVariant variant, String checksum) {
+        mongo.update()
+             .where(MongoVariant.ID, variant.getIdAsString())
+             .set(MongoVariant.CHECKSUM, checksum)
+             .executeForOne(MongoVariant.class);
+    }
+
+    @Override
+    protected String fetchStartId() {
+        return process.get(START_FROM_ID_PARAMETER).asString();
+    }
+
+    @Override
+    protected List<MongoBlob> fetchNextBlobBatch(String lastId) {
+        MongoQuery<MongoBlob> query = mango.selectFromSecondary(MongoBlob.class)
+                                           .eq(MongoBlob.SPACE_NAME, getStorageSpaceName())
+                                           .eq(MongoBlob.DELETED, false)
+                                           .eq(MongoBlob.COMMITTED, true)
+                                           .ne(MongoBlob.PHYSICAL_OBJECT_KEY, null)
+                                           .eq(MongoBlob.CHECKSUM, null);
+        if (Strings.isFilled(lastId)) {
+            query.where(mango.filters().gt(MongoBlob.ID, lastId));
+        } else {
+            String startId = fetchStartId();
+            if (Strings.isFilled(startId)) {
+                // This is the first query, and we want to start from a specific ID
+                query.where(mango.filters().gte(MongoBlob.ID, startId));
+            }
+        }
+        if (queryTuner != null) {
+            queryTuner.accept(query);
+        }
+        return query.orderAsc(MongoBlob.ID).limit(BaseQuery.MAX_LIST_SIZE).queryList();
+    }
+
+    /**
+     * Provides a factory to create instances of {@link FillMongoBlobChecksumJob}.
+     */
+    @Register(framework = MongoBlobStorage.FRAMEWORK_MONGO_BLOB_STORAGE)
+    @Permission(TenantUserManager.PERMISSION_SYSTEM_ADMINISTRATOR)
+    public static class FillMongoBlobJobFactory extends FillBlobChecksumJobFactory {
+
+        @Override
+        protected BatchJob createJob(ProcessContext process) throws Exception {
+            return new FillMongoBlobChecksumJob(process, getQueryTuner());
+        }
+
+        @Nonnull
+        @Override
+        public String getName() {
+            return "fill-mongo-blob-checksum";
+        }
+
+        /**
+         * Allows to provide a tuner for the query used to fetch the blobs to be processed.
+         *
+         * @return a consumer which tunes the query or <tt>null</tt> to not modify it
+         */
+        public Consumer<MongoQuery<MongoBlob>> getQueryTuner() {
+            return null;
+        }
+    }
+}
